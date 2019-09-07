98f24f5c2b3a57c680d0c5cca7233892bf1dabe2
Serhii Pashchenko <serh11p@protonmail.com>
2019-09-07 22:19:14 +0300

    Setup Account #2

--- ts/lib/ContextProvider.tsx
+++ ts/lib/ContextProvider.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import useConseilJSContext from "lib/useConseilJSContext";
+import useThanosSDKContext from "lib/useThanosSDKContext";
 import useAccountContext from "lib/useAccountContext";
 import useThanosContext from "lib/useThanosContext";
 
 const ContextProvider: React.FC = ({ children }) => {
   return (
     <useConseilJSContext.Provider>
-      <useAccountContext.Provider>
-        <useThanosContext.Provider>{children}</useThanosContext.Provider>
-      </useAccountContext.Provider>
+      <useThanosSDKContext.Provider>
+        <useAccountContext.Provider>
+          <useThanosContext.Provider>{children}</useThanosContext.Provider>
+        </useAccountContext.Provider>
+      </useThanosSDKContext.Provider>
     </useConseilJSContext.Provider>
   );
 };
