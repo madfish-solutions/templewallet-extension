2b60f3606270cf033e9371605fa910bbec34cc7c
Korney Vasilchenko <starkkor@gmail.com>
2019-09-07 22:51:29 +0300

    Add ExploreAccount page with shitty markup ;)

--- ts/app/App/View.tsx
+++ ts/app/App/View.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 
 import ImportAccountFromFile from "app/views/ImportAccountFromFile";
 import ImportAccountManual from "app/views/ImportAccountManual";
+import ExploreAccount from "app/views/ExploreAccount";
 
 const View: React.FC = () => (
   <Router>
+    <Redirect from="/" to="/account" />
     <Route component={ImportAccountFromFile} path="/import/file" />
     <Route component={ImportAccountManual} path="/import/manual" />
+    <Route component={ExploreAccount} path="/account" />
   </Router>
 );
 
