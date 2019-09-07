beb179e99b2dde514a1e292f3f01f0e356f4cfdc
Serhii Pashchenko <serh11p@protonmail.com>
2019-09-07 20:07:16 +0300

    Setup basic flow with account

--- ts/main/App.tsx
+++ ts/main/App.tsx
@@ -1,28 +1,50 @@
 import * as React from "react";
-import WidthContainer from "layouts/WidthContainer";
 import useConseilJSContext from "lib/useConseilJSContext";
-import useThanos from "lib/useThanos";
+import useAccountContext from "lib/useAccountContext";
+import useThanosContext from "lib/useThanosContext";
 
 const App: React.FC = () => {
   return (
     <useConseilJSContext.Provider>
-      <WidthContainer>
-        <div>KEK LAL plS</div>
-        <ConseilJSCheck />
-      </WidthContainer>
+      <useAccountContext.Provider>
+        <useThanosContext.Provider>KEK</useThanosContext.Provider>
+      </useAccountContext.Provider>
     </useConseilJSContext.Provider>
   );
 };
 
 export default App;
 
-const ConseilJSCheck: React.FC = () => {
-  const thanos = useThanos();
+// const ThanosCheck: React.FC = () => {
+//   const { initialized, setup } = useAccountContext();
+//   React.useEffect(() => {
+//     if (initialized) {
+//       setup({
+//         mnemonic: [
+//           "garbage",
+//           "utility",
+//           "estate",
+//           "lounge",
+//           "lava",
+//           "fantasy",
+//           "wish",
+//           "old",
+//           "alter",
+//           "clinic",
+//           "because",
+//           "vintage",
+//           "donate",
+//           "trumpet",
+//           "lemon"
+//         ],
+//         secret: "129f0ea640d93dfbe8c25e640f521441f6f848f1",
+//         amount: "43888838596",
+//         pkh: "tz1abiiJXDW9hdCAbpF79xM5EVK1diAgnXeH",
+//         password: "F7MTvJTNFW",
+//         email: "htrtjpnq.jgczvxww@tezos.example.org"
+//       });
+//     }
+//   }, [initialized, setup]);
 
-  if (thanos.conseilJsLoaded) {
-    (window as any)["thanos"] = thanos;
-    return <div>Thanos has come.</div>;
-  }
-
-  return null;
-};
+//   return null;
+// };
