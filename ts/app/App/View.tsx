0d770a9ce5490320b0f6d5199772bfd9fa0816f8
Korney Vasilchenko <starkkor@gmail.com>
2019-09-07 23:36:12 +0300

    Add ReceiveFunds view

--- ts/app/App/View.tsx
+++ ts/app/App/View.tsx
@@ -5,14 +5,16 @@ import ImportAccountFromFile from "app/views/ImportAccountFromFile";
 import ImportAccountManual from "app/views/ImportAccountManual";
 import ExploreAccount from "app/views/ExploreAccount";
 import TransferFunds from "app/views/TransferFunds";
+import ReceiveFunds from "app/views/ReceiveFunds";
 
 const View: React.FC = () => (
   <Router>
-    <Redirect from="/" to="/account/transfer" />
+    <Redirect from="/" to="/account/receive" />
     <Route component={ImportAccountFromFile} path="/import/file" />
     <Route component={ImportAccountManual} path="/import/manual" />
     <Route exact component={ExploreAccount} path="/account" />
     <Route component={TransferFunds} path="/account/transfer" />
+    <Route component={ReceiveFunds} path="/account/receive" />
   </Router>
 );
 
