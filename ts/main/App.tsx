abb65a728f3f787c803561d2543c27a4218d808b
Serhii Pashchenko <serh11p@protonmail.com>
2019-09-07 12:13:49 +0300

    Setup ConseilJS

--- ts/main/App.tsx
+++ ts/main/App.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
 import WidthContainer from "main/layouts/WidthContainer";
+import useConseilJSContext from "lib/useConseilJSContext";
 
-const App: React.FC = () => (
-  <WidthContainer>
-    <div>KEK LAL plS</div>
-  </WidthContainer>
-);
+const App: React.FC = () => {
+  return (
+    <useConseilJSContext.Provider>
+      <WidthContainer>
+        <div>KEK LAL plS</div>
+        <ConseilJSCheck />
+      </WidthContainer>
+    </useConseilJSContext.Provider>
+  );
+};
 
 export default App;
+
+const ConseilJSCheck: React.FC = () => {
+  const conseiljs = useConseilJSContext();
+  console.info(conseiljs);
+
+  return null;
+};
