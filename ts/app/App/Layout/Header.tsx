d3b4fe33c2f5b24ec1628017ef73f8f876a37ed1
Korney Vasilchenko <starkkor@gmail.com>
2019-09-08 10:43:24 +0300

    Update icons and header logo

--- ts/app/App/Layout/Header.tsx
+++ ts/app/App/Layout/Header.tsx
@@ -14,17 +14,11 @@ const Header: React.FC<HeaderProps> = ({ popup }) => (
       className="flex items-center justify-between flex-wrap py-6"
     >
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          className="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-        </svg>
+        <img src="../misc/icon.png" width="36" height="36" />
         {!popup && (
-          <span className="font-semibold text-xl tracking-tight">Thanos</span>
+          <span className="font-semibold ml-2 text-xl tracking-tight">
+            Thanos
+          </span>
         )}
       </div>
       <div className="flex flex-grow items-center w-auto">
