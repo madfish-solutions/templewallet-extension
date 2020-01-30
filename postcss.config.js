cdd39a5482996a8610128d0c456274940bf811c0
serh11p <git@serh11p.com>
2020-01-30 16:37:35 +0200

    Fix PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,12 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
-    NODE_ENV === "production" && require("@fullhuman/postcss-purgecss")(),
+    NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./static/**/*.{html,js,mjs}", "./ts/**/*.{js,jsx,ts,tsx}"],
+        // Include any special characters you're using in this regular expression
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      }),
     require("autoprefixer"),
     NODE_ENV === "production" && require("cssnano")({ preset: "default" })
   ].filter(Boolean)
--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ["./static/**/*.{html,js,mjs}", "./ts/**/*.{js,jsx,ts,tsx}"],
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-};
