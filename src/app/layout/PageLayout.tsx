b77299a11b6efa26fb41036078fb5dd6f64bbabf
serh11p <git@serh11p.com>
2020-02-05 19:17:59 +0200

    Rename var

--- src/app/layout/PageLayout.tsx
+++ src/app/layout/PageLayout.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import Header from "./PageLayout/Header";
 import Content from "./PageLayout/Content";
 
-const Layout: React.FC = ({ children }) => (
+const PageLayout: React.FC = ({ children }) => (
   <>
     <Header />
     <Content>{children}</Content>
   </>
 );
 
-export default Layout;
+export default PageLayout;
