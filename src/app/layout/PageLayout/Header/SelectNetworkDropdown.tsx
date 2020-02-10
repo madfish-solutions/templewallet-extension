4e844fc90268b3aa0479176448bebe6641dc3a05
serh11p <git@serh11p.com>
2020-02-10 14:12:24 +0200

    Update primary color, rollback to orange

--- src/app/layout/PageLayout/Header/SelectNetworkDropdown.tsx
+++ src/app/layout/PageLayout/Header/SelectNetworkDropdown.tsx
@@ -40,8 +40,8 @@ const SelectNetworkDropdown: React.FC = () => {
       <button
         className={classNames(
           "px-4 py-2",
-          "border-2 border-indigo-100 hover:border-white",
-          "text-indigo-100 hover:text-white",
+          "border-2 border-primary-orange-lighter hover:border-white",
+          "text-primary-orange-lighter hover:text-white",
           "text-base font-medium",
           "rounded",
           "flex items-center"
