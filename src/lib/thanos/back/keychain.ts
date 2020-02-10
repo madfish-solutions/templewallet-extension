9cd84c80805c30ee814702c38990366b34537669
serh11p <git@serh11p.com>
2020-02-10 17:25:41 +0200

    Add few drafts...

---
+++ src/lib/thanos/back/keychain.ts
@@ -0,0 +1,26 @@
+/**
+ * @description
+ * TODO: Add desc
+ */
+export interface Keyring {
+  /**
+   * @description
+   * In this method, you must return any JSON-serializable JavaScript object
+   * that you like. It will be encoded to a string, encrypted with
+   * the user's password, and stored to disk.
+   * This is the same object you will receive in the deserialize() method,
+   * so it should capture all the information you need to restore
+   * the Keyring's state.
+   */
+  serialize(): Promise<any>;
+
+  /**
+   * @description
+   * As discussed above, the deserialize() method will be passed
+   * the JavaScript object that you returned whenthe serialize() method
+   * was called.
+   */
+  deserialize(): Promise<any>;
+
+  addAccounts();
+}
