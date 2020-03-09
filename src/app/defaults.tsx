d86e6862c1493220c8711978af49c1e50be69b65
serh11p <git@serh11p.com>
2020-03-09 14:08:58 +0200

    Make password more strict

--- src/app/defaults.tsx
+++ src/app/defaults.tsx
@@ -5,7 +5,8 @@ export const ACCOUNT_NAME_PATTERN = /^[a-zA-Z0-9 _-]{3,14}$/;
 export const PASSWORD_PATTERN = new RegExp(
   [
     "^",
-    "(?=.*[a-zA-Z])", // Must contain at least 1 alphabetical character
+    "(?=.*[a-z])", // Must contain at least 1 lowercase alphabetical character
+    "(?=.*[A-Z])", // Must contain at least 1 uppercase alphabetical character
     "(?=.*[0-9])", // Must contain at least 1 numeric character
     "(?=.{8,})" // Must be eight characters or longer
   ].join("")
@@ -16,7 +17,8 @@ export const PASSWORD_ERROR_CAPTION = (
     <li>Required</li>
     <li>At least 8 characters</li>
     <li>At least 1 number</li>
-    <li>At least 1 letter</li>
+    <li>At least 1 lowercase letter</li>
+    <li>At least 1 uppercase letter</li>
   </ul>
 );
 
