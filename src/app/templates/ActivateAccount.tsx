809118d3f10fe691608b6c1b79cc709c8c1000fb
Denis <1duglas1@gmail.com>
2020-03-17 15:09:04 +0200

    Implemented ActivateAccount

---
+++ src/app/templates/ActivateAccount.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+
+import Alert from "app/atoms/Alert";
+import FormField from "app/atoms/FormField";
+import FormSubmitButton from "app/atoms/FormSubmitButton";
+
+type FormData = {
+  secret: string;
+};
+
+const SUBMIT_ERROR_TYPE = "submit-error";
+
+const ActivateAccount: React.FC = () => {
+  const [isSuccess, setSuccess] = React.useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState,
+    clearError,
+    setError,
+    errors
+  } = useForm<FormData>();
+  const { isSubmitting } = formState;
+
+  const onSubmit = React.useCallback(
+    async (data: FormData) => {
+      if (isSubmitting) return;
+
+      clearError("secret");
+      setSuccess(false);
+      try {
+        const activateActionMock = (secret: string) =>
+          new Promise(res => setTimeout(() => res(secret), 350));
+        await activateActionMock(data.secret);
+        // throw new Error("This secret key is not valid :(");
+
+        setSuccess(true);
+      } catch (err) {
+        if (process.env.NODE_ENV === "development") {
+          console.error(err);
+        }
+
+        setError("secret", SUBMIT_ERROR_TYPE, err.message);
+      }
+    },
+    [clearError, isSubmitting, setError]
+  );
+
+  return (
+    <form
+      className="w-full max-w-sm mx-auto p-2"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      {isSuccess && (
+        <Alert
+          type="success"
+          title="Success"
+          description="You successfully activated your account"
+          className="mb-4"
+        />
+      )}
+      <FormField
+        name="secret"
+        ref={register({ required: true })}
+        containerClassName="mb-4"
+        label="Secret"
+        labelDescription="Wtf description"
+        placeholder="e.g. n4hs7sd3..."
+        errorCaption={errors.secret && errors.secret.message}
+      />
+      <FormSubmitButton loading={isSubmitting}>Activate</FormSubmitButton>
+    </form>
+  );
+};
+
+export default ActivateAccount;
