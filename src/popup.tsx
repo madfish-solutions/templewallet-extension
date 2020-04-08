c9de7b81be11971ff2f6104f5e4b39c9b68016bd
serh11p <git@serh11p.com>
2020-04-08 18:39:26 +0300

    Update manifest file; remove useless code

--- src/popup.tsx
+++ src/popup.tsx
@@ -9,19 +9,3 @@ ReactDOM.render(
   <App env={{ windowType: WindowType.Popup }} />,
   document.getElementById("root")
 );
-
-// (async () => {
-//   const tabs = await browser.tabs.query({
-//     active: true,
-//     lastFocusedWindow: true
-//   });
-
-//   const url = tabs.length && tabs[0].url;
-
-//   const response = await browser.runtime.sendMessage({
-//     msg: "hello",
-//     url
-//   });
-
-//   console.info(response);
-// })();
