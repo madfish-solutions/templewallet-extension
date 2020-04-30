8932dc3014a4ba448b249637b239ca12cca83c56
serh11p <git@serh11p.com>
2020-04-30 13:40:15 +0300

    Refactor Identicon component

--- src/app/atoms/Identicon.tsx
+++ src/app/atoms/Identicon.tsx
@@ -35,8 +35,7 @@ const Identicon: React.FC<IdenticonProps> = ({
       };
       const imgSrc =
         type === "jdenticon"
-          ? // ? `data:image/svg+xml;base64,${btoa(toSvg(hash, size))}`
-            jdenticonIcons.create(hash, opts)
+          ? jdenticonIcons.create(hash, opts)
           : botttsIcons.create(hash, opts);
 
       const bi = `url('${imgSrc}')`;
@@ -47,26 +46,21 @@ const Identicon: React.FC<IdenticonProps> = ({
 
   return (
     <div
-      className="flex items-center justify-center"
-      style={{ width: size, height: size }}
-    >
-      <div
-        className={classNames(
-          "inline-block",
-          "bg-gray-100 bg-no-repeat bg-center",
-          "overflow-hidden",
-          className
-        )}
-        style={{
-          backgroundImage,
-          width: size,
-          height: size,
-          borderRadius: Math.round(size / 10),
-          ...style,
-        }}
-        {...rest}
-      />
-    </div>
+      className={classNames(
+        "inline-block",
+        "bg-gray-100 bg-no-repeat bg-center",
+        "overflow-hidden",
+        className
+      )}
+      style={{
+        backgroundImage,
+        width: size,
+        height: size,
+        borderRadius: Math.round(size / 10),
+        ...style,
+      }}
+      {...rest}
+    />
   );
 };
 
