bbb3f8422443053b9e93cfa94ac19f77579fafa1
Alexandr Zosimchuk <al.zosimchuk@gmail.com>
2020-05-05 16:46:38 +0300

    implement deposit componene

--- src/app/pages/Receive.tsx
+++ src/app/pages/Receive.tsx
@@ -1,16 +1,35 @@
 import * as React from "react";
 import classNames from "clsx";
 import { QRCode } from "react-qr-svg";
+import { createNewTab } from "background";
 import { useReadyThanos } from "lib/thanos/front";
 import useCopyToClipboard from "lib/ui/useCopyToClipboard";
 import PageLayout from "app/layouts/PageLayout";
 import FormField from "app/atoms/FormField";
 import { ReactComponent as QRIcon } from "app/icons/qr.svg";
 import { ReactComponent as CopyIcon } from "app/icons/copy.svg";
+import Name from "app/atoms/Name";
+import SubTitle from "app/atoms/SubTitle";
+import { TEZOS_FAUCET } from "app/defaults";
 
+interface FaucetType {
+  main: string;
+  test: string;
+}
 const Receive: React.FC = () => {
-  const { account } = useReadyThanos();
+  const {
+    account,
+    network: { type: networkType },
+  } = useReadyThanos();
+
   const address = account.publicKeyHash;
+
+  const isMainNetwork = networkType === "main";
+  const coinswitchUrl = `https://coinswitch.thanoswallet.com/?to=xtz&address=${address}`;
+  const tezosFaucetUrl = (TEZOS_FAUCET as any)[networkType];
+  const tezosFaucetName = isMainNetwork
+    ? "Tezos Faucet"
+    : "Tezos Faucet (Alpha)";
 
   const { fieldRef, copy, copied } = useCopyToClipboard();
 
@@ -94,6 +113,75 @@ const Receive: React.FC = () => {
                 style={{ width: "100%" }}
                 value={address}
               />
+            </div>
+            <div className="py-4">
+              <SubTitle>Deposit</SubTitle>
+              <div
+                className={classNames(
+                  "w-64",
+                  "rounded-md overflow-hidden",
+                  "border-2 bg-gray-100",
+                  "flex flex-col",
+                  "text-gray-700 text-sm leading-tight"
+                )}
+              >
+                <button
+                  key={0}
+                  type="button"
+                  className={classNames(
+                    "block w-full ",
+                    "overflow-hidden",
+                    /*!last &&*/ "border-b border-gray-200",
+                    "hover:bg-gray-200 focus:bg-gray-200",
+                    "flex items-center justify-center",
+                    "text-gray-700",
+                    "transition ease-in-out duration-200",
+                    "focus:outline-none",
+                    "opacity-90 hover:opacity-100"
+                  )}
+                  style={{
+                    padding: "0.8rem 0.375rem 0.8rem 0.375rem",
+                  }}
+                  onClick={() => createNewTab(tezosFaucetUrl)}
+                >
+                  <div className="ml-2 flex flex-col items-center">
+                    <div className="flex flex-wrap items-center">
+                      <Name className="text-sm font-medium leading-tight">
+                        {tezosFaucetName}
+                      </Name>
+                    </div>
+                  </div>
+                </button>
+                {isMainNetwork && (
+                  <button
+                    key={1}
+                    type="button"
+                    className={classNames(
+                      "block w-full",
+                      "overflow-hidden",
+                      "border-b border-gray-200",
+                      "hover:bg-gray-200 focus:bg-gray-200",
+                      "flex items-center justify-center",
+                      "text-gray-700",
+                      "transition ease-in-out duration-200",
+                      "focus:outline-none",
+                      "opacity-90 hover:opacity-100"
+                    )}
+                    style={{
+                      padding: "0.8rem 0.375rem 0.8rem 0.375rem",
+                    }}
+                    onClick={() => createNewTab(coinswitchUrl)}
+                  >
+                    <div className="ml-2 flex flex-col items-center">
+                      <div className="flex flex-wrap items-center">
+                        <Name className="text-sm font-medium leading-tight">
+                          Use CoinSwitch to exchange
+                        </Name>
+                      </div>
+                    </div>
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
