3592b21432a9fb573ee8f9ae51feb0a5b41bcd8a
serh11p <git@serh11p.com>
2020-09-16 18:29:52 +0300

    Implement displaying balance for DApp Confirm Page

--- src/app/templates/AccountBanner.tsx
+++ src/app/templates/AccountBanner.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import classNames from "clsx";
-import { ThanosAccount, ThanosAccountType } from "lib/thanos/front";
+import { ThanosAccount, ThanosAccountType, XTZ_ASSET } from "lib/thanos/front";
 import Balance from "app/templates/Balance";
 import Money from "app/atoms/Money";
 import Identicon from "app/atoms/Identicon";
@@ -9,6 +9,7 @@ import Name from "app/atoms/Name";
 type AccountBannerProps = React.HTMLAttributes<HTMLDivElement> & {
   account: ThanosAccount;
   displayBalance?: boolean;
+  networkRpc?: string;
   label?: React.ReactNode;
   labelDescription?: React.ReactNode;
   labelIndent?: "sm" | "md";
@@ -17,116 +18,109 @@ type AccountBannerProps = React.HTMLAttributes<HTMLDivElement> & {
 const AccountBanner: React.FC<AccountBannerProps> = ({
   account,
   displayBalance = true,
+  networkRpc,
   className,
   label = "Account",
   labelIndent = "md",
   labelDescription,
-}) => {
-  const assetSymbol = "XTZ";
-
-  return (
-    <div className={classNames("flex flex-col", className)}>
-      {(label || labelDescription) && (
-        <h2
-          className={classNames(
-            labelIndent === "md" ? "mb-4" : "mb-2",
-            "leading-tight",
-            "flex flex-col"
-          )}
-        >
-          {label && (
-            <span className="text-base font-semibold text-gray-700">
-              {label}
-            </span>
-          )}
-
-          {labelDescription && (
-            <span
-              className={classNames("mt-1", "text-xs font-light text-gray-600")}
-              style={{ maxWidth: "90%" }}
-            >
-              {labelDescription}
-            </span>
-          )}
-        </h2>
-      )}
-
-      <div
+}) => (
+  <div className={classNames("flex flex-col", className)}>
+    {(label || labelDescription) && (
+      <h2
         className={classNames(
-          "w-full",
-          "border rounded-md",
-          "p-2",
-          "flex items-center"
+          labelIndent === "md" ? "mb-4" : "mb-2",
+          "leading-tight",
+          "flex flex-col"
         )}
       >
-        <Identicon
-          type="bottts"
-          hash={account.publicKeyHash}
-          size={32}
-          className="flex-shrink-0 shadow-xs"
-        />
+        {label && (
+          <span className="text-base font-semibold text-gray-700">{label}</span>
+        )}
 
-        <div className="ml-2 flex flex-col items-start">
-          <div className="flex flex-wrap items-center">
-            <Name className="text-sm font-medium leading-tight text-gray-800">
-              {account.name}
-            </Name>
+        {labelDescription && (
+          <span
+            className={classNames("mt-1", "text-xs font-light text-gray-600")}
+            style={{ maxWidth: "90%" }}
+          >
+            {labelDescription}
+          </span>
+        )}
+      </h2>
+    )}
 
-            {account.type === ThanosAccountType.Imported && (
-              <span
-                className={classNames(
-                  "ml-2",
-                  "rounded-sm",
-                  "border border-black border-opacity-25",
-                  "px-1 py-px",
-                  "leading-tight",
-                  "text-black text-opacity-50"
-                )}
-                style={{ fontSize: "0.6rem" }}
-              >
-                Imported
-              </span>
-            )}
-          </div>
+    <div
+      className={classNames(
+        "w-full",
+        "border rounded-md",
+        "p-2",
+        "flex items-center"
+      )}
+    >
+      <Identicon
+        type="bottts"
+        hash={account.publicKeyHash}
+        size={32}
+        className="flex-shrink-0 shadow-xs"
+      />
 
-          <div className="mt-1 flex flex-wrap items-center">
-            <div
-              className={classNames("text-xs leading-none", "text-gray-700")}
+      <div className="flex flex-col items-start ml-2">
+        <div className="flex flex-wrap items-center">
+          <Name className="text-sm font-medium leading-tight text-gray-800">
+            {account.name}
+          </Name>
+
+          {account.type === ThanosAccountType.Imported && (
+            <span
+              className={classNames(
+                "ml-2",
+                "rounded-sm",
+                "border border-black border-opacity-25",
+                "px-1 py-px",
+                "leading-tight",
+                "text-black text-opacity-50"
+              )}
+              style={{ fontSize: "0.6rem" }}
             >
-              {(() => {
-                const val = account.publicKeyHash;
-                const ln = val.length;
-                return (
-                  <>
-                    {val.slice(0, 7)}
-                    <span className="opacity-75">...</span>
-                    {val.slice(ln - 4, ln)}
-                  </>
-                );
-              })()}
-            </div>
+              Imported
+            </span>
+          )}
+        </div>
 
-            {displayBalance && (
-              <Balance address={account.publicKeyHash}>
-                {(bal) => (
-                  <div
-                    className={classNames(
-                      "ml-2",
-                      "text-xs leading-none",
-                      "text-gray-600"
-                    )}
-                  >
-                    <Money>{bal}</Money>{" "}
-                    <span style={{ fontSize: "0.75em" }}>{assetSymbol}</span>
-                  </div>
-                )}
-              </Balance>
-            )}
+        <div className="flex flex-wrap items-center mt-1">
+          <div className={classNames("text-xs leading-none", "text-gray-700")}>
+            {(() => {
+              const val = account.publicKeyHash;
+              const ln = val.length;
+              return (
+                <>
+                  {val.slice(0, 7)}
+                  <span className="opacity-75">...</span>
+                  {val.slice(ln - 4, ln)}
+                </>
+              );
+            })()}
           </div>
+
+          {displayBalance && (
+            <Balance address={account.publicKeyHash} networkRpc={networkRpc}>
+              {(bal) => (
+                <div
+                  className={classNames(
+                    "ml-2",
+                    "text-xs leading-none",
+                    "text-gray-600"
+                  )}
+                >
+                  <Money>{bal}</Money>{" "}
+                  <span style={{ fontSize: "0.75em" }}>{XTZ_ASSET.symbol}</span>
+                </div>
+              )}
+            </Balance>
+          )}
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default AccountBanner;
