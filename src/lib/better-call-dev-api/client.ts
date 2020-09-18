5a27f583f1bd0b941c877b55f52a909b70e02def
Inokentii Mazhara <keshan3262@gmail.com>
2020-09-18 17:44:07 +0300

    Remove data transformation

--- src/lib/better-call-dev-api/client.ts
+++ src/lib/better-call-dev-api/client.ts
@@ -2,8 +2,6 @@ import axios, { AxiosError } from "axios";
 import {
   BcdTokenTransfersQueryParams,
   BcdContractsQueryParams,
-  BcdRawPageableTokenContracts,
-  BcdRawPageableTokenTransfers,
   BcdPageableTokenContracts,
   BcdPageableTokenTransfers,
 } from "lib/better-call-dev-api/types";
@@ -17,57 +15,34 @@ api.interceptors.response.use(
   }
 );
 
-const identityFn = (x: any) => x;
-
 export const getContracts = makeQuery<
   BcdContractsQueryParams,
-  BcdRawPageableTokenContracts,
   BcdPageableTokenContracts
 >(
   (params) =>
     `/tokens/${params.network}${
       params.faversion ? `/version/${params.faversion}` : ""
     }`,
-  ["last_id", "size"],
-  ({ tokens, last_id, total }) => ({
-    last_id,
-    total,
-    tokens: tokens.map(({ last_action, timestamp, ...restTokenProps }) => ({
-      ...restTokenProps,
-      last_action: new Date(last_action),
-      timestamp: new Date(timestamp),
-    })),
-  })
+  ["last_id", "size"]
 );
 
 export const getTokenTransfers = makeQuery<
   BcdTokenTransfersQueryParams,
-  BcdRawPageableTokenTransfers,
   BcdPageableTokenTransfers
->(
-  (params) => `/tokens/${params.network}/transfers/${params.address}`,
-  ["last_id", "size"],
-  ({ transfers, last_id }) => ({
-    last_id,
-    transfers: transfers.map(({ timestamp, ...restProps }) => ({
-      ...restProps,
-      timestamp: new Date(timestamp),
-    })),
-  })
-);
+>((params) => `/tokens/${params.network}/transfers/${params.address}`, [
+  "last_id",
+  "size",
+]);
 
-function makeQuery<P extends Record<string, unknown>, R, T = R>(
+function makeQuery<P extends Record<string, unknown>, R>(
   url: (params: P) => string,
-  searchParamsKeys: Array<keyof P>,
-  transformer: (rawData: R) => T = identityFn
+  searchParamsKeys: Array<keyof P>
 ) {
   return (params: P) => {
     const searchParams = Object.fromEntries(
       Object.entries(params).filter(([key]) => searchParamsKeys.includes(key))
     );
 
-    return api
-      .get<R>(url(params), { params: searchParams })
-      .then((response) => transformer(response.data));
+    return api.get<R>(url(params), { params: searchParams });
   };
 }
