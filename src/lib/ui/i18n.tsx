795823945c0885781efd9c90cdae286b38ca72d9
Inokentii Mazhara <keshan3262@gmail.com>
2020-09-28 17:00:06 +0300

    Remake some labels using i18n

--- src/lib/ui/i18n.tsx
+++ src/lib/ui/i18n.tsx
@@ -3,13 +3,37 @@ import { t } from "lib/i18n";
 
 export * from "lib/i18n";
 
+type BrowserI18nCompatibleSubstitutions = string | string[] | undefined;
 export type TProps = {
   name: string;
-  substitutions?: any;
-  children?: (m: string) => React.ReactElement;
+  substitutions?: BrowserI18nCompatibleSubstitutions | React.ReactChild[];
+  children?: (m: React.ReactChild) => React.ReactElement;
 };
 
 export const T = React.memo<TProps>(({ name, substitutions, children }) => {
-  const message = t(name, substitutions);
+  const message = isBrowserI18nCompatibleSubstitutions(substitutions) ? (
+    t(name, substitutions)
+  ) : (
+    <>
+      {t(name)
+        ?.split("%ch")
+        .map((fragment, index) => (
+          <React.Fragment key={index}>
+            {fragment}
+            {substitutions[index]}
+          </React.Fragment>
+        ))}
+    </>
+  );
   return children ? children(message) : <>{message}</>;
 });
+
+function isBrowserI18nCompatibleSubstitutions(
+  substitutions: TProps["substitutions"]
+): substitutions is BrowserI18nCompatibleSubstitutions {
+  return (
+    !substitutions ||
+    typeof substitutions === "string" ||
+    substitutions.every((sub) => typeof sub === "string")
+  );
+}
