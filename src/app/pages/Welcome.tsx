c49ef861af841bda0b164ca53dccec6f533e69cc
Inokentii Mazhara <keshan3262@gmail.com>
2020-09-29 18:31:11 +0300

    Remake most of templates labels using i18n

--- src/app/pages/Welcome.tsx
+++ src/app/pages/Welcome.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import classNames from "clsx";
+import { T } from "lib/ui/i18n";
 import { Link } from "lib/woozie";
 import Logo from "app/atoms/Logo";
 import { ReactComponent as EntranceIcon } from "app/icons/entrance.svg";
@@ -19,26 +20,16 @@ const SIGNS = [
         {...rest}
       />
     ),
-    title: "Import existing Wallet",
-    description: (
-      <>
-        Already have a seed phrase? Import your existing wallet using a 12 or
-        more mnemonic words
-      </>
-    ),
+    titleI18nKey: "importExistingWallet",
+    descriptionI18nKey: "importExistingWalletDescription",
   },
   {
     key: "create",
     linkTo: "/create-wallet",
     filled: true,
     Icon: FolderAddIcon,
-    title: "Create a new Wallet",
-    description: (
-      <>
-        New to Thanos Wallet? Let’s set it up! This will create a new wallet
-        and seed phrase
-      </>
-    ),
+    titleI18nKey: "createNewWallet",
+    descriptionI18nKey: "createNewWalletDescription",
   },
 ];
 
@@ -69,64 +60,74 @@ const Welcome: React.FC = () => {
       </div>
 
       <div className={classNames("w-full", "my-4", "flex items-stretch")}>
-        {SIGNS.map(({ key, linkTo, filled, Icon, title, description }) => (
-          <div key={key} className={classNames("w-1/2", "p-4")}>
-            <Link
-              to={linkTo}
-              className={classNames(
-                "relative block",
-                "w-full pb-2/3",
-                "bg-primary-orange",
-                "overflow-hidden rounded-lg",
-                "transition duration-300 ease-in-out",
-                "transform hover:scale-110 focus:scale-110",
-                "shadow-md hover:shadow-lg focus:shadow-lg"
-              )}
-            >
-              <div className={classNames("absolute inset-0", "p-1")}>
-                <div
-                  className={classNames(
-                    "w-full h-full",
-                    "overflow-hidden rounded-md",
-                    "px-10 py-4",
-                    "flex flex-col",
-                    filled
-                      ? "text-white"
-                      : "shadow-inner bg-primary-orange-lighter text-primary-orange",
-                    "text-shadow-black-orange"
-                  )}
-                >
+        {SIGNS.map(
+          ({ key, linkTo, filled, Icon, titleI18nKey, descriptionI18nKey }) => (
+            <div key={key} className={classNames("w-1/2", "p-4")}>
+              <Link
+                to={linkTo}
+                className={classNames(
+                  "relative block",
+                  "w-full pb-2/3",
+                  "bg-primary-orange",
+                  "overflow-hidden rounded-lg",
+                  "transition duration-300 ease-in-out",
+                  "transform hover:scale-110 focus:scale-110",
+                  "shadow-md hover:shadow-lg focus:shadow-lg"
+                )}
+              >
+                <div className={classNames("absolute inset-0", "p-1")}>
                   <div
                     className={classNames(
-                      "flex-1",
-                      "flex flex-col items-center justify-end"
+                      "w-full h-full",
+                      "overflow-hidden rounded-md",
+                      "px-10 py-4",
+                      "flex flex-col",
+                      filled
+                        ? "text-white"
+                        : "shadow-inner bg-primary-orange-lighter text-primary-orange",
+                      "text-shadow-black-orange"
                     )}
                   >
-                    <Icon className="transform scale-125 stroke-current" />
-                  </div>
-
-                  <h1 className="pb-1 text-xl font-semibold text-center">
-                    {title}
-                  </h1>
-
-                  <div className="flex-1">
-                    <p
+                    <div
                       className={classNames(
-                        "my-1 text-center",
-                        "text-xs",
-                        filled
-                          ? "text-primary-orange-lighter"
-                          : "text-primary-orange"
+                        "flex-1",
+                        "flex flex-col items-center justify-end"
                       )}
                     >
-                      {description}
-                    </p>
+                      <Icon className="transform scale-125 stroke-current" />
+                    </div>
+
+                    <T name={titleI18nKey}>
+                      {(message) => (
+                        <h1 className="pb-1 text-xl font-semibold text-center">
+                          {message}
+                        </h1>
+                      )}
+                    </T>
+
+                    <div className="flex-1">
+                      <T name={descriptionI18nKey}>
+                        {(message) => (
+                          <p
+                            className={classNames(
+                              "my-1 text-center",
+                              "text-xs",
+                              filled
+                                ? "text-primary-orange-lighter"
+                                : "text-primary-orange"
+                            )}
+                          >
+                            {message}
+                          </p>
+                        )}
+                      </T>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        ))}
+              </Link>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
