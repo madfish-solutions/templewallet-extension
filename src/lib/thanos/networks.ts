6298bbcd6d66f484088b13b25f66750ee062e20c
Inokentii Mazhara <keshan3262@gmail.com>
2020-09-30 11:41:04 +0300

    Remake other labels using i18n but for some names and currency symbols

--- src/lib/thanos/networks.ts
+++ src/lib/thanos/networks.ts
@@ -1,10 +1,11 @@
 import { ThanosNetwork } from "lib/thanos/types";
+import { t } from "lib/ui/i18n";
 
 export const NETWORKS: ThanosNetwork[] = [
   {
     id: "mainnet",
-    name: "Tezos Mainnet",
-    description: "Carthage mainnet",
+    name: t("tezosMainnet"),
+    description: t("tezosMainnetDescription"),
     type: "main",
     rpcBaseURL: "https://mainnet-tezos.giganode.io",
     color: "#83b300",
@@ -12,8 +13,8 @@ export const NETWORKS: ThanosNetwork[] = [
   },
   {
     id: "carthagenet",
-    name: "Carthage Testnet",
-    description: "Carthage testnet",
+    name: t("carthageTestnet"),
+    description: t("carthageTestnetDescription"),
     type: "test",
     rpcBaseURL: "https://testnet-tezos.giganode.io",
     color: "#0f4c81",
@@ -21,8 +22,8 @@ export const NETWORKS: ThanosNetwork[] = [
   },
   {
     id: "dalphanet",
-    name: "Dalpha Testnet (soon)",
-    description: "Dalpha testnet",
+    name: t("dAlphaTestnet"),
+    description: t("dAlphaTestnetDescription"),
     type: "test",
     rpcBaseURL: "https://dalphanet-tezos.giganode.io",
     color: "#ed6663",
@@ -31,7 +32,7 @@ export const NETWORKS: ThanosNetwork[] = [
   {
     id: "sandbox",
     name: "localhost:8732",
-    description: "Local Sandbox",
+    description: t("localSandbox"),
     type: "test",
     rpcBaseURL: "http://localhost:8732",
     color: "#e9e1cc",
