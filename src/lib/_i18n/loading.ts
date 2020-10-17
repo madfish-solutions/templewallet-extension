3d2309686836f1cf4c3a46172221b00a92bedf52
serh11p <git@serh11p.com>
2020-10-17 21:41:08 +0300

    Implement global version of i18n

---
+++ src/lib/_i18n/loading.ts
@@ -0,0 +1,38 @@
+import { browser } from "webextension-polyfill-ts";
+import { init } from "./core";
+import { setSavedLocale } from "./saving";
+
+export const REFRESH_MSGTYPE = "THANOS_I18N_REFRESH";
+
+const initPromise = init();
+
+browser.runtime.onMessage.addListener(async (msg) => {
+  if (msg?.type === REFRESH_MSGTYPE) {
+    if (await isBackgroundScript()) {
+      init();
+    } else {
+      window.location.reload();
+    }
+  }
+});
+
+export function onInited(callback: () => void) {
+  initPromise.then(callback);
+}
+
+export function updateLocale(locale: string) {
+  setSavedLocale(locale);
+  refresh();
+}
+
+function refresh() {
+  browser.runtime.sendMessage({ type: REFRESH_MSGTYPE });
+}
+
+async function isBackgroundScript() {
+  let backgroundWindow;
+  try {
+    backgroundWindow = await browser.runtime.getBackgroundPage();
+  } catch {}
+  return window === backgroundWindow;
+}
