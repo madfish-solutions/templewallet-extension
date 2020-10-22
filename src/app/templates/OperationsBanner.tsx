ff5de54876832a5d736d86fa369d0a36dc9cd574
serh11p <git@serh11p.com>
2020-10-22 21:20:06 +0300

    Fix OperationsBanner label

--- src/app/templates/OperationsBanner.tsx
+++ src/app/templates/OperationsBanner.tsx
@@ -5,55 +5,51 @@ import { t } from "lib/i18n/react";
 
 type OperationsBanner = {
   opParams: any[];
-  label?: string | null;
+  label?: React.ReactNode;
   className?: string;
 };
 
 const OperationsBanner = React.memo<OperationsBanner>(
-  ({ opParams, label, className }) => {
-    const labelWithFallback = label ?? t("operations");
-
-    return (
-      <>
-        {labelWithFallback && (
-          <h2
-            className={classNames(
-              "w-full mb-2",
-              "text-base font-semibold leading-tight",
-              "text-gray-700"
-            )}
-          >
-            {labelWithFallback}
-          </h2>
-        )}
-
-        <div
+  ({ opParams, label = t("operations"), className }) => (
+    <>
+      {label && (
+        <h2
           className={classNames(
-            "block w-full max-w-full mb-2 p-1",
-            "rounded-md overflow-auto",
-            "border-2 bg-gray-100 bg-opacity-50",
-            "text-base leading-tight font-medium whitespace-no-wrap",
-            className
+            "w-full mb-2",
+            "text-base font-semibold leading-tight",
+            "text-gray-700"
           )}
-          style={{
-            height: "10rem",
-          }}
         >
-          <ReactJson
-            src={opParams}
-            name={null}
-            iconStyle="square"
-            indentWidth={4}
-            collapsed={false}
-            collapseStringsAfterLength={36}
-            enableClipboard={false}
-            displayObjectSize={false}
-            displayDataTypes={false}
-          />
-        </div>
-      </>
-    );
-  }
+          {label}
+        </h2>
+      )}
+
+      <div
+        className={classNames(
+          "block w-full max-w-full mb-2 p-1",
+          "rounded-md overflow-auto",
+          "border-2 bg-gray-100 bg-opacity-50",
+          "text-base leading-tight font-medium whitespace-no-wrap",
+          className
+        )}
+        style={{
+          height: "10rem",
+        }}
+      >
+        <ReactJson
+          src={opParams}
+          name={null}
+          iconStyle="square"
+          indentWidth={4}
+          collapsed={false}
+          collapseStringsAfterLength={36}
+          enableClipboard={false}
+          displayObjectSize={false}
+          displayDataTypes={false}
+        />
+      </div>
+    </>
+  )
 );
 
 export default OperationsBanner;
