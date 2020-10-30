b417be5ff356eef75d216e21a87306e0b96c066b
Inokentii Mazhara <keshan3262@gmail.com>
2020-10-30 14:19:32 +0200

    Fix redirection to main page after attempt to refresh 'Import Managed KT account' page if some contracts are added

--- src/app/pages/ImportAccount.tsx
+++ src/app/pages/ImportAccount.tsx
@@ -6,11 +6,11 @@ import { Link, navigate } from "lib/woozie";
 import { T, t } from "lib/i18n/react";
 import {
   useThanosClient,
-  useRelevantAccounts,
   useSetAccountPkh,
   validateDerivationPath,
   useTezos,
   ActivationStatus,
+  useAllAccounts,
 } from "lib/thanos/front";
 import useSafeState from "lib/ui/useSafeState";
 import { MNEMONIC_ERROR_CAPTION, formatMnemonic } from "app/defaults";
@@ -27,7 +27,7 @@ type ImportAccountProps = {
 };
 
 const ImportAccount: React.FC<ImportAccountProps> = ({ tabSlug }) => {
-  const allAccounts = useRelevantAccounts();
+  const allAccounts = useAllAccounts();
   const setAccountPkh = useSetAccountPkh();
 
   const prevAccLengthRef = React.useRef(allAccounts.length);
