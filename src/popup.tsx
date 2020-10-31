9a756ce85cbe7725814d63f3af7a806a89daf97f
serh11p <git@serh11p.com>
2020-10-31 17:35:59 +0200

    Implement logic for configurable popup mode

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,6 +3,7 @@ import "./main.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { browser } from "webextension-polyfill-ts";
+import { isPopupModeEnabled } from "lib/popup-mode";
 import { WindowType, openInFullPage } from "app/env";
 import App from "app/App";
 
@@ -12,7 +13,7 @@ ReactDOM.render(
 );
 
 const popups = browser.extension.getViews({ type: "popup" });
-if (!popups.includes(window)) {
+if (!popups.includes(window) || !isPopupModeEnabled()) {
   openInFullPage();
   window.close();
 }
