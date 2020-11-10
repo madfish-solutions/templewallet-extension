d77f1b7c257109d8cd00696c5c9e7ee25f65e20a
serh11p <git@serh11p.com>
2020-11-10 16:10:07 +0200

    Integrate lambda view with new prerelease Taquito

--- src/lib/thanos/contract.ts
+++ src/lib/thanos/contract.ts
@@ -1,11 +1,17 @@
 import memoize from "micro-memoize";
-import { TezosToolkit } from "@taquito/taquito";
+import { TezosToolkit, WalletContract } from "@taquito/taquito";
 
 export const loadContract = memoize(fetchContract, {
   isPromise: true,
   maxSize: 100,
 });
 
-export function fetchContract(tezos: TezosToolkit, address: string) {
-  return tezos.wallet.at(address);
+export function fetchContract(
+  tezos: TezosToolkit,
+  address: string,
+  walletAPI = true
+): Promise<WalletContract> {
+  return walletAPI
+    ? tezos.wallet.at(address)
+    : (tezos.contract.at(address) as any);
 }
