2eb24284e3182e8bf22cadfe7630971e0e88a34e
serh11p <git@serh11p.com>
2020-11-11 19:52:10 +0200

    Improve code style

--- src/lib/thanos/front/tzdns.ts
+++ src/lib/thanos/front/tzdns.ts
@@ -1,22 +1,19 @@
 import { TezosToolkit } from "@taquito/taquito";
-import {
-  TezosDomainsClient
-} from "@tezos-domains/client";
+import { TezosDomainsClient } from "@tezos-domains/client";
 import {
   DomainNameValidationResult,
   isTezosDomainsSupportedNetwork,
 } from "@tezos-domains/core";
 
 export function getClient(networkId: string, tezos: TezosToolkit) {
-  if (isTezosDomainsSupportedNetwork(networkId)) {
-    return new TezosDomainsClient({ network: networkId, tezos });
-  }
-
-  return TezosDomainsClient.Unsupported;
+  return isTezosDomainsSupportedNetwork(networkId)
+    ? new TezosDomainsClient({ network: networkId, tezos })
+    : TezosDomainsClient.Unsupported;
 }
 
 export function isDomainNameValid(name: string, client: TezosDomainsClient) {
   return (
-    client.validator.validateDomainName(name) === DomainNameValidationResult.VALID
+    client.validator.validateDomainName(name) ===
+    DomainNameValidationResult.VALID
   );
 }
