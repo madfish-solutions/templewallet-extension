d502e1ec07659e1c789bf918d15f658eed8b8f65
Inokentii Mazhara <keshan3262@gmail.com>
2020-11-24 12:45:05 +0200

    Change expenses preview design

--- src/app/templates/ExpensesView.tsx
+++ src/app/templates/ExpensesView.tsx
@@ -1,15 +1,17 @@
+import classNames from "clsx";
 import React from "react";
 import {
   ThanosAsset,
   RawOperationExpenses,
   RawOperationAssetExpense,
+  XTZ_ASSET,
 } from "lib/thanos/front";
-import { T, t } from "lib/i18n/react";
-import CustomSelect, { OptionRenderProps } from "app/templates/CustomSelect";
+import { T, t, TProps } from "lib/i18n/react";
 import Money from "app/atoms/Money";
 import Identicon from "app/atoms/Identicon";
-import HashShortView from "app/atoms/HashShortView";
-import { getAssetIconUrl } from "app/defaults";
+import HashChip from "app/templates/HashChip";
+import InUSD from "app/templates/InUSD";
+import { ReactComponent as ClipboardIcon } from "app/icons/clipboard.svg";
 
 type OperationAssetExpense = Omit<RawOperationAssetExpense, "tokenAddress"> & {
   asset: ThanosAsset | string;
@@ -31,78 +33,232 @@ const ExpensesView: React.FC<ExpensesViewProps> = (props) => {
   }
 
   return (
-    <CustomSelect
-      items={expenses}
-      className="h-40"
-      OptionContent={ExpenseViewContent}
-    />
+    <div
+      className={classNames(
+        "rounded-md overflow-y-auto border-2 bg-gray-100",
+        "flex flex-col text-gray-700 text-sm leading-tight",
+        "h-40"
+      )}
+    >
+      {expenses.map((item, index) => (
+        <ExpenseViewItem item={item} key={index} />
+      ))}
+    </div>
   );
 };
 
-const ExpenseViewContent: React.FC<OptionRenderProps<OperationExpenses>> = ({
-  item,
-  index,
-}) => {
+export default ExpensesView;
+
+type ExpenseViewItemProps = {
+  item: OperationExpenses;
+};
+
+const ExpenseViewItem: React.FC<ExpenseViewItemProps> = ({ item }) => {
   const operationTypeLabel = React.useMemo(() => {
     switch (item.type) {
       // TODO: add translations for other operations types
       case "transaction":
       case "transfer":
-        return t("transfer");
+        return `↑ ${t("transfer")}`;
       case "approve":
         return t("approveToken");
       case "delegation":
-        return t("delegation");
+        return item.delegate ? t("delegation") : t("undelegation");
       default:
-        return item.isEntrypointInteraction
-          ? t("interactionWithSomeEntrypoint", [
-              item.type,
-              <HashShortView hash={item.contractAddress!} />,
-            ])
-          : t("transactionOfSomeType", item.type);
+        return item.isEntrypointInteraction ? (
+          <>
+            <ClipboardIcon className="mr-1 h-3 w-auto stroke-current inline align-text-top" />
+            <T id="interaction" />
+          </>
+        ) : (
+          t("transactionOfSomeType", item.type)
+        );
     }
   }, [item]);
+  const { iconHash, iconType, argumentDisplayProps } = React.useMemo<{
+    iconHash: string;
+    iconType: "bottts" | "jdenticon";
+    argumentDisplayProps?: OperationArgumentDisplayProps;
+  }>(() => {
+    const receivers = [
+      ...new Set(item.expenses.map(({ to }) => to).filter((value) => !!value)),
+    ];
+    switch (item.type) {
+      case "transaction":
+      case "transfer":
+        return {
+          iconHash: item.expenses[0]?.to || "unknown",
+          iconType: "bottts",
+          argumentDisplayProps: {
+            i18nKey: "transferToSmb",
+            arg: receivers,
+          },
+        };
+      case "approve":
+        return {
+          iconHash: item.expenses[0]?.to || "unknown",
+          iconType: "bottts",
+          argumentDisplayProps: {
+            i18nKey: "approveForSmb",
+            arg: receivers,
+          },
+        };
+      case "delegation":
+        if (item.delegate) {
+          return {
+            iconHash: item.delegate,
+            iconType: "bottts",
+            argumentDisplayProps: {
+              i18nKey: "delegationToSmb",
+              arg: [item.delegate],
+            },
+          };
+        }
+        return {
+          iconHash: "none",
+          iconType: "jdenticon",
+        };
+      default:
+        return item.isEntrypointInteraction
+          ? {
+              iconHash: item.contractAddress!,
+              iconType: "jdenticon",
+              argumentDisplayProps: {
+                i18nKey: "interactionWithContract",
+                arg: [item.contractAddress!],
+              },
+            }
+          : {
+              iconHash: "unknown",
+              iconType: "jdenticon",
+            };
+    }
+  }, [item]);
+
   return (
-    <>
-      <p className="text-xs text-gray-700">
-        {index + 1}. {operationTypeLabel}
-      </p>
-      <div className="flex flex-col">
-        {item.expenses.map(({ asset, amount }, index) => (
-          <div className="mt-2 flex flex-wrap items-center" key={index}>
-            {typeof asset === "string" ? (
-              <>
-                <Identicon className="h-8 w-auto mr-2" size={32} hash={asset} />
-                <span className="text-xl text-gray-700">
-                  <Money>{amount}</Money>{" "}
-                  <span className="text-base">
-                    (
-                    <T
-                      id="someUnknownToken"
-                      substitutions={<HashShortView hash={asset} />}
-                    />
-                    )
-                  </span>
-                </span>
-              </>
-            ) : (
-              <>
-                <img
-                  className="h-8 w-auto mr-2"
-                  alt={asset.symbol}
-                  src={getAssetIconUrl(asset)}
-                />
-                <span className="text-xl text-gray-700">
-                  <Money>{amount.div(10 ** asset.decimals)}</Money>{" "}
-                  <span className="text-base">{asset.symbol}</span>
-                </span>
-              </>
+    <div className="my-3 px-2 flex items-stretch">
+      <div className="mr-2">
+        <Identicon
+          hash={iconHash}
+          type={iconType}
+          size={50}
+          className="shadow-xs"
+        />
+      </div>
+
+      <div className="flex-1">
+        <div className="flex items-center">
+          <div className={classNames("flex-1", "h-px", "bg-gray-200")} />
+        </div>
+
+        <div className="flex items-stretch">
+          <div className="flex flex-col">
+            <div className="flex items-center mt-1 text-xs text-blue-600 opacity-75">
+              {operationTypeLabel}
+            </div>
+            {argumentDisplayProps && (
+              <OperationArgumentDisplay {...argumentDisplayProps} />
             )}
           </div>
-        ))}
+
+          <div className="flex-1" />
+          <div className="flex flex-col items-end flex-shrink-0">
+            {item.expenses.map((expense, index) => (
+              <OperationVolumeDisplay
+                key={index}
+                expense={expense}
+                operationType={item.type}
+                volume={item.amount}
+              />
+            ))}
+            {item.expenses.length === 0 && (item.amount || undefined) && (
+              <OperationVolumeDisplay
+                volume={item.amount!}
+                operationType={item.type}
+              />
+            )}
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ExpensesView;
+type OperationArgumentDisplayProps = {
+  i18nKey: TProps["id"];
+  arg: string[];
+};
+
+const OperationArgumentDisplay = React.memo<OperationArgumentDisplayProps>(
+  ({ i18nKey, arg }) => (
+    <span className="font-light text-gray-500 text-xs">
+      <T
+        id={i18nKey}
+        substitutions={arg.map((value, index) => (
+          <>
+            <HashChip
+              className="text-blue-600 opacity-75"
+              key={index}
+              hash={value}
+              type="link"
+            />
+            {index === arg.length - 1 ? null : ", "}
+          </>
+        ))}
+      />
+    </span>
+  )
+);
+
+type OperationVolumeDisplayProps = {
+  expense?: OperationAssetExpense;
+  volume?: number;
+  operationType: string;
+};
+
+const OperationVolumeDisplay: React.FC<OperationVolumeDisplayProps> = (
+  props
+) => {
+  const { expense, volume, operationType } = props;
+
+  const asset = typeof expense?.asset === "object" ? expense.asset : undefined;
+
+  const finalVolume = expense
+    ? expense.amount.div(10 ** (asset?.decimals || 0))
+    : volume;
+
+  return (
+    <>
+      <div
+        className={classNames(
+          "text-sm",
+          (() => {
+            switch (operationType) {
+              case "transaction":
+              case "transfer":
+                return "text-red-700";
+              case "approve":
+                return "text-yellow-600";
+              default:
+                return "text-gray-800";
+            }
+          })()
+        )}
+      >
+        <Money>{finalVolume || 0}</Money>{" "}
+        {expense?.asset ? asset?.symbol || "???" : "ꜩ"}
+      </div>
+
+      {(!expense?.asset || asset) && (
+        <InUSD volume={finalVolume || 0} asset={asset || XTZ_ASSET}>
+          {(usdVolume) => (
+            <div className="text-xs text-gray-500">
+              <span className="mr-px">$</span>
+              {usdVolume}
+            </div>
+          )}
+        </InUSD>
+      )}
+    </>
+  );
+};
