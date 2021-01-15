ae6748918b3e104762b0c6a289bf6522cd3abea0
serh11p <git@serh11p.com>
2021-01-15 16:38:49 +0200

    Fix expenses view

--- src/lib/thanos/front/tokens.ts
+++ src/lib/thanos/front/tokens.ts
@@ -13,14 +13,26 @@ import {
   DELPHINET_TOKENS,
 } from "lib/thanos/front";
 import { t } from "lib/i18n/react";
+import { useAllNetworks } from "./ready";
 
 const NETWORK_TOKEN_MAP = new Map([
   ["mainnet", MAINNET_TOKENS],
   ["delphinet", DELPHINET_TOKENS],
 ]);
 
-export function useTokens() {
-  const network = useNetwork();
+export function useTokens(networkRpc?: string) {
+  const allNetworks = useAllNetworks();
+  const selectedNetwork = useNetwork();
+
+  const network = React.useMemo(() => {
+    if (!networkRpc) return selectedNetwork;
+    return (
+      allNetworks.find(
+        (n) => n.rpcBaseURL === networkRpc || n.id === networkRpc
+      ) ?? selectedNetwork
+    );
+  }, [allNetworks, selectedNetwork, networkRpc]);
+
   const [tokensPure, setTokens] = useStorage<ThanosToken[]>(
     getTokensSWRKey(network.id),
     []
