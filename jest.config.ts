2236597b5f0a66859df30a59dd716a2888a61004
Michael Pautov <mpautov@deem.com>
2021-02-23 17:34:00 +0600

    feat(jest): introduced jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  clearMocks: true,
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    '^lib/(.*)$': '<rootDir>/src/lib/$1'
+  },
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js"],
+};
---
+++ src/lib/thanos/back/store.test.ts
@@ -0,0 +1,11 @@
+import "mockzilla-webextension";
+import { store } from './store'
+
+describe("Web-Extension Helpers", () => {
+
+  it('should ...', async () => {
+    const test = store
+    console.log(test);
+  })
+
+})
