9207d58478745580b0b5f2edfd13203f3a8e4014
serh11p <git@serh11p.com>
2021-03-10 13:12:16 +0200

    Fix Vault init test

--- src/lib/temple/back/vault.test.ts
+++ src/lib/temple/back/vault.test.ts
@@ -2,99 +2,61 @@ import { generateKey } from "../passworder";
 import { Vault } from "./vault";
 import { inited } from "./store";
 
-let passKey: CryptoKey
-let vault: Vault
-const password = "Test123!"
-const mnemonic = "champion lumber erupt shy hat smooth sound great spin cliff dolphin stuff"
-const accountName = "Temple"
-
+// let passKey: CryptoKey;
+// let vault: Vault;
+const password = "Test123!";
+const mnemonic =
+  "champion lumber erupt shy hat smooth sound great spin cliff dolphin stuff";
+const accountName = "Temple";
 
 describe("Safe Storage tests", () => {
-
   beforeAll(async () => {
-    passKey = await generateKey("passKey")
-    vault = new Vault(passKey)
-    inited(true);
-  })
+    // passKey = await generateKey("passKey")
+    // inited(true);
+  });
 
   it("init test", async () => {
     await Vault.spawn(password, mnemonic);
-    const accounts = await vault.createHDAccount(accountName)
-    console.log(accounts);
-
+    const vault = await Vault.setup(password);
+    const accounts = await vault.createHDAccount(accountName);
+    expect(Array.isArray(accounts)).toBeTruthy();
   });
 
-  it("isExist test", async () => {
-  });
+  it("isExist test", async () => {});
 
-  it("setup test", async () => {
-  });
+  it("setup test", async () => {});
 
-  it("spawn test", async () => {
-  });
+  it("spawn test", async () => {});
 
-  it("runMigrations test", async () => {
+  it("runMigrations test", async () => {});
 
-  });
+  it("revealMnemonic test", async () => {});
 
-  it("revealMnemonic test", async () => {
+  it("revealPrivateKey test", async () => {});
 
-  });
+  it("removeAccount test", async () => {});
 
-  it("revealPrivateKey test", async () => {
+  it("revealPublicKey test", async () => {});
 
-  });
+  it("fetchAccounts test", async () => {});
 
-  it("removeAccount test", async () => {
+  it("createHDAccount test", async () => {});
 
-  });
+  it("importAccount test", async () => {});
 
-  it("revealPublicKey test", async () => {
+  it("importMnemonicAccount test", async () => {});
 
-  });
+  it("importFundraiserAccount test", async () => {});
 
-  it("fetchAccounts test", async () => {
+  it("importManagedKTAccount test", async () => {});
 
-  });
+  it("importWatchOnlyAccount test", async () => {});
 
-  it("createHDAccount test", async () => {
+  it("editAccountName test", async () => {});
 
-  });
+  it("updateSettings test", async () => {});
 
-  it("importAccount test", async () => {
+  it("sign test", async () => {});
 
-  });
-
-  it("importMnemonicAccount test", async () => {
-
-  });
-
-  it("importFundraiserAccount test", async () => {
-
-  });
-
-  it("importManagedKTAccount test", async () => {
-
-  });
-
-  it("importWatchOnlyAccount test", async () => {
-
-  });
-
-  it("editAccountName test", async () => {
-
-  });
-
-  it("updateSettings test", async () => {
-
-  });
-
-  it("sign test", async () => {
-
-  });
-
-  it("importWatchOnlyAccount test", async () => {
-
-  });
-
+  it("importWatchOnlyAccount test", async () => {});
 });
