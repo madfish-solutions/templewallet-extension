3a38b42aa231c6ec818bbf736c814737676c278b
Andrii Ivaniv <unixvb@gmail.com>
2021-03-11 18:21:25 +0200

    setting screen testIDs added

---
+++ src/app/pages/Settings.selectors.ts
@@ -0,0 +1,10 @@
+export enum SettingsSelectors {
+  GeneralButton = 'SettingsSelectors.GeneralButton',
+  RevealPrivateKeyButton = 'SettingsSelectors.RevealPrivateKeyButton',
+  RevealSeedPhraseButton = 'SettingsSelectors.RevealSeedPhraseButton',
+  DAppsButton = 'SettingsSelectors.DAppsButton',
+  NetworksButton = 'SettingsSelectors.NetworksButton',
+  ActivateAccountButton = 'SettingsSelectors.ActivateAccountButton',
+  RemoveAccountButton = 'SettingsSelectors.RemoveAccountButton',
+  AboutButton = 'SettingsSelectors.AboutButton'
+}
