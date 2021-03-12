41f6c7b4462a02fe5e9d6f5ace74bceaeb20a0f4
Andrii Ivaniv <unixvb@gmail.com>
2021-03-12 13:22:05 +0200

    tracking DApp events

---
+++ src/app/ConfirmPage.selectors.ts
@@ -0,0 +1,10 @@
+export enum ConfirmPageSelectors {
+  ConnectAction_CancelButton = 'ConfirmPageSelectors.ConnectAction_CancelButton',
+  ConnectAction_RetryButton = 'ConfirmPageSelectors.ConnectAction_RetryButton',
+  ConnectAction_ConnectButton = 'ConfirmPageSelectors.ConnectAction_ConnectButton',
+  ConfirmOperationsAction_RejectButton = 'ConfirmPageSelectors.ConfirmOperationsAction_RejectButton',
+  ConfirmOperationsAction_RetryButton = 'ConfirmPageSelectors.ConfirmOperationsAction_RetryButton',
+  ConfirmOperationsAction_ConfirmButton = 'ConfirmPageSelectors.ConfirmOperationsAction_ConfirmButton',
+  SignAction_RejectButton = 'ConfirmPageSelectors.SignAction_RejectButton',
+  SignAction_SignButton = 'ConfirmPageSelectors.SignAction_SignButton',
+}
