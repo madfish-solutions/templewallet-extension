9a985d99a490ba43cc65ce8311699607556b1507
serh11p <git@serh11p.com>
2021-03-15 20:10:06 +0200

    Fix AddUnknownTokens

--- src/lib/better-call-dev/account.ts
+++ src/lib/better-call-dev/account.ts
@@ -41,12 +41,6 @@ export interface BcdAccountInfo {
 export interface BcdAccountToken {
   balance: number;
   contract: string;
-  decimals?: number;
-  level?: number;
-  name?: string;
   network: string;
-  symbol?: string;
   token_id: number;
-  token_info?: any;
-  volume_24_hours?: number;
 }
