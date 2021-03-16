0f802d13503275974a4fb934a6049ec24ffc36ba
Andrii Ivaniv <unixvb@gmail.com>
2021-03-16 17:49:23 +0200

    router types fixed

--- src/lib/woozie/router.ts
+++ src/lib/woozie/router.ts
@@ -1,9 +1,10 @@
+import { FC } from "react";
 import regexparam from "regexparam";
 
 export type Path = string;
 export type Route = string;
 export type Params = { [key: string]: string | null };
-export type ResolveResult<C> = (params: Params, ctx: C) => any;
+export type ResolveResult<C> = (params: Params, ctx: C) => ReturnType<FC> | typeof SKIP;
 export type Pattern = RegExp;
 export type Keys = Array<string> | false;
 export type Routes<C> = Array<[Route, ResolveResult<C>]>;
@@ -15,7 +16,6 @@ export type RouteMap<C> = Array<{
 }>;
 
 export const SKIP = Symbol("Woozie.Router.Skip");
-export const NOT_FOUND = Symbol("Woozie.Router.NotFound");
 
 export function createMap<C>(routes: Routes<C>): RouteMap<C> {
   return routes.map(([route, resolveResult]) => {
@@ -29,7 +29,7 @@ export function createMap<C>(routes: Routes<C>): RouteMap<C> {
   });
 }
 
-export function resolve<C>(preparedRM: RouteMap<C>, path: Path, ctx: C): any {
+export function resolve<C>(preparedRM: RouteMap<C>, path: Path, ctx: C): ReturnType<FC> {
   for (const { resolveResult, pattern, keys } of preparedRM) {
     if (pattern.test(path)) {
       const params = createParams(path, pattern, keys);
@@ -40,7 +40,8 @@ export function resolve<C>(preparedRM: RouteMap<C>, path: Path, ctx: C): any {
     }
   }
 
-  return NOT_FOUND;
+  // Page not found
+  return null;
 }
 
 function createParams(path: Path, pattern: Pattern, keys: Keys): Params {
