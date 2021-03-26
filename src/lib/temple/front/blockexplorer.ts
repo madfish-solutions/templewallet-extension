23d107b4009b2de04348096b1eb532d9d214955e
Inokentii Mazhara <keshan3262@gmail.com>
2021-03-26 15:15:25 +0200

    Add fallbacking for the case a selected block explorer is unavailable for a chosen network

--- src/lib/temple/front/blockexplorer.ts
+++ src/lib/temple/front/blockexplorer.ts
@@ -1,10 +1,15 @@
-import { useMemo } from 'react';
+import { useMemo } from "react";
 
-import { isKnownChainId, TempleChainId, useChainId, useStorage } from "lib/temple/front";
+import {
+  isKnownChainId,
+  TempleChainId,
+  useChainId,
+  useStorage,
+} from "lib/temple/front";
 
 export type BlockExplorerId = "tzkt" | "tzstats" | "bcd" | "tezblock";
 
-type BaseUrls = { account?: string, transaction: string };
+type BaseUrls = { account?: string; transaction: string };
 
 export type BlockExplorer = {
   id: BlockExplorerId;
@@ -17,78 +22,120 @@ export const BLOCK_EXPLORERS: BlockExplorer[] = [
     id: "tzkt",
     name: "TzKT",
     baseUrls: new Map([
-      [TempleChainId.Mainnet, {
-        account: "https://tzkt.io",
-        transaction: "https://tzkt.io"
-      }],
-      [TempleChainId.Delphinet, {
-        account: "https://delphinet.tzkt.io",
-        transaction: "https://delphinet.tzkt.io"
-      }],
-      [TempleChainId.Edo2net, {
-        account: "https://edo2net.tzkt.io",
-        transaction: "https://edo2net.tzkt.io"
-      }],
-      [TempleChainId.Florencenet, {
-        account: "https://florencenet.tzkt.io",
-        transaction: "https://florencenet.tzkt.io"
-      }]
-    ])
+      [
+        TempleChainId.Mainnet,
+        {
+          account: "https://tzkt.io",
+          transaction: "https://tzkt.io",
+        },
+      ],
+      [
+        TempleChainId.Delphinet,
+        {
+          account: "https://delphinet.tzkt.io",
+          transaction: "https://delphinet.tzkt.io",
+        },
+      ],
+      [
+        TempleChainId.Edo2net,
+        {
+          account: "https://edo2net.tzkt.io",
+          transaction: "https://edo2net.tzkt.io",
+        },
+      ],
+      [
+        TempleChainId.Florencenet,
+        {
+          account: "https://florencenet.tzkt.io",
+          transaction: "https://florencenet.tzkt.io",
+        },
+      ],
+    ]),
   },
   {
     id: "tzstats",
     name: "TzStats",
     baseUrls: new Map([
-      [TempleChainId.Mainnet, {
-        account: "https://tzstats.com",
-        transaction: "https://tzstats.com",
-      }],
-      [TempleChainId.Delphinet, {
-        account: "https://delphi.tzstats.com",
-        transaction: "https://delphi.tzstats.com",
-      }],
-      [TempleChainId.Edo2net, {
-        account: "https://edo.tzstats.com",
-        transaction: "https://edo.tzstats.com",
-      }],
-      [TempleChainId.Florencenet, {
-        account: "https://florence.tzstats.com",
-        transaction: "https://florence.tzstats.com"
-      }]
-    ])
+      [
+        TempleChainId.Mainnet,
+        {
+          account: "https://tzstats.com",
+          transaction: "https://tzstats.com",
+        },
+      ],
+      [
+        TempleChainId.Delphinet,
+        {
+          account: "https://delphi.tzstats.com",
+          transaction: "https://delphi.tzstats.com",
+        },
+      ],
+      [
+        TempleChainId.Edo2net,
+        {
+          account: "https://edo.tzstats.com",
+          transaction: "https://edo.tzstats.com",
+        },
+      ],
+      [
+        TempleChainId.Florencenet,
+        {
+          account: "https://florence.tzstats.com",
+          transaction: "https://florence.tzstats.com",
+        },
+      ],
+    ]),
   },
   {
     id: "bcd",
     name: "Better Call Dev",
     baseUrls: new Map([
-      [TempleChainId.Mainnet, {
-        transaction: "https://better-call.dev/mainnet/opg",
-      }],
-      [TempleChainId.Delphinet, {
-        transaction: "https://better-call.dev/delphinet/opg",
-      }],
-      [TempleChainId.Edo2net, {
-        transaction: "https://better-call.dev/edo2net/opg",
-      }],
-      [TempleChainId.Florencenet, {
-        transaction: "https://better-call.dev/florencenet/opg",
-      }]
-    ])
+      [
+        TempleChainId.Mainnet,
+        {
+          transaction: "https://better-call.dev/mainnet/opg",
+        },
+      ],
+      [
+        TempleChainId.Delphinet,
+        {
+          transaction: "https://better-call.dev/delphinet/opg",
+        },
+      ],
+      [
+        TempleChainId.Edo2net,
+        {
+          transaction: "https://better-call.dev/edo2net/opg",
+        },
+      ],
+      [
+        TempleChainId.Florencenet,
+        {
+          transaction: "https://better-call.dev/florencenet/opg",
+        },
+      ],
+    ]),
   },
   {
     id: "tezblock",
     name: "tezblock",
     baseUrls: new Map([
-      [TempleChainId.Mainnet, {
-        account: "http://tezblock.io/account",
-        transaction: "http://tezblock.io/transaction"
-      }],
-      [TempleChainId.Delphinet, {
-        account: "http://delphinet.tezblock.io/account",
-        transaction: "http://delphinet.tezblock.io/transaction"
-      }]
-    ])
-  }
+      [
+        TempleChainId.Mainnet,
+        {
+          account: "http://tezblock.io/account",
+          transaction: "http://tezblock.io/transaction",
+        },
+      ],
+      [
+        TempleChainId.Delphinet,
+        {
+          account: "http://delphinet.tezblock.io/account",
+          transaction: "http://delphinet.tezblock.io/transaction",
+        },
+      ],
+    ]),
+  },
 ];
 
 const BLOCK_EXPLORER_STORAGE_KEY = "block_explorer";
@@ -104,20 +151,21 @@ export function useBlockExplorer() {
   );
   return {
     explorer,
-    setExplorerId
+    setExplorerId,
   };
 }
 
 export function useExplorerBaseUrls() {
   const chainId = useChainId();
   const { explorer } = useBlockExplorer();
-  return useMemo<Partial<BaseUrls>>(
-    () =>
-      (chainId &&
-        (isKnownChainId(chainId)
-          ? explorer.baseUrls.get(chainId)
-          : undefined)) ??
-      {},
-    [chainId, explorer]
-  );
+  return useMemo<Partial<BaseUrls>>(() => {
+    if (chainId && isKnownChainId(chainId)) {
+      const fallbackBaseUrls =
+        BLOCK_EXPLORERS.find((explorer) =>
+          explorer.baseUrls.get(chainId)
+        )?.baseUrls.get(chainId) ?? {};
+      return explorer.baseUrls.get(chainId) ?? fallbackBaseUrls;
+    }
+    return {};
+  }, [chainId, explorer]);
 }
