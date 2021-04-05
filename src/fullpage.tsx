04ffc40410f149a5e5d044fceb1d8e784f107526
serh11p <git@serh11p.com>
2021-04-05 18:15:29 +0300

    Rollback fullapge examples

--- src/fullpage.tsx
+++ src/fullpage.tsx
@@ -2,23 +2,10 @@ import "./main.css";
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Dexie from "dexie";
 import { WindowType } from "app/env";
 import App from "app/App";
-import { getOperations } from "lib/tzkt";
-import { getTokenTransfers } from "lib/better-call-dev";
-import * as Repo from "lib/temple/repo";
-import { syncOperations } from "lib/temple/activity";
 
 ReactDOM.render(
   <App env={{ windowType: WindowType.FullPage }} />,
   document.getElementById("root")
 );
-
-Object.assign(window as any, {
-  getOperations,
-  getTokenTransfers,
-  Dexie,
-  Repo,
-  syncOperations,
-});
