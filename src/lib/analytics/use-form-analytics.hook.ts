37245a8954d21043815eb4636341cecf28b00f42
serh11p <git@serh11p.com>
2021-04-07 17:51:57 +0300

    Fix UI issues with analytics

--- src/lib/analytics/use-form-analytics.hook.ts
+++ src/lib/analytics/use-form-analytics.hook.ts
@@ -1,16 +1,24 @@
+import { useMemo } from "react";
+
 import { AnalyticsEventCategory } from "./analytics-event.enum";
 import { useAnalytics } from "./use-analytics.hook";
 
 export const useFormAnalytics = (formName: string) => {
   const { trackEvent } = useAnalytics();
 
-  const trackSubmit = (properties?: object) => trackEvent(formName, AnalyticsEventCategory.FormSubmit, properties);
-  const trackSubmitSuccess = (properties?: object) => trackEvent(formName, AnalyticsEventCategory.FormSubmitSuccess, properties);
-  const trackSubmitFail = (properties?: object) => trackEvent(formName, AnalyticsEventCategory.FormSubmitFail, properties);
-
-  return {
-    trackSubmit,
-    trackSubmitSuccess,
-    trackSubmitFail
-  };
-}
+  return useMemo(
+    () => ({
+      trackSubmit: (properties?: object) =>
+        trackEvent(formName, AnalyticsEventCategory.FormSubmit, properties),
+      trackSubmitSuccess: (properties?: object) =>
+        trackEvent(
+          formName,
+          AnalyticsEventCategory.FormSubmitSuccess,
+          properties
+        ),
+      trackSubmitFail: (properties?: object) =>
+        trackEvent(formName, AnalyticsEventCategory.FormSubmitFail, properties),
+    }),
+    [formName, trackEvent]
+  );
+};
