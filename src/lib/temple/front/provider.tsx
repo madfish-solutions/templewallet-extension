941fc21c06e42588c2df545470ba8408a4e6d1b0
serh11p <git@serh11p.com>
2021-04-07 15:15:59 +0300

    Fix context nesting and event duplicating for analytics

--- src/lib/temple/front/provider.tsx
+++ src/lib/temple/front/provider.tsx
@@ -1,18 +1,22 @@
 import React, { FC, useMemo } from "react";
 
+import { CustomRpsContext } from "lib/analytics";
 import { useAssets } from "lib/temple/front/assets";
 import { NewBlockTriggersProvider } from "lib/temple/front/chain";
 import { TempleClientProvider, useTempleClient } from "lib/temple/front/client";
 import {
   ReadyTempleProvider,
   TempleRefsProvider,
+  useNetwork,
 } from "lib/temple/front/ready";
 import { USDPriceProvider } from "lib/temple/front/usdprice";
 
 export const TempleProvider: FC = ({ children }) => (
-  <TempleClientProvider>
-    <ConditionalReadyTemple>{children}</ConditionalReadyTemple>
-  </TempleClientProvider>
+  <CustomRpsContext.Provider value={undefined}>
+    <TempleClientProvider>
+      <ConditionalReadyTemple>{children}</ConditionalReadyTemple>
+    </TempleClientProvider>
+  </CustomRpsContext.Provider>
 );
 
 const ConditionalReadyTemple: FC = ({ children }) => {
@@ -22,13 +26,15 @@ const ConditionalReadyTemple: FC = ({ children }) => {
     () =>
       ready ? (
         <ReadyTempleProvider>
-          <TempleRefsProvider>
-            <USDPriceProvider>
-              <NewBlockTriggersProvider>
-                <PreloadAssetsProvider>{children}</PreloadAssetsProvider>
-              </NewBlockTriggersProvider>
-            </USDPriceProvider>
-          </TempleRefsProvider>
+          <WalletRpcProvider>
+            <TempleRefsProvider>
+              <USDPriceProvider>
+                <NewBlockTriggersProvider>
+                  <PreloadAssetsProvider>{children}</PreloadAssetsProvider>
+                </NewBlockTriggersProvider>
+              </USDPriceProvider>
+            </TempleRefsProvider>
+          </WalletRpcProvider>
         </ReadyTempleProvider>
       ) : (
         <>{children}</>
@@ -40,4 +46,14 @@ const ConditionalReadyTemple: FC = ({ children }) => {
 const PreloadAssetsProvider: FC = ({ children }) => {
   useAssets();
   return <>{children}</>;
+};
+
+const WalletRpcProvider: FC = ({ children }) => {
+  const network = useNetwork();
+
+  return (
+    <CustomRpsContext.Provider value={network.rpcBaseURL}>
+      {children}
+    </CustomRpsContext.Provider>
+  );
 };
