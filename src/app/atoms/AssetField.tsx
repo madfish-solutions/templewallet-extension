ff09ab24580d88b318ba4e7d18b9aa19c0b8e1fa
serh11p <git@serh11p.com>
2021-04-15 19:57:12 +0300

    Fix UI issue in AssetField

--- src/app/atoms/AssetField.tsx
+++ src/app/atoms/AssetField.tsx
@@ -53,7 +53,7 @@ const AssetField = forwardRef<HTMLInputElement, AssetFieldProps>(
     const handleChange = useCallback(
       (evt) => {
         let val = evt.target.value.replace(/ /g, "").replace(/,/g, ".");
-        let numVal = new BigNumber(val);
+        let numVal = new BigNumber(val || 0);
         const indexOfDot = val.indexOf(".");
         if (indexOfDot !== -1 && val.length - indexOfDot > assetDecimals + 1) {
           val = val.substring(0, indexOfDot + assetDecimals + 1);
