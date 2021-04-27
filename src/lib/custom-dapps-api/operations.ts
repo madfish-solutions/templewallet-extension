d4ef64b67fe46d74bd5746ba5c4a47ce08400eba
Inokentii Mazhara <keshan3262@gmail.com>
2021-04-27 15:30:44 +0300

    Update API route

--- src/lib/custom-dapps-api/operations.ts
+++ src/lib/custom-dapps-api/operations.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 import { CustomDAppsInfo } from "./types";
 
 export function getDApps() {
-  return axios.get<CustomDAppsInfo>("http://104.236.217.192")
+  return axios.get<CustomDAppsInfo>("http://104.236.217.192/api/dapps")
     .then(res => res.data);
 }
