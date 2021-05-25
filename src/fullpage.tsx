52dd1075594dc429dfa74128b3d31c3dde072f9a
serh11p <git@serh11p.com>
2021-05-25 13:43:35 +0300

    Remove dev stuff

--- src/fullpage.tsx
+++ src/fullpage.tsx
@@ -6,21 +6,8 @@ import * as ReactDOM from "react-dom";
 
 import App from "app/App";
 import { WindowType } from "app/env";
-import {
-  syncOperations,
-  fetchOperations,
-  isSyncSupported,
-} from "lib/temple/activity";
-import * as Repo from "lib/temple/repo";
 
 ReactDOM.render(
   <App env={{ windowType: WindowType.FullPage }} />,
   document.getElementById("root")
 );
-
-Object.assign(window as any, {
-  syncOperations,
-  fetchOperations,
-  Repo,
-  isSyncSupported,
-});
