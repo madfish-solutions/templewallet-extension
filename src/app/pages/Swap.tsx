8578f7e96213b66f20b99ab1c92278c5865f682c
serh11p <git@serh11p.com>
2021-06-29 18:03:21 +0300

    Add Disclaimer on Swap apge

--- src/app/pages/Swap.tsx
+++ src/app/pages/Swap.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useMemo } from "react";
 
+import classNames from "clsx";
+
+import { ReactComponent as InfoIcon } from "app/icons/info.svg";
 import { ReactComponent as SwapIcon } from "app/icons/swap.svg";
 import PageLayout from "app/layouts/PageLayout";
 import SwapForm from "app/templates/SwapForm";
 import { SwappableAssetsProvider } from "app/templates/SwapForm/useSwappableAssets";
-import { t } from "lib/i18n/react";
+import { t, T } from "lib/i18n/react";
+import useTippy from "lib/ui/useTippy";
 
 type SwapProps = {
   assetSlug?: string | null;
@@ -20,6 +24,8 @@ const Swap: React.FC<SwapProps> = ({ assetSlug }) => (
   >
     <div className="py-4">
       <div className="w-full max-w-sm mx-auto">
+        <SwapDisclaimer />
+
         <SwappableAssetsProvider initialAssetKey={assetSlug ?? undefined}>
           <SwapForm assetSlug={assetSlug} />
         </SwappableAssetsProvider>
@@ -29,3 +35,42 @@ const Swap: React.FC<SwapProps> = ({ assetSlug }) => (
 );
 
 export default Swap;
+
+const SwapDisclaimer: React.FC = () => {
+  const tippyProps = useMemo(
+    () => ({
+      trigger: "mouseenter",
+      hideOnClick: false,
+      content: t("swapDisclaimerContent"),
+      animation: "shift-away-subtle",
+    }),
+    []
+  );
+  const tippyRef = useTippy<HTMLSpanElement>(tippyProps);
+
+  return (
+    <div className="mb-4 flex items-center">
+      <span
+        ref={tippyRef}
+        className={classNames(
+          "inline-flex items-center",
+          "-ml-1 p-1",
+          "cursor-default",
+          "text-xs",
+          "rounded-sm",
+          "text-gray-600 hover:bg-gray-100"
+        )}
+      >
+        <T id="swapDisclaimerTitle" />
+        <InfoIcon
+          style={{
+            width: "0.625rem",
+            height: "auto",
+            marginLeft: "0.125rem",
+          }}
+          className="stroke-current"
+        />
+      </span>
+    </div>
+  );
+};
