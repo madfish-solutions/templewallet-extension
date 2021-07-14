dd6b78fdc10cd514d4ac75bc3be3b907e9e9f4d7
serh11p <git@serh11p.com>
2021-07-14 17:35:33 +0300

    Implement verify Seed Phrase step

--- src/app/templates/NewWallet.tsx
+++ src/app/templates/NewWallet.tsx
@@ -19,6 +19,9 @@ import { useTempleClient } from "lib/temple/front";
 import { useAlert } from "lib/ui/dialog";
 import { Link } from "lib/woozie";
 
+import Backup from "./NewWallet/Backup";
+import Verify from "./NewWallet/Verify";
+
 interface FormData {
   mnemonic?: string;
   password: string;
@@ -36,10 +39,7 @@ type NewWalletProps = {
   title: string;
 };
 
-const NewWallet: FC<NewWalletProps> = ({
-  ownMnemonic = false,
-  title,
-}) => {
+const NewWallet: FC<NewWalletProps> = ({ ownMnemonic = false, title }) => {
   const { locked, registerWallet, setSeedRevealed } = useTempleClient();
   const alert = useAlert();
 
@@ -62,6 +62,7 @@ const NewWallet: FC<NewWalletProps> = ({
   }, [triggerValidation, formState.dirtyFields, passwordValue]);
 
   const [backupData, setBackupData] = useState<BackupData | null>(null);
+  const [verifySeedPhrase, setVerifySeedPhrase] = useState(false);
 
   const onSubmit = useCallback(
     async (data: FormData) => {
@@ -98,6 +99,10 @@ const NewWallet: FC<NewWalletProps> = ({
     ]
   );
 
+  const handleBackupComplete = useCallback(() => {
+    setVerifySeedPhrase(true);
+  }, [setVerifySeedPhrase]);
+
   return (
     <div className="py-4">
       <h1
@@ -106,7 +111,11 @@ const NewWallet: FC<NewWalletProps> = ({
           "text-2xl font-light text-gray-700 text-center"
         )}
       >
-        {backupData ? t("backupNewSeedPhrase") : title}
+        {backupData
+          ? verifySeedPhrase
+            ? t("verifySeedPhrase")
+            : t("backupNewSeedPhrase")
+          : title}
       </h1>
 
       <hr className="my-4" />
@@ -254,105 +263,13 @@ const NewWallet: FC<NewWalletProps> = ({
             )}
           </T>
         </form>
+      ) : verifySeedPhrase ? (
+        <Verify data={backupData} />
       ) : (
-        <Backup data={backupData} />
+        <Backup data={backupData} onBackupComplete={handleBackupComplete} />
       )}
     </div>
   );
 };
 
 export default NewWallet;
-
-interface BackupFormData {
-  backuped: boolean;
-}
-
-type BackupProps = {
-  data: BackupData;
-};
-
-const Backup: FC<BackupProps> = ({ data }) => {
-  const { registerWallet, setSeedRevealed } = useTempleClient();
-
-  const {
-    register,
-    handleSubmit,
-    errors,
-    formState,
-  } = useForm<BackupFormData>();
-  const submitting = formState.isSubmitting;
-
-  const onSubmit = useCallback(async () => {
-    if (submitting) return;
-
-    try {
-      await registerWallet(data.password, data.mnemonic);
-      setSeedRevealed(true);
-    } catch (err) {
-      if (process.env.NODE_ENV === "development") {
-        console.error(err);
-      }
-
-      alert(err.message);
-    }
-  }, [
-    submitting,
-    registerWallet,
-    setSeedRevealed,
-    data.password,
-    data.mnemonic,
-  ]);
-
-  return (
-    <div className="w-full max-w-sm mx-auto my-8">
-      <Alert
-        title={""}
-        description={
-          <>
-            <p>
-              <T id="revealNewSeedPhrase" />
-            </p>
-
-            <p className="mt-1">
-              <T id="doNotSharePhrase" />
-            </p>
-          </>
-        }
-        className="mt-4 mb-8"
-      />
-
-      <FormField
-        secret
-        textarea
-        rows={4}
-        readOnly
-        label={t("mnemonicInputLabel")}
-        labelDescription={t("youWillNeedThisSeedPhrase")}
-        id="backup-mnemonic"
-        spellCheck={false}
-        containerClassName="mb-4"
-        className="resize-none notranslate"
-        value={data.mnemonic}
-      />
-
-      <form className="w-full mt-8" onSubmit={handleSubmit(onSubmit)}>
-        <FormCheckbox
-          ref={register({
-            validate: (val) => val || t("unableToContinueWithoutConfirming"),
-          })}
-          errorCaption={errors.backuped?.message}
-          name="backuped"
-          label={t("backupedInputLabel")}
-          labelDescription={<T id="backupedInputDescription" />}
-          containerClassName="mb-6"
-        />
-
-        <T id="continue">
-          {(message) => (
-            <FormSubmitButton loading={submitting}>{message}</FormSubmitButton>
-          )}
-        </T>
-      </form>
-    </div>
-  );
-};
---
+++ src/app/templates/NewWallet/Verify.tsx
@@ -0,0 +1,186 @@
+import React, { FC, memo, useCallback, useMemo, useState } from "react";
+
+import classNames from "clsx";
+import { useForm } from "react-hook-form";
+
+import FormField from "app/atoms/FormField";
+import FormSubmitButton from "app/atoms/FormSubmitButton";
+import { T } from "lib/i18n/react";
+import { useTempleClient } from "lib/temple/front";
+
+type VerifyProps = {
+  data: {
+    mnemonic: string;
+    password: string;
+  };
+};
+
+const WORDS_TO_FILL = 2;
+
+const Verify: FC<VerifyProps> = ({ data }) => {
+  const { registerWallet, setSeedRevealed } = useTempleClient();
+
+  const words = useMemo(() => data.mnemonic.split(" "), [data.mnemonic]);
+  const indexesToFill = useMemo(() => {
+    const indexes: number[] = [];
+    for (let i = 0; i < WORDS_TO_FILL; i++) {
+      while (true) {
+        const index = getRandomInt(0, words.length - 1);
+        const twoNearIndexes = getTwoNearIndexes(index, words.length);
+        if ([index, ...twoNearIndexes].every((ni) => !indexes.includes(ni))) {
+          indexes.push(index);
+          break;
+        }
+      }
+    }
+    return sortNumbers(indexes);
+  }, [words]);
+
+  const [filledIndexes, setFilledIndexes] = useState<number[]>([]);
+
+  const handleFill = useCallback(
+    (index: number, filled: boolean) => {
+      setFilledIndexes((fi) =>
+        filled
+          ? fi.includes(index)
+            ? fi
+            : [...fi, index]
+          : fi.filter((i) => i !== index)
+      );
+    },
+    [setFilledIndexes]
+  );
+
+  const filled = useMemo(
+    () => indexesToFill.every((i) => filledIndexes.includes(i)),
+    [indexesToFill, filledIndexes]
+  );
+
+  const { handleSubmit, formState } = useForm();
+  const submitting = formState.isSubmitting;
+
+  const onSubmit = useCallback(async () => {
+    if (submitting || !filled) return;
+
+    try {
+      await registerWallet(data.password, data.mnemonic);
+      setSeedRevealed(true);
+    } catch (err) {
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
+
+      alert(err.message);
+    }
+  }, [
+    filled,
+    submitting,
+    registerWallet,
+    setSeedRevealed,
+    data.password,
+    data.mnemonic,
+  ]);
+
+  return (
+    <div className="w-full max-w-md mx-auto my-8">
+      <form className="w-full mt-8" onSubmit={handleSubmit(onSubmit)}>
+        <h3
+          className={classNames(
+            "mt-2 mb-8",
+            "text-gray-600 text-xl font-light",
+            "text-center"
+          )}
+        >
+          <T id="verifySeedPhraseDescription" />
+        </h3>
+
+        <div className="mb-8 flex flex-col">
+          {indexesToFill.map((indexToFill, i) => (
+            <WordsRow
+              key={i}
+              allWords={words}
+              indexToFill={indexToFill}
+              onFill={(filled) => handleFill(indexToFill, filled)}
+            />
+          ))}
+        </div>
+
+        <FormSubmitButton loading={submitting} disabled={!filled}>
+          <T id="finish" />
+        </FormSubmitButton>
+      </form>
+    </div>
+  );
+};
+
+export default Verify;
+
+type WordsRowProps = {
+  allWords: string[];
+  indexToFill: number;
+  onFill: (filled: boolean) => void;
+};
+
+const WordsRow = memo<WordsRowProps>(({ allWords, indexToFill, onFill }) => {
+  const nearIndexes = useMemo(
+    () => getTwoNearIndexes(indexToFill, allWords.length),
+    [indexToFill, allWords.length]
+  );
+  const indexes = useMemo(
+    () => sortNumbers([indexToFill, ...nearIndexes]),
+    [indexToFill, nearIndexes]
+  );
+  const [fillValue, setFillValue] = useState("");
+
+  const handleChange = useCallback(
+    (evt) => {
+      const { value } = evt.target;
+      setFillValue(value);
+      onFill(value === allWords[indexToFill]);
+    },
+    [setFillValue, onFill, allWords, indexToFill]
+  );
+
+  return (
+    <div className={classNames("mb-6", "-mx-2", "flex items-stretch")}>
+      {indexes.map((i) => {
+        const toFill = i === indexToFill;
+
+        return (
+          <div key={i} className="p-2">
+            <FormField
+              label={<T id="word" substitutions={i + 1} />}
+              {...(toFill
+                ? {
+                    value: fillValue,
+                    onChange: handleChange,
+                  }
+                : {
+                    readOnly: true,
+                    defaultValue: allWords[i],
+                  })}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+});
+
+function getTwoNearIndexes(index: number, limit: number) {
+  return index === 0
+    ? [1, 2]
+    : index === limit - 1
+    ? [limit - 2, limit - 3]
+    : [index - 1, index + 1];
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sortNumbers(arr: number[]) {
+  return arr.sort((a, b) => a - b);
+}
