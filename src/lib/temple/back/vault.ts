7b49e33d0440edc8209578fd8208c037d0e1aac8
serh11p <git@serh11p.com>
2021-08-03 15:27:09 +0300

    Fix back script

--- src/lib/temple/back/vault.ts
+++ src/lib/temple/back/vault.ts
@@ -17,7 +17,7 @@ import * as Ed25519 from "ed25519-hd-key";
 import { browser } from "webextension-polyfill-ts";
 
 import { getMessage } from "lib/i18n";
-import { mergeAssets } from "lib/temple/assets";
+import { mergeAssets } from "lib/temple/assets/legacy";
 import { PublicError } from "lib/temple/back/defaults";
 import { TempleLedgerSigner } from "lib/temple/back/ledger-signer";
 import {
