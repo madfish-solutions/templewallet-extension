4084bee5bb9a5e8ed7f410a6ebfd3cc6dedbe8c0
Innokentiy Mazhara <keshan3262@gmail.com>
2021-08-06 13:20:16 +0300

    Add swapping with liquidity baking contract (#375)
    * Implement swapping with liquidity baking contract
    
    * 'Swap' page: Override hardcoded tokens metadata with one from async storage
    
    * Add mainnet contracts for Liquidity Baking
    
    Co-authored-by: serh11p <git@serh11p.com>

--- src/lib/temple/front/swap.ts
+++ src/lib/temple/front/swap.ts
@@ -4,7 +4,7 @@ import BigNumber from "bignumber.js";
 import { batchify, loadContract, withTokenApprove } from "lib/temple/front";
 import { TempleAsset, TempleAssetType, TempleChainId } from "lib/temple/types";
 
-export type ExchangerType = "dexter" | "quipuswap";
+export type ExchangerType = "dexter" | "quipuswap" | "liquidity_baking";
 
 type SwapContractDescriptor = {
   type: ExchangerType;
@@ -25,7 +25,11 @@ export type SwapParams = {
   tezos: TezosToolkit;
 };
 
-export const ALL_EXCHANGERS_TYPES: ExchangerType[] = ["dexter", "quipuswap"];
+export const ALL_EXCHANGERS_TYPES: ExchangerType[] = [
+  "dexter",
+  "quipuswap",
+  "liquidity_baking",
+];
 
 export const EXCHANGE_XTZ_RESERVE = new BigNumber("0.3");
 
@@ -62,6 +66,28 @@ export const DEXTER_EXCHANGE_CONTRACTS = new Map<
       },
       KT1FCMQk44tEP9fm9n5JJEhkSk1TW3XQdaWH: {
         0: "KT1RfTPvrfAQDGAJ7wB71EtwxLQgjmfz59kE",
+      },
+    },
+  ],
+]);
+
+export const LIQUIDITY_BAKING_CONTRACTS = new Map<
+  string,
+  Record<string, Record<number, string>>
+>([
+  [
+    TempleChainId.Mainnet,
+    {
+      KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn: {
+        0: "KT1TxqZ8QtKvLu3V3JH7Gx58n7Co8pgtpQU5",
+      },
+    },
+  ],
+  [
+    TempleChainId.Granadanet,
+    {
+      KT1VqarPDicMFn1ejmQqqshUkUXTCTXwmkCN: {
+        0: "KT1TxqZ8QtKvLu3V3JH7Gx58n7Co8pgtpQU5",
       },
     },
   ],
@@ -135,6 +161,13 @@ function floor(x: BigNumber) {
   return x.integerValue(BigNumber.ROUND_FLOOR);
 }
 
+export function getFeePercentage(exchangerType: ExchangerType) {
+  if (exchangerType === "liquidity_baking") {
+    return new BigNumber("0.1");
+  }
+  return new BigNumber("0.3");
+}
+
 export async function getPoolParameters(
   tezos: TezosToolkit,
   contractAddress: string,
@@ -142,16 +175,17 @@ export async function getPoolParameters(
 ): Promise<{ tokenPool: BigNumber; xtzPool: BigNumber }> {
   const contract = await loadContract(tezos, contractAddress, false);
   const storage = await contract.storage<any>();
-  if (type === "dexter") {
+
+  if (type === "quipuswap") {
     return {
-      tokenPool: storage.tokenPool,
-      xtzPool: storage.xtzPool,
+      tokenPool: storage.storage.token_pool,
+      xtzPool: storage.storage.tez_pool,
     };
   }
 
   return {
-    tokenPool: storage.storage.token_pool,
-    xtzPool: storage.storage.tez_pool,
+    tokenPool: storage.tokenPool,
+    xtzPool: storage.xtzPool,
   };
 }
 
@@ -161,11 +195,13 @@ export async function getMutezOutput(
   { address, type }: SwapContractDescriptor
 ) {
   const { tokenPool, xtzPool } = await getPoolParameters(tezos, address, type);
-  const invariant = tokenPool.multipliedBy(xtzPool);
-  if (invariant.eq(0)) {
+  if (tokenPool.eq(0) || xtzPool.eq(0)) {
     return new BigNumber(0);
   }
-  const tokenInWithFee = tokenAmount.multipliedBy(997);
+  const tokenInQuotient = new BigNumber(1000).minus(
+    getFeePercentage(type).times(10)
+  );
+  const tokenInWithFee = tokenAmount.times(tokenInQuotient);
   return tokenInWithFee
     .times(xtzPool)
     .idiv(tokenPool.times(1000).plus(tokenInWithFee));
@@ -181,7 +217,10 @@ export async function getMutezInput(
   }
   const { tokenPool, xtzPool } = await getPoolParameters(tezos, address, type);
   const numerator = xtzPool.times(1000).times(tokenAmount);
-  const denominator = tokenPool.minus(tokenAmount).times(997);
+  const denominatorQuotient = new BigNumber(1000).minus(
+    getFeePercentage(type).times(10)
+  );
+  const denominator = tokenPool.minus(tokenAmount).times(denominatorQuotient);
   return numerator.idiv(denominator).plus(1);
 }
 
@@ -195,7 +234,10 @@ export async function getTokenOutput(
   if (invariant.eq(0)) {
     return new BigNumber(0);
   }
-  const tezInWithFee = mutezAmount.multipliedBy(997);
+  const tezInQuotient = new BigNumber(1000).minus(
+    getFeePercentage(type).times(10)
+  );
+  const tezInWithFee = mutezAmount.multipliedBy(tezInQuotient);
   return tezInWithFee
     .times(tokenPool)
     .idiv(xtzPool.times(1000).plus(tezInWithFee));
@@ -211,7 +253,10 @@ export async function getTokenInput(
   }
   const { tokenPool, xtzPool } = await getPoolParameters(tezos, address, type);
   const numerator = tokenPool.times(1000).times(mutezAmount);
-  const denominator = xtzPool.minus(mutezAmount).times(997);
+  const denominatorQuotient = new BigNumber(1000).minus(
+    getFeePercentage(type).times(10)
+  );
+  const denominator = xtzPool.minus(mutezAmount).times(denominatorQuotient);
   return numerator.idiv(denominator).plus(1);
 }
 
@@ -252,19 +297,33 @@ export async function swap({
       1
     );
     const exchangeOperations = [
-      exchangerType === "quipuswap"
-        ? exchangeContract.methods
-            .tokenToTezPayment(rawInputAssetAmount, mutezOutput, accountPkh)
-            .toTransferParams()
-        : exchangeContract.methods
-            .tokenToXtz(
-              accountPkh,
-              accountPkh,
-              rawInputAssetAmount,
-              mutezOutput,
-              deadline.toString()
-            )
-            .toTransferParams(),
+      (() => {
+        switch (exchangerType) {
+          case "quipuswap":
+            return exchangeContract.methods
+              .tokenToTezPayment(rawInputAssetAmount, mutezOutput, accountPkh)
+              .toTransferParams();
+          case "dexter":
+            return exchangeContract.methods
+              .tokenToXtz(
+                accountPkh,
+                accountPkh,
+                rawInputAssetAmount,
+                mutezOutput,
+                deadline.toString()
+              )
+              .toTransferParams();
+          default:
+            return exchangeContract.methods
+              .tokenToXtz(
+                accountPkh,
+                rawInputAssetAmount,
+                mutezOutput,
+                deadline.toString()
+              )
+              .toTransferParams();
+        }
+      })(),
     ];
     batchify(
       transactionsBatch,
