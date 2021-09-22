86bcc816907329baa29dd84073dca35c757a33a1
Andrew Kryshtal <andrewkryshtal@Andrews-MacBook-Pro.local>
2021-09-22 17:06:46 +0300

    added files

---
+++ src/app/pages/Collectibles/CollectibleItem.tsx
@@ -0,0 +1,46 @@
+import React, { FC, useState } from "react";
+
+import { formatImgUri } from "../../../lib/image-uri";
+import { useAssetMetadata } from "../../../lib/temple/front";
+import { Link } from "../../../lib/woozie";
+import { ReactComponent as CollectiblePlaceholder } from "../../icons/collectiblePlaceholder.svg";
+
+interface Props {
+  assetSlug: string;
+}
+
+const CollectibleItem: FC<Props> = ({ assetSlug }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const collectibleMetadata = useAssetMetadata(assetSlug)!;
+
+  return (
+    <Link to={`/collectible/${assetSlug}`}>
+      <div className="flex items-center">
+        <div className="p-2">
+          <div
+            style={{ borderRadius: "12px" }}
+            className="border border-gray-300 w-16 h-16 flex items-center justify-center"
+          >
+            <img
+              onLoad={() => setIsLoaded(true)}
+              alt={collectibleMetadata.name}
+              style={!isLoaded ? { display: "none" } : {}}
+              className="w-12 h-12"
+              src={formatImgUri(collectibleMetadata.artifactUri!)}
+            />
+            {!isLoaded && <CollectiblePlaceholder />}
+          </div>
+        </div>
+        <div className="pl-2">
+          <p style={{ color: "#1B262C" }} className="text-sm">
+            {collectibleMetadata.name}
+          </p>
+          <p className="text-gray-600 text-xs">Last price 1000000 tez</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CollectibleItem;
---
+++ src/app/pages/Collectibles/CollectiblePage.tsx
@@ -0,0 +1,115 @@
+import React, { FC, useState } from "react";
+
+import classNames from "clsx";
+
+import PageLayout from "app/layouts/PageLayout";
+import { navigate } from "lib/woozie";
+
+import { T } from "../../../lib/i18n/react";
+import { formatImgUri } from "../../../lib/image-uri";
+import {
+  useAccount,
+  useAssetMetadata,
+  useBalance, useExplorerBaseUrls,
+} from "../../../lib/temple/front";
+import useCopyToClipboard from "../../../lib/ui/useCopyToClipboard";
+import CopyButton from "../../atoms/CopyButton";
+import Divider from "../../atoms/Divider";
+import FormSubmitButton from "../../atoms/FormSubmitButton";
+import HashShortView from "../../atoms/HashShortView";
+import { ReactComponent as CollectiblePlaceholderLarge } from "../../icons/collectiblePlaceholderLarge.svg";
+import { ReactComponent as CopyIcon } from "../../icons/copy.svg";
+
+interface Props {
+  assetSlug: string;
+}
+
+const CollectiblePage: FC<Props> = ({ assetSlug }) => {
+  const account = useAccount();
+  const accountPkh = account.publicKeyHash;
+  const { data: collectibleBalance } = useBalance(assetSlug, accountPkh);
+  const { copy } = useCopyToClipboard();
+  const collectibleData = useAssetMetadata(assetSlug)!;
+  const [isLoaded, setIsLoaded] = useState(false);
+  const {account: accountBaseUrl} = useExplorerBaseUrls();
+  console.log({accountBaseUrl});
+  return (
+    <PageLayout pageTitle={collectibleData.name}>
+      <div
+        style={{ maxWidth: "360px", margin: "auto" }}
+        className="text-center"
+      >
+        <div className={classNames("w-full max-w-sm mx-auto")}>
+          <div
+            style={{ borderRadius: "12px" }}
+            className={"border border-gray-300 max-w-xs p-6 mx-auto my-10"}
+          >
+            <img
+              onLoad={() => setIsLoaded(true)}
+              style={!isLoaded ? { display: "none" } : {}}
+              src={formatImgUri(collectibleData.artifactUri!)}
+              alt=""
+            />
+            {!isLoaded && <CollectiblePlaceholderLarge />}
+          </div>
+        </div>
+        <Divider />
+        <div className="flex justify-between items-baseline mt-4 mb-4">
+          <p className="text-gray-600 text-xs">
+            <T id={"name"} />
+          </p>
+          <p style={{ color: "#1B262C" }} className="text-xs">
+            {collectibleData.name}
+          </p>
+        </div>
+        <div className="flex justify-between items-baseline mt-4 mb-4">
+          <p className="text-gray-600 text-xs">
+            <T id={"amount"} />
+          </p>
+          <p style={{ color: "#1B262C" }} className="text-xs">
+            {collectibleBalance!.toFixed()}
+          </p>
+        </div>
+        <div className="flex justify-between items-baseline mb-4">
+          <p className="text-gray-600 text-xs">
+            <T id={"address"} />
+          </p>
+          <span>
+            <p
+              style={{ color: "#1B262C" }}
+              className="text-xs inline align-text-bottom"
+            >
+              <a className={"text-blue-700 underline"} href={`${accountBaseUrl}/${assetSlug}`}>
+                <HashShortView hash={assetSlug} />
+              </a>
+            </p>
+            <CopyButton text={assetSlug} type="link">
+              <CopyIcon
+                style={{ verticalAlign: "inherit" }}
+                className={classNames(
+                  "h-4 ml-1 w-auto inline",
+                  "stroke-orange stroke-2"
+                )}
+                onClick={() => copy()}
+              />
+            </CopyButton>
+          </span>
+        </div>
+        <Divider />
+        <FormSubmitButton
+          className="w-full justify-center border-none"
+          style={{
+            padding: "10px 2rem",
+            background: "#4299e1",
+            marginTop: "24px",
+          }}
+          onClick={() => navigate(`/send/${assetSlug}`)}
+        >
+          <T id={"send"} />
+        </FormSubmitButton>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default CollectiblePage;
