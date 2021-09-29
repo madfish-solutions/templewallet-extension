e994ad1eef2cc58edf3a797f5313d05e1c56bc97
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2021-09-29 12:22:15 +0300

    price impact added

--- src/app/templates/usePriceImpact.hook.ts
+++ src/app/templates/usePriceImpact.hook.ts
@@ -6,63 +6,63 @@ import BigNumber from "bignumber.js";
 import {ExchangerType, getPoolParameters, TempleAsset, tokensToAtoms} from "../../lib/temple/front";
 
 
-export const usePriceImpact = (tezos: TezosToolkit, selectedExchanger: ExchangerType, inputContractAddress?: string, outputContractAddress?: string, inputAssetAmount?: BigNumber, outputAssetAmount?: BigNumber, inputAsset?: TempleAsset, outputAsset?: TempleAsset ) => {
+export const usePriceImpact = (tezos: TezosToolkit,
+                               selectedExchanger: ExchangerType,
+                               inputContractAddress?: string,
+                               outputContractAddress?: string,
+                               inputAssetAmount?: BigNumber,
+                               outputAssetAmount?: BigNumber,
+                               inputAsset?: TempleAsset,
+                               outputAsset?: TempleAsset) => {
+
     const [priceImpact, setPriceImpact] = useState(new BigNumber(0));
 
     useEffect(() => {
-        ( async () => { let inputPool, outputPool, marketPrice;
+        (async () => {
+            let marketPrice, newPrice;
 
-            if(inputContractAddress !== undefined && outputContractAddress === undefined) {
-                const pool = await getPoolParameters(tezos, inputContractAddress, selectedExchanger);
-
-                inputPool = pool.tokenPool;
-                outputPool = pool.xtzPool;
-
-                marketPrice = inputPool.div(outputPool);
-
-            } else if(inputContractAddress === undefined && outputContractAddress !== undefined) {
-                const pool = await getPoolParameters(tezos, outputContractAddress, selectedExchanger);
-
-                inputPool = pool.xtzPool;
-                outputPool = pool.tokenPool;
-
-                marketPrice = inputPool.div(outputPool);
-
-            } else if(inputContractAddress !== undefined && outputContractAddress !== undefined){
-                const pool1 = await getPoolParameters(tezos, inputContractAddress, selectedExchanger);
-                const pool2 = await getPoolParameters(tezos, outputContractAddress, selectedExchanger);
-
-                inputPool = pool1.tokenPool;
-                outputPool = pool2.tokenPool;
-
-                marketPrice = pool1.tokenPool.multipliedBy(pool2.xtzPool).div(pool1.xtzPool.multipliedBy(pool2.tokenPool));
-                // const newPrice = inputPool.plus(inputAssetAmount).div(outputPool.minus(outputAssetAmount));
-
-            }
-
-            if(
-                inputPool !== undefined &&
-                outputPool !== undefined &&
-                marketPrice !== undefined &&
+            if (
                 inputAssetAmount !== undefined &&
                 outputAssetAmount !== undefined &&
                 inputAsset !== undefined &&
-                outputAsset !== undefined ) {
+                outputAsset !== undefined) {
 
                 const inputAtomsAmount = tokensToAtoms(inputAssetAmount, inputAsset.decimals);
                 const outputAtomsAmount = tokensToAtoms(outputAssetAmount, outputAsset.decimals);
-                console.log("inputPool:", inputPool.toFixed());
-                console.log("outputPool:", outputPool.toFixed());
 
-                const hundred = new BigNumber(100);
+                const calculateMarketPrice = (inputPool: BigNumber, outputPool: BigNumber) => inputPool.div(outputPool);
+                const calculateNewPrice = (inputPool: BigNumber,
+                                           outputPool: BigNumber,
+                                           inputAtomsAmount: BigNumber,
+                                           outputAtomsAmount: BigNumber) => (inputPool.plus(inputAtomsAmount)).div(outputPool.minus(outputAtomsAmount));
 
-                const newPrice = inputPool.plus(inputAtomsAmount).div(outputPool.minus(outputAtomsAmount));
+                if (inputContractAddress !== undefined && outputContractAddress === undefined) {
+                    const pool = await getPoolParameters(tezos, inputContractAddress, selectedExchanger);
 
-                const result = hundred.minus(hundred.div(newPrice).multipliedBy(marketPrice));
+                    marketPrice = calculateMarketPrice(pool.tokenPool, pool.xtzPool);
+                    newPrice = calculateNewPrice(pool.tokenPool, pool.xtzPool, inputAtomsAmount, outputAtomsAmount);
 
-                console.log(result.toFixed(2));
+                } else if (inputContractAddress === undefined && outputContractAddress !== undefined) {
+                    const pool = await getPoolParameters(tezos, outputContractAddress, selectedExchanger);
 
-                setPriceImpact(result);
+                    marketPrice = calculateMarketPrice(pool.xtzPool, pool.tokenPool);
+                    newPrice = calculateNewPrice(pool.xtzPool, pool.tokenPool, inputAtomsAmount, outputAtomsAmount);
+
+                } else if (inputContractAddress !== undefined && outputContractAddress !== undefined) {
+                    const pool1 = await getPoolParameters(tezos, inputContractAddress, selectedExchanger),
+                        pool2 = await getPoolParameters(tezos, outputContractAddress, selectedExchanger);
+
+                    marketPrice = pool1.tokenPool.div(pool1.xtzPool).multipliedBy(pool2.xtzPool.div(pool2.tokenPool));
+
+                    newPrice = (pool1.tokenPool.plus(inputAtomsAmount)).div(pool1.xtzPool).multipliedBy(pool2.xtzPool.div(pool2.tokenPool.minus(outputAtomsAmount)));
+                }
+
+                if (newPrice !== undefined && marketPrice !== undefined) {
+                    const hundred = new BigNumber(100),
+                        result = hundred.minus(hundred.div(newPrice).multipliedBy(marketPrice));
+
+                    setPriceImpact(result);
+                }
             } })();
     }, [tezos, inputContractAddress, outputContractAddress, inputAssetAmount, outputAssetAmount, selectedExchanger, inputAsset, outputAsset])
 
