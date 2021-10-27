80120484ce6bbcea4cf5c499b02a522833900ee4
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2021-10-27 11:33:13 +0300

    Onboarding (#407)
    * two steps
    
    * layout finished
    
    * attention page finished
    
    * set back button
    
    * isolated version
    
    * done
    
    * newlines at the end of files added
    
    * refactoring
    
    * more refactoring
    
    * final corrections
    
    * last final corrections

--- src/app/layouts/PageLayout.tsx
+++ src/app/layouts/PageLayout.tsx
@@ -1,43 +1,35 @@
-import React, {
-  ComponentProps,
-  FC,
-  ReactNode,
-  Suspense,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import React, {ComponentProps, FC, ReactNode, Suspense, useEffect, useLayoutEffect, useRef, useState,} from "react";
 
 import classNames from "clsx";
 
 import DocBg from "app/a11y/DocBg";
-import { Button } from "app/atoms/Button";
+import {Button} from "app/atoms/Button";
 import Spinner from "app/atoms/Spinner";
-import { useAppEnv } from "app/env";
+import {useAppEnv} from "app/env";
 import ErrorBoundary from "app/ErrorBoundary";
-import { ReactComponent as ChevronLeftIcon } from "app/icons/chevron-left.svg";
+import {ReactComponent as ChevronLeftIcon} from "app/icons/chevron-left.svg";
 import ContentContainer from "app/layouts/ContentContainer";
 import ConfirmationOverlay from "app/layouts/PageLayout/ConfirmationOverlay";
 import Header from "app/layouts/PageLayout/Header";
 import NoLambdaViewContractAlert from "app/templates/NoLambdaViewContractAlert";
-import { AnalyticsConfirmationOverlay } from "lib/analytics";
-import { T } from "lib/i18n/react";
-import { HistoryAction, useLocation, goBack, navigate } from "lib/woozie";
+import {AnalyticsConfirmationOverlay} from "lib/analytics";
+import {T} from "lib/i18n/react";
+import {goBack, HistoryAction, navigate, useLocation} from "lib/woozie";
 
-import { PageLayoutSelectors } from "./PageLayout.selectors";
+import {useOnboardingProgress} from "../pages/Onboarding/hooks/useOnboardingProgress.hook";
+import {PageLayoutSelectors} from "./PageLayout.selectors";
 
 type PageLayoutProps = ToolbarProps;
 
-const PageLayout: FC<PageLayoutProps> = ({ children, ...toolbarProps }) => {
-  const { fullPage } = useAppEnv();
+const PageLayout: FC<PageLayoutProps> = ({children, ...toolbarProps}) => {
+  const {fullPage} = useAppEnv();
 
   return (
     <>
-      <DocBg bgClassName="bg-primary-orange" />
+      <DocBg bgClassName="bg-primary-orange"/>
 
       <div className={classNames(fullPage && "pb-20", "relative")}>
-        <Header />
+        <Header/>
 
         <ContentPaper>
           <Toolbar {...toolbarProps} />
@@ -100,11 +92,21 @@ const SpinnerSection: FC = () => (
 type ToolbarProps = {
   pageTitle?: ReactNode;
   hasBackAction?: boolean;
+  step?: number;
+  setStep?: (step: number) => void;
+  skip?: boolean;
 };
 
-const Toolbar: FC<ToolbarProps> = ({ pageTitle, hasBackAction = true }) => {
-  const { historyPosition, pathname } = useLocation();
-  const { fullPage, registerBackHandler, onBack } = useAppEnv();
+const Toolbar: FC<ToolbarProps> = ({pageTitle, hasBackAction = true, step, setStep, skip}) => {
+  const {historyPosition, pathname} = useLocation();
+  const {fullPage, registerBackHandler, onBack} = useAppEnv();
+  const {setOnboardingCompleted} = useOnboardingProgress();
+
+  const onStepBack = () => {
+    if (step && setStep && step > 0) {
+      setStep(step - 1);
+    }
+  }
 
   const inHome = pathname === "/";
   const canBack = historyPosition > 0 || !inHome;
@@ -179,7 +181,33 @@ const Toolbar: FC<ToolbarProps> = ({ pageTitle, hasBackAction = true }) => {
               "transition duration-300 ease-in-out",
               "opacity-90 hover:opacity-100"
             )}
-            onClick={onBack}
+            onClick={step ? onStepBack : onBack}
+            testID={PageLayoutSelectors.BackButton}
+          >
+            <ChevronLeftIcon
+              className={classNames(
+                "-ml-2",
+                "h-5 w-auto",
+                "stroke-current",
+                "stroke-2"
+              )}
+            />
+            <T id="back" />
+          </Button>
+        )}
+        {!!step && (step > 0) && (
+          <Button
+            className={classNames(
+              "px-4 py-2",
+              "rounded",
+              "flex items-center",
+              "text-gray-600 text-shadow-black",
+              "text-sm font-semibold leading-none",
+              "hover:bg-black hover:bg-opacity-5",
+              "transition duration-300 ease-in-out",
+              "opacity-90 hover:opacity-100"
+            )}
+            onClick={step ? onStepBack : onBack}
             testID={PageLayoutSelectors.BackButton}
           >
             <ChevronLeftIcon
@@ -208,7 +236,26 @@ const Toolbar: FC<ToolbarProps> = ({ pageTitle, hasBackAction = true }) => {
         </h2>
       )}
 
-      <div className="flex-1" />
+      <div className="flex-1"/>
+      {skip && (
+        <div className="flex content-end">
+          <Button
+            className={classNames(
+              "px-4 py-2",
+              "rounded",
+              "flex items-center",
+              "text-gray-600 text-shadow-black",
+              "text-sm font-semibold leading-none",
+              "hover:bg-black hover:bg-opacity-5",
+              "transition duration-300 ease-in-out",
+              "opacity-90 hover:opacity-100"
+            )}
+            onClick={() => setOnboardingCompleted(true)}
+          >
+            <T id="skip"/>
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
