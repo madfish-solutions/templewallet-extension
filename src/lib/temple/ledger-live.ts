62fe924c42e392e0d1990d7bc1d56fe82d2bcd75
serh11p <git@serh11p.com>
2021-11-01 12:37:56 +0200

    Update Ledger Live env

--- src/lib/temple/ledger-live.ts
+++ src/lib/temple/ledger-live.ts
@@ -1,10 +1,7 @@
-import { browser } from "webextension-polyfill-ts";
-
 import { TempleSharedStorageKey } from "lib/temple/types";
 
 export async function isLedgerLiveEnabledByDefault() {
-  const isWin = (await browser.runtime.getPlatformInfo()).os === "win";
-  return process.env.TARGET_BROWSER === "chrome" && !isWin;
+  return process.env.TARGET_BROWSER === "chrome";
 }
 
 export async function isLedgerLiveEnabled() {
