6164326a1ba2ee7229a9d5d1af645ff7deeb60b9
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-10 17:18:09 +0200

    fix unsafe return issue

--- src/app/a11y/AwaitFonts.tsx
+++ src/app/a11y/AwaitFonts.tsx
@@ -38,7 +38,6 @@ async function awaitFonts(name: string, weights: number[], className: string) {
     if (process.env.NODE_ENV === "development") {
       console.error(err);
     }
-  } finally {
-    return null;
   }
+  return null
 }
