ebe9e2671d3d0a3363339660c1655b2cdcf47016
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-11 12:06:31 +0200

    Removed analytics tracking (#420)
    * removed analytics tracking
    
    * remove unused variable

--- src/lib/analytics/use-analytics-settings.hook.ts
+++ src/lib/analytics/use-analytics-settings.hook.ts
@@ -1,21 +1,10 @@
-import { AnalyticsEventEnum } from "./analytics-event.enum";
-import { useAnalyticsNetwork } from "./use-analytics-network.hook";
-import { sendTrackEvent, useAnalyticsState } from "./use-analytics-state.hook";
+import { useAnalyticsState } from "./use-analytics-state.hook";
 
 export const useAnalyticsSettings = () => {
   const { analyticsState, setAnalyticsState } = useAnalyticsState();
-  const rpc = useAnalyticsNetwork();
 
   const setAnalyticsEnabled = (enabled?: boolean) => {
     setAnalyticsState({ ...analyticsState, enabled });
-
-    sendTrackEvent(
-      analyticsState.userId,
-      rpc,
-      enabled
-        ? AnalyticsEventEnum.AnalyticsEnabled
-        : AnalyticsEventEnum.AnalyticsDisabled
-    );
   };
 
   return {
