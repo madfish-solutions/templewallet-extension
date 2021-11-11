39a86e235cb3859cea55284cbaae2efbdfe594a5
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-11 11:57:34 +0200

    remove unused variable

--- src/lib/analytics/use-analytics-settings.hook.ts
+++ src/lib/analytics/use-analytics-settings.hook.ts
@@ -1,9 +1,7 @@
-import { useAnalyticsNetwork } from "./use-analytics-network.hook";
 import { useAnalyticsState } from "./use-analytics-state.hook";
 
 export const useAnalyticsSettings = () => {
   const { analyticsState, setAnalyticsState } = useAnalyticsState();
-  const rpc = useAnalyticsNetwork();
 
   const setAnalyticsEnabled = (enabled?: boolean) => {
     setAnalyticsState({ ...analyticsState, enabled });
