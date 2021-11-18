28855c6447b497c415b76c8d479e3e4ac1df2389
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-18 11:27:37 +0200

    Lock timeout (#432)
    * wip, added detection with enabled and closed extension
    
    * wip, global variable for background task
    
    * added extension lock after inactivity
    
    * fix review
    
    * fix multiple connections logic
    
    * review fixes

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,6 @@
 import { browser } from "webextension-polyfill-ts";
 
+import { lock } from "lib/temple/back/actions";
 import { start } from "lib/temple/back/main";
 
 browser.runtime.onInstalled.addListener(({ reason }) => {
@@ -23,3 +24,20 @@ function openFullPage() {
     url: browser.runtime.getURL("fullpage.html"),
   });
 }
+
+const LOCK_TIME = 5 * 60_000;
+let disconnectTimestamp = 0;
+let connectionsCount = 0;
+
+browser.runtime.onConnect.addListener((externalPort) => {
+  connectionsCount++;
+  if (connectionsCount === 1 && Date.now() - disconnectTimestamp >= LOCK_TIME) {
+    lock();
+  }
+  externalPort.onDisconnect.addListener(() => {
+    connectionsCount--;
+    if (connectionsCount === 0) {
+      disconnectTimestamp = Date.now();
+    }
+  });
+});
