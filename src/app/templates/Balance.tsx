48767772da16d9ce29cf229de958ae8539ccbccb
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-30 12:07:53 +0200

    wip

--- src/app/templates/Balance.tsx
+++ src/app/templates/Balance.tsx
@@ -25,7 +25,7 @@ const Balance = memo<BalanceProps>(({ address, children, assetSlug = 'tez', netw
   const exist = balance !== undefined;
 
   return useMemo(() => {
-    const childNode = children(exist ? balance! : new BigNumber(0));
+    const childNode = children(balance !== undefined ? balance : new BigNumber(0));
 
     return (
       <CSSTransition
