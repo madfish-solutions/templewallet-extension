55ab96809e076de2e4adeabd26639a6cf96ac39a
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-12-15 15:29:40 +0200

    added additional check for img loader

--- src/lib/image-uri.ts
+++ src/lib/image-uri.ts
@@ -8,7 +8,7 @@ export function formatImgUri(origin: string) {
 }
 
 export function sanitizeImgUri(url: string, x = 64, y = 64) {
-  if (url.startsWith('http')) {
+  if (typeof url === 'string' && url.startsWith('http')) {
     return `https://img.templewallet.com/insecure/fit/${x}/${y}/ce/0/plain/${url}`;
   }
   return url;
