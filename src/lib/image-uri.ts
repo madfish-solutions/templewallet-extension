6c4b7f1358d02d7af1fd6be935436c5ab6d5ea1a
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-12-16 12:00:11 +0200

    fix default types

--- src/lib/image-uri.ts
+++ src/lib/image-uri.ts
@@ -1,17 +1,17 @@
 export const IPFS_GATEWAY = 'ipfs.io';
 
-export function formatImgUri(origin: string | undefined) {
-  if (origin && origin.startsWith('ipfs://')) {
+export function formatImgUri(origin = '') {
+  if (origin.startsWith('ipfs://')) {
     return `https://${IPFS_GATEWAY}/ipfs/${origin.substring(7)}/`;
   }
-  return origin ?? '';
+  return origin;
 }
 
-export function sanitizeImgUri(url: string | undefined, x = 64, y = 64): string {
-  if (typeof url === 'string' && url.startsWith('http')) {
+export function sanitizeImgUri(url = '', x = 64, y = 64) {
+  if (url.startsWith('http')) {
     return `https://img.templewallet.com/insecure/fit/${x}/${y}/ce/0/plain/${url}`;
   }
-  return url ?? '';
+  return url;
 }
 
 export const formatCollectibleUri = (address: string, id: string) => {
