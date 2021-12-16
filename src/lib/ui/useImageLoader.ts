33b28c9ecb9eb0023364d677ee683ea5b6794ec2
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-12-16 15:59:18 +0200

    changed loader to objkt (#488)
    * changed loader to objkt
    
    * fix
    
    * added objkt to mainnet and temple proxy to testnet, fixed image loading at manage tab
    
    * fix for code check and compatability
    
    * refactor image loader to separate hook
    
    * fix checks
    
    * added additional check for img loader
    
    * fix types
    
    * make function require same nullable type
    
    * fix default types
    
    * fix ubisoft loader

---
+++ src/lib/ui/useImageLoader.ts
@@ -0,0 +1,24 @@
+import { formatCollectibleUri, formatImgUri, sanitizeImgUri } from 'lib/image-uri';
+import { useRetryableSWR } from 'lib/swr';
+import { fromAssetSlug } from 'lib/temple/assets';
+import { useAssetMetadata, getThumbnailUri, useNetwork, useTezos } from 'lib/temple/front';
+
+const useImageLoader = (assetSlug: string): string => {
+  const tezos = useTezos();
+  const network = useNetwork();
+  const collectibleMetadata = useAssetMetadata(assetSlug)!;
+  const asset = useRetryableSWR(['asset', assetSlug, tezos.checksum], () => fromAssetSlug(tezos, assetSlug), {
+    suspense: true
+  }).data!;
+  if (asset === 'tez') {
+    return getThumbnailUri(collectibleMetadata) ?? '';
+  }
+  const assetId = asset.id ? asset.id.toString() : '0';
+  if (network.type === 'main') {
+    return formatCollectibleUri(asset.contract, assetId);
+  } else {
+    return sanitizeImgUri(formatImgUri(collectibleMetadata.displayUri || collectibleMetadata.artifactUri), 512, 512);
+  }
+};
+
+export default useImageLoader;
