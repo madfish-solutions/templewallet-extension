9ba11778a358a9a9254771cc3780657069877749
letier3110 <letier3110@gmail.com>
2021-12-24 15:15:27 +0200

    fix review

--- src/lib/exolix-api.ts
+++ src/lib/exolix-api.ts
@@ -49,7 +49,7 @@ export const getCurrencies = async () => {
   return api.get<CurrenciesInterface[]>('/currency').then(r => r.data);
 };
 
-export interface IGetRateData {
+export interface GetRateData {
   destination_amount: number;
   rate: number;
   min_amount: string;
