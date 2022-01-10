16a8d756bab69c32aea0f50fed246528448736d0
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2022-01-10 15:35:14 +0200

    Import watch-only account 'Clean' button not clickable

--- src/app/atoms/CleanButton.tsx
+++ src/app/atoms/CleanButton.tsx
@@ -48,7 +48,7 @@ const CleanButton: FC<CleanButtonProps> = ({
       tabIndex={-1}
       {...rest}
     >
-      <CloseIcon className="w-auto h-4 stroke-current" style={iconStyle} />
+      <CloseIcon title="" className="w-auto h-4 stroke-current" style={iconStyle} />
     </button>
   );
 };
