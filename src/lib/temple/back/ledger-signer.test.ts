64fc7736de539d60c45cb84c018fcefec602c9fb
letier3110 <letier3110@gmail.com>
2022-01-12 17:06:22 +0200

    added more test cases

--- src/lib/temple/back/ledger-signer.test.ts
+++ src/lib/temple/back/ledger-signer.test.ts
@@ -1,54 +1,112 @@
-import { b58cdecode, b58decode, hex2buf } from '@taquito/utils';
-import { LedgerTempleBridgeTransport } from '@temple-wallet/ledger-bridge';
-import * as sodium from 'libsodium-wrappers';
-import { browser } from 'webextension-polyfill-ts';
+import { b58cdecode, hex2buf } from '@taquito/utils';
+import { crypto_generichash, ready } from 'libsodium-wrappers';
 
 import toBuffer from 'typedarray-to-buffer';
 
-import { curves, getSig, pref, safeSignEdData, TempleLedgerSigner } from './ledger-signer';
+import { curves, getSig, pref, safeSignEdData, safeSignP2Data, safeSignSpData, verifySignature } from './ledger-signer';
+
+const PAYLOAD = {
+  ed: '0501313230363136653739323037333734373236393665363732303734363836313734323037373639366336633230363236353230373336393637366536353634',
+  sp: '0501313230363136653739323037333734373236393665363732303734363836313734323037373639366336633230363236353230373336393637366536353634',
+  p2: '0501313230363136653739323037333734373236393665363732303734363836313734323037373639366336633230363236353230373336393637366536353634'
+};
+
+const WRONG_PAYLOAD =
+  '0501313230363136653739323037333734373236393665363732303734363836313734523037373639366336633230363236353230373336393637366536353634';
+const SIGNATURE = {
+  ed: 'edsigtyb1zfVFHCxy458ZqZ1s4CGJdhrV6ocgj3JxpMj3qX6vHEaYKnifTPFNirwxznPW3xBrTWWrDdu2qDoJqCPNQi3DVMc8dS',
+  sp: 'spsig1UoxGshstbBBbdsWEKMQXDnZeSQUgYmWkSup2L2NoGWYfWH6it3V8Fpds1obbHPwe133WdGUez6VwDAU6429xjsn21Zr84',
+  p2: 'p2sigsHG1Wprp1N8npqXFHEjkNgh9NUBMsr6xUzy8dyAD3cmwn6PUo4LsFAs97otv3Vqm2C5DyNduaydvS91zB3NzynV1rACUb'
+};
+
+const PUBLIC_KEY = {
+  ed: 'edpkvBYQLaemWxY8k6R5nYmdWhUQmBNgMW81mo9kMovfeWpD71kTSL',
+  sp: 'sppk7c2AdKViHtcjmfY7cYuGn8iMTa5U91wx4brSJw9FadzPzo5BB3A',
+  p2: 'p2pk67DMkN5mgnXxt9ShrDmuiS6b3hSyhfMN8SkHDbM95zcaGcn7GAN'
+};
+
+const PUBLIC_KEY_HASH = {
+  ed: 'tz1gohAk1nxzdKTt76iXeynpZESwTEMnjRq8',
+  sp: 'tz2WanjrRiuHat2ZaQrbsq13rm4E5qmqnBwA',
+  p2: 'tz3RMrbWr9g3tb4oM48LUF91EGK4mrBMS5ja'
+};
 
 describe('Ledger Signer tests', () => {
+  describe('Ledger transport', () => {
+    it('Verify message', async () => {
+      await ready;
+      const res = verifySignature(PAYLOAD.ed, SIGNATURE.ed, PUBLIC_KEY.ed, PUBLIC_KEY_HASH.ed);
+      expect(res).toBe(true);
+    });
+    it('Wrong public key to verify message', async () => {
+      await ready;
+      expect(() => verifySignature(PAYLOAD.ed, SIGNATURE.ed, PUBLIC_KEY.sp, PUBLIC_KEY_HASH.ed)).toThrow();
+    });
+    it('Wrong public key to verify message', async () => {
+      await ready;
+      expect(() => verifySignature(PAYLOAD.ed, SIGNATURE.ed, PUBLIC_KEY.sp, PUBLIC_KEY_HASH.ed)).toThrow();
+    });
+    it('Unsupported curve to verify message', async () => {
+      await ready;
+      expect(() =>
+        verifySignature(PAYLOAD.ed, SIGNATURE.ed, 'do' + PUBLIC_KEY.ed.substring(2), PUBLIC_KEY_HASH.ed)
+      ).toThrow();
+    });
+  });
   describe('Signature curve', () => {
-    // it('Sign ed curve', async () => {
-    //   await sodium.ready;
-    //   const bridgeUrl = process.env.TEMPLE_WALLET_LEDGER_BRIDGE_URL!;
-    //   const transport = await LedgerTempleBridgeTransport.open(bridgeUrl);
-    //   const twt = new TempleLedgerSigner(transport);
-    //   const bytesPayload =
-    //     '05010000004254657a6f73205369676e6564204d6573736167653a206d79646170702e636f6d20323032312d30312d31345431353a31363a30345a2048656c6c6f20776f726c6421';
-    //   const res = twt.sign(bytesPayload);
-    //   // console.log(res);
-    //   expect(res).toBe('true');
-    //   //   const storageKey = toPubKeyStorageKey('myAwesomeDappKey');
-    //   //   expect(storageKey).toBe('beacon_myAwesomeDappKey_pubkey');
-    // });
-    it('Verify ed curve', async () => {
-      await sodium.ready;
-
-      // 616e7920737472696e6720746861742077696c6c206265207369676e6564
-      // 0501313230363136653739323037333734373236393665363732303734363836313734323037373639366336633230363236353230373336393637366536353634
-      // edsigtyb1zfVFHCxy458ZqZ1s4CGJdhrV6ocgj3JxpMj3qX6vHEaYKnifTPFNirwxznPW3xBrTWWrDdu2qDoJqCPNQi3DVMc8dS
-      // tz1gohAk1nxzdKTt76iXeynpZESwTEMnjRq8
-      const bytesPayload =
-        '0501313230363136653739323037333734373236393665363732303734363836313734323037373639366336633230363236353230373336393637366536353634';
-      const signature =
-        'edsigtyb1zfVFHCxy458ZqZ1s4CGJdhrV6ocgj3JxpMj3qX6vHEaYKnifTPFNirwxznPW3xBrTWWrDdu2qDoJqCPNQi3DVMc8dS';
-      const res = b58decode(
-        'edsigtyb1zfVFHCxy458ZqZ1s4CGJdhrV6ocgj3JxpMj3qX6vHEaYKnifTPFNirwxznPW3xBrTWWrDdu2qDoJqCPNQi3DVMc8dS'
-      );
-      const publicKey = 'edpkunHr4t3DErGD1uGzX78uyZ9uHwRvVF9ASMArNvQxKnwuoxYNZK';
-      const privateKey =
-        'edskRyjosWqCEL7EAWAPv9iNNSCvAMjejXimPsZjr4USAzSjvgKFfVHd5Q5Xeu8viH1SgJ1vWa3fTfKw6axtpiju7g7nvTXSRY';
-      const pkh = 'tz1Q7DbSZdM1RfBZwey1eRNia22uPpg5Hpyo';
-      const curve = publicKey.substring(0, 2) as curves;
-      const _publicKey = toBuffer(b58cdecode(publicKey, pref[curve].pk));
-      let sig = getSig(signature, curve, pref);
-      const bytesHash = sodium.crypto_generichash(32, hex2buf(bytesPayload));
+    it('Verify correct message with ed curve', async () => {
+      await ready;
+      const curve = PUBLIC_KEY.ed.substring(0, 2) as curves;
+      const _publicKey = new Uint8Array(toBuffer(b58cdecode(PUBLIC_KEY.ed, pref[curve].pk)));
+      const sig = new Uint8Array(getSig(SIGNATURE.ed, curve, pref));
+      const bytesHash = crypto_generichash(32, hex2buf(PAYLOAD.ed));
       const data = safeSignEdData(sig, bytesHash, _publicKey);
-      console.log(res);
-      expect(sig).toBe('true');
-      //   const storageKey = toPubKeyStorageKey('myAwesomeDappKey');
-      //   expect(storageKey).toBe('beacon_myAwesomeDappKey_pubkey');
+      expect(data).toBe(true);
+    });
+    it('Verify incorrect message with ed curve', async () => {
+      await ready;
+      const curve = PUBLIC_KEY.ed.substring(0, 2) as curves;
+      const _publicKey = new Uint8Array(toBuffer(b58cdecode(PUBLIC_KEY.ed, pref[curve].pk)));
+      const sig = new Uint8Array(getSig(SIGNATURE.ed, curve, pref));
+      const bytesHash = crypto_generichash(32, hex2buf(WRONG_PAYLOAD));
+      const data = safeSignEdData(sig, bytesHash, _publicKey);
+      expect(data).toBe(false);
+    });
+    it('Verify correct message with secp256k1 curve', async () => {
+      await ready;
+      const curve = PUBLIC_KEY.sp.substring(0, 2) as curves;
+      const _publicKey = new Uint8Array(toBuffer(b58cdecode(PUBLIC_KEY.sp, pref[curve].pk)));
+      const sig = new Uint8Array(getSig(SIGNATURE.sp, curve, pref));
+      const bytesHash = crypto_generichash(32, hex2buf(PAYLOAD.sp));
+      const data = safeSignSpData(sig, bytesHash, _publicKey);
+      expect(data).toBe(true);
+    });
+    it('Verify incorrect message with secp256k1 curve', async () => {
+      await ready;
+      const curve = PUBLIC_KEY.sp.substring(0, 2) as curves;
+      const _publicKey = new Uint8Array(toBuffer(b58cdecode(PUBLIC_KEY.sp, pref[curve].pk)));
+      const sig = new Uint8Array(getSig(SIGNATURE.sp, curve, pref));
+      const bytesHash = crypto_generichash(32, hex2buf(WRONG_PAYLOAD));
+      const data = safeSignSpData(sig, bytesHash, _publicKey);
+      expect(data).toBe(false);
+    });
+    it('Verify correct message with p256 curve', async () => {
+      await ready;
+      const curve = PUBLIC_KEY.p2.substring(0, 2) as curves;
+      const _publicKey = new Uint8Array(toBuffer(b58cdecode(PUBLIC_KEY.p2, pref[curve].pk)));
+      const sig = new Uint8Array(getSig(SIGNATURE.p2, curve, pref));
+      const bytesHash = crypto_generichash(32, hex2buf(PAYLOAD.p2));
+      const data = safeSignP2Data(sig, bytesHash, _publicKey);
+      expect(data).toBe(true);
+    });
+    it('Verify incorrect message with p256 curve', async () => {
+      await ready;
+      const curve = PUBLIC_KEY.p2.substring(0, 2) as curves;
+      const _publicKey = new Uint8Array(toBuffer(b58cdecode(PUBLIC_KEY.p2, pref[curve].pk)));
+      const sig = new Uint8Array(getSig(SIGNATURE.p2, curve, pref));
+      const bytesHash = crypto_generichash(32, hex2buf(WRONG_PAYLOAD));
+      const data = safeSignP2Data(sig, bytesHash, _publicKey);
+      expect(data).toBe(false);
     });
   });
 });
