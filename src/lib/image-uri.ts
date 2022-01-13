bb45ea3816e61d76678ea311711e4d4733452d28
Andrii Ivaniv <unixvb@gmail.com>
2022-01-13 16:13:59 +0200

    fix

--- src/lib/image-uri.ts
+++ src/lib/image-uri.ts
@@ -2,22 +2,22 @@ import { AssetMetadata } from './temple/metadata';
 
 export const IPFS_GATEWAY = 'cloudflare-ipfs.com';
 
-export function formatIpfsUri(origin = '') {
-  if (origin.startsWith('ipfs://')) {
-    return `https://${IPFS_GATEWAY}/ipfs/${origin.substring(7)}/`;
-  }
-  return origin;
-}
-
-export function sanitizeImgUri(url = '', x = 64, y = 64) {
-  if (url.startsWith('http')) {
-    return `https://img.templewallet.com/insecure/fit/${x}/${y}/ce/0/plain/${url}`;
+export function formatIpfsUri(url = '') {
+  if (url.startsWith('ipfs://')) {
+    return `https://${IPFS_GATEWAY}/ipfs/${url.substring(7)}/`;
   }
   return url;
 }
 
-export const formatTokenUri = (metadata: AssetMetadata) => {
-  const ipfsUri = formatIpfsUri(metadata.thumbnailUri);
+const sanitizeImgUri = (url: string, x = 64, y = 64) => {
+  if (url.startsWith('http')) {
+    return `https://img.templewallet.com/insecure/fit/${x}/${y}/ce/0/plain/${url}`;
+  }
+  return url;
+};
+
+export const formatTokenUri = (thumbnailUri = '') => {
+  const ipfsUri = formatIpfsUri(thumbnailUri);
 
   return sanitizeImgUri(ipfsUri);
 };
