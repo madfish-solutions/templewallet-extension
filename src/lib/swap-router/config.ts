659cfa97489d536231073cc3c2f6c9dbcc7686c2
Andrii Ivaniv <unixvb@gmail.com>
2022-01-28 12:08:13 +0200

    TEZOS_DEXES_API_URL updated

--- src/lib/swap-router/config.ts
+++ src/lib/swap-router/config.ts
@@ -1,4 +1,4 @@
-export const TEZOS_DEXES_API_URL = 'ws://mainnet-node.madfish.xyz:3001/';
+export const TEZOS_DEXES_API_URL = 'wss://mainnet-dexes-test.madfish.xyz/';
 
 export const ROUTING_FEE_ADDRESS = 'tz1ZDvLWqzqBNNYkHNEWm2aEWBk2YCGqZeTW';
 
@@ -6,5 +6,3 @@ export const ROUTING_FEE_PERCENT = 0.5;
 
 export const ROUTING_FEE_RATIO = (100 - ROUTING_FEE_PERCENT) / 100;
 export const ROUTING_FEE_INVERTED_RATIO = 100 / (100 - ROUTING_FEE_PERCENT);
-
-export const TRANSACTION_LIFE_MINUTES = 20;
