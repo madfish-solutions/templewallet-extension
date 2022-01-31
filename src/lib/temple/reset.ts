36d3008d4c4033b5f9c155d51753a83f49d8b8ae
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2022-01-31 21:12:21 +0200

    Delete outdated pending operations from activities (#559)
    * Delete outdated pending operations from activities
    
    * refactoring
    
    * pending op not appearing after wallet reset fix
    
    * pending op not appearing after wallet reset fix
    
    * tests fixed

--- src/lib/temple/reset.ts
+++ src/lib/temple/reset.ts
@@ -4,6 +4,7 @@ import * as Repo from 'lib/temple/repo';
 
 export async function clearStorage() {
   await Repo.db.delete();
+  await Repo.db.open();
   await browser.storage.local.clear();
   await localStorage.clear();
 }
