069671bc15603fc858d1e67dcc769a93df63f7b8
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-05-03 18:31:31 +0100

    added quipuswap tokens whitelist

--- src/lib/temple/metadata/fixtures.ts
+++ src/lib/temple/metadata/fixtures.ts
@@ -97,5 +97,481 @@ export const PRESERVED_TOKEN_METADATA = new Map<string, AssetMetadata>([
       name: 'SmartLink',
       thumbnailUri: 'https://quipuswap.com/tokens/smak.png'
     }
+  ],
+  [
+    'KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW_0',
+    {
+      decimals: 6,
+      symbol: 'hDAO',
+      name: 'hic et nunc DAO',
+      thumbnailUri: 'ipfs://QmPfBrZiRsC39S2VvNbhuxH9HnNcSx8aef9uBCG51J5c4e'
+    }
+  ],
+  [
+    'KT1LRboPna9yQY9BrjtQYDS1DVxhKESK4VVd_0',
+    {
+      decimals: 8,
+      symbol: 'WRAP',
+      name: 'Wrap Governance Token',
+      thumbnailUri: 'ipfs://Qma2o69VRZe8aPsuCUN1VRUE5k67vw2mFDXb35uDkqn17o'
+    }
+  ],
+  [
+    'KT1BHCumksALJQJ8q8to2EPigPW6qpyTr7Ng_0',
+    {
+      decimals: 8,
+      symbol: 'CRUNCH',
+      name: 'CRUNCH',
+      thumbnailUri: 'ipfs://bafybeienhhbxz53n3gtg7stjou2zs3lmhupahwovv2kxwh5uass3bc5xzq'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_0',
+    {
+      decimals: 18,
+      symbol: 'wAAVE',
+      name: 'Wrapped AAVE',
+      thumbnailUri: 'ipfs://QmVUVanUUjHmgkjnUC6TVzG7pPz6iy7C8tnAoXNNpofYPg'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_1',
+    {
+      decimals: 18,
+      symbol: 'wBUSD',
+      name: 'Wrapped BUSD',
+      thumbnailUri: 'ipfs://QmRB63vb8ThpmxHKF4An3XD8unHyCUuLYm5bZNhXwU4gAZ'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_2',
+    {
+      decimals: 4,
+      symbol: 'wCEL',
+      name: 'Wrapped CEL',
+      thumbnailUri: 'ipfs://QmfNuyU3V6XeS9PgVXMDq4h5ux1VciUXtApP2ZGC4VSGLd'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_3',
+    {
+      decimals: 18,
+      symbol: 'wCOMP',
+      name: 'Wrapped COMP',
+      thumbnailUri: 'ipfs://QmYy2jUUE69W5eE9uwh4x5LqUxHt3GVy8sjXHhpViqCspG'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_4',
+    {
+      decimals: 8,
+      symbol: 'wCRO',
+      name: 'Wrapped CRO',
+      thumbnailUri: 'ipfs://QmUuhxgCm2EXwddpU8ofBBh9z1qzxf2BJRbxxR1ebYr8Hd'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_5',
+    {
+      decimals: 18,
+      symbol: 'wDAI',
+      name: 'Wrapped DAI',
+      thumbnailUri: 'ipfs://QmVov6RtfRNzuQGvGKmhnABUsfCiDKvn31amg8DUxzowtM'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_6',
+    {
+      decimals: 18,
+      symbol: 'wFTT',
+      name: 'Wrapped FTT',
+      thumbnailUri: 'ipfs://QmVBjgrJiUynv72MR6rRyUu1WLKX52bWGiKk7H2CaFDRNW'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_7',
+    {
+      decimals: 18,
+      symbol: 'wHT',
+      name: 'Wrapped HT',
+      thumbnailUri: 'ipfs://Qmayt4JbYTkQinNUeVGLhp51LTRMD1273HRgo9p96SoQaM'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_8',
+    {
+      decimals: 8,
+      symbol: 'wHUSD',
+      name: 'Wrapped HUSD',
+      thumbnailUri: 'ipfs://QmT9bozupnmWjmjnXp8KcZqDY9HYLLr5KgojfxqgoWRgCt'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_9',
+    {
+      decimals: 18,
+      symbol: 'wLEO',
+      name: 'Wrapped LEO',
+      thumbnailUri: 'ipfs://QmPrmELnhSoheHCHx6XcjSera7f89NYmXabBnSRSCdDjBh'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_10',
+    {
+      decimals: 18,
+      symbol: 'wLINK',
+      name: 'Wrapped LINK',
+      thumbnailUri: 'ipfs://QmeaRuB578Xgy8jxbTxqmQ9s5wyioAEP85V7qbJFnn2uT8'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_11',
+    {
+      decimals: 18,
+      symbol: 'wMATIC',
+      name: 'Wrapped MATIC',
+      thumbnailUri: 'ipfs://QmchBnjRjpweznHes7bVKHwgzd8D6Q7Yzwf6KmA4KS6Dgi'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_12',
+    {
+      decimals: 18,
+      symbol: 'wMKR',
+      name: 'Wrapped MKR',
+      thumbnailUri: 'ipfs://QmPTob6YP9waErN4gMXqHg6ZyazSFB9CEojot4BB2XPpZJ'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_13',
+    {
+      decimals: 18,
+      symbol: 'wOKB',
+      name: 'Wrapped OKB',
+      thumbnailUri: 'ipfs://QmPpmuLw4i9qJLMmGjXrGxrcPBwTiNZgLGuh7kYXeyTdyA'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_14',
+    {
+      decimals: 18,
+      symbol: 'wPAX',
+      name: 'Wrapped PAX',
+      thumbnailUri: 'ipfs://QmZD5QDAeAUyyLYKiMmxD4vfWpVeYHctcbTkPmo4NudDHt'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_15',
+    {
+      decimals: 18,
+      symbol: 'wSUSHI',
+      name: 'Wrapped SUSHI',
+      thumbnailUri: 'ipfs://QmTpss9a4uL3op7x5Lte7CcTKUSUhZsM1Gr34BWNvZCfy4'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_16',
+    {
+      decimals: 18,
+      symbol: 'wUNI',
+      name: 'Wrapped UNI',
+      thumbnailUri: 'ipfs://QmQBezdVvotCGnFHgQNKduLdxEJhfgruSEqtwnWY7mESb2'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_17',
+    {
+      decimals: 6,
+      symbol: 'wUSDC',
+      name: 'Wrapped USDC',
+      thumbnailUri: 'ipfs://QmQfHU9mYLRDU4yh2ihm3zrvVFxDrLPiXNYtMovUQE2S2t'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_18',
+    {
+      decimals: 6,
+      symbol: 'wUSDT',
+      name: 'Wrapped USDT',
+      thumbnailUri: 'ipfs://QmVbiHa37pe2U9FfXBYfvrLNpb38rbXwaN19HwZD2speFA'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_19',
+    {
+      decimals: 8,
+      symbol: 'wWBTC',
+      name: 'Wrapped WBTC',
+      thumbnailUri: 'ipfs://Qmdj6n9T48LDWex8NkBMKUQJfZgardxZVdtRRibYQVzLCJ'
+    }
+  ],
+  [
+    'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ_20',
+    {
+      decimals: 18,
+      symbol: 'wWETH',
+      name: 'Wrapped WETH',
+      thumbnailUri: 'ipfs://Qmezz1ztvo5JFshHupBEdUzVppyMfJH6K4kPjQRSZp8cLq'
+    }
+  ],
+  [
+    'KT1GRSvLoikDsXujKgZPsGLX8k8VvR2Tq95b_0',
+    {
+      decimals: 18,
+      symbol: 'PLENTY',
+      name: 'Plenty DAO',
+      thumbnailUri: 'https://raw.githubusercontent.com/Plenty-DeFi/Plenty-Logo/main/PlentyTokenIcon.png'
+    }
+  ],
+  [
+    'KT1A5P4ejnLix13jtadsfV9GCnXLMNnab8UT_0',
+    {
+      decimals: 10,
+      symbol: 'KALAM',
+      name: 'Kalamint',
+      thumbnailUri: 'ipfs://Qme9FX9M7o2PZt9h6rvkUbfXoLpQr1HsuMQi6sL5Y75g3A'
+    }
+  ],
+  [
+    'KT1XPFjZqCULSnqfKaaYy8hJjeY63UNSGwXg_0',
+    {
+      decimals: 8,
+      symbol: 'crDAO',
+      name: 'Crunchy DAO',
+      thumbnailUri: 'ipfs://bafybeigulbzm5x72qtmckxqvd3ksk6q3vlklxjgpnvvnbcofgdp6qwu43u'
+    }
+  ],
+  [
+    'KT1JkoE42rrMBP9b2oDhbx6EUr26GcySZMUH_0',
+    {
+      decimals: 18,
+      symbol: 'kDAO',
+      name: 'Kolibri DAO',
+      thumbnailUri: 'https://kolibri-data.s3.amazonaws.com/kdao-logo.png'
+    }
+  ],
+  [
+    'KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW_0',
+    {
+      decimals: 12,
+      symbol: 'uUSD',
+      name: 'youves uUSD',
+      thumbnailUri: 'ipfs://QmbvhanNCxydZEbGu1RdqkG3LcpNGv7XYsCHgzWBXnmxRd'
+    }
+  ],
+  [
+    'KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW_1',
+    {
+      decimals: 12,
+      symbol: 'uDEFI',
+      name: 'youves uDEFI',
+      thumbnailUri: 'ipfs://QmNfosyixuXVG2TGmE7FmLGZGJhK2Q9qHZicsMixsbgqH1'
+    }
+  ],
+  [
+    'KT1GUNKmkrgtMQjJp3XxcmCj6HZBhkUmMbge_0',
+    {
+      decimals: 6,
+      symbol: 'bDAO',
+      name: 'Bazaar DAO',
+      thumbnailUri: 'ipfs://QmVMyqkFKAeyfGBeR81h72ugqj4UM8hjqA7KaKcnc35Vtt'
+    }
+  ],
+  [
+    'KT1Xobej4mc6XgEjDoJoHtTKgbD1ELMvcQuL_0',
+    {
+      decimals: 12,
+      symbol: 'YOU',
+      name: 'youves YOU Governance',
+      thumbnailUri: 'ipfs://QmYAJaJvEJuwvMEgRbBoAUKrTxRTT22nCC9RuY7Jy4L4Gc'
+    }
+  ],
+  [
+    'KT19JYndHaesXpvUfiwgg8BtE41HKkjjGMRC_0',
+    {
+      decimals: 6,
+      symbol: 'RCKT',
+      name: 'Rocket',
+      thumbnailUri:
+        'https://gblobscdn.gitbook.com/assets%2F-MayY_wA8g4oLd9eVN9A%2F-MbbCn2yMCnBWfDTMUTb%2F-MbbDTyyuxM6uBAYA6ms%2FToken.png'
+    }
+  ],
+  [
+    'KT1MuyJ7gVw74FNJpfb2mHR15aCREdyEbe2e_0',
+    { decimals: 8, symbol: 'rkDAO', name: 'Rocket DAO', thumbnailUri: 'https://i.ibb.co/3m7xx5C/Rocket-9.png' }
+  ],
+  [
+    'KT1ErKVqEhG9jxXgUG2KGLW3bNM7zXHX8SDF_0',
+    {
+      decimals: 9,
+      symbol: 'UNO',
+      name: 'Unobtanium',
+      thumbnailUri: 'ipfs://QmXdQ3DaMsnqH7MEhX77foWfjxRnj8Qe88mwekkn4PSt3q'
+    }
+  ],
+  [
+    'KT1XTxpQvo7oRCqp85LikEZgAZ22uDxhbWJv_0',
+    {
+      decimals: 9,
+      symbol: 'GIF',
+      name: 'GIF DAO',
+      thumbnailUri: 'ipfs://QmQxoTVVuFS677TQJFdVh1PNRoBmVbvkwJSxe1xvf9cSqU'
+    }
+  ],
+  [
+    'KT1WapdVeFqhCfqwdHWwTzSTX7yXoHgiPRPU_0',
+    { decimals: 8, symbol: 'IDZ', name: 'TezID', thumbnailUri: 'https://tezid.net/idz.png' }
+  ],
+  [
+    'KT1QgAtLPu3SNq9c6DPLanwL5bvfX3rgh2CS_0',
+    {
+      decimals: 6,
+      symbol: 'EASY',
+      name: 'CryptoEasy',
+      thumbnailUri: 'https://cryptoeasy.io/img/cryptoeasy-logo.png'
+    }
+  ],
+  [
+    'KT19y6R8x53uDKiM46ahgguS6Tjqhdj2rSzZ_0',
+    {
+      decimals: 9,
+      symbol: 'INSTA',
+      name: 'Instaraise',
+      thumbnailUri: 'ipfs://QmYMMztcxtohk1t3p4X8DDX45REzeThL1TaJroH5RT5Chj'
+    }
+  ],
+  [
+    'KT1Rpviewjg82JgjGfAKFneSupjAR1kUhbza_0',
+    {
+      decimals: 18,
+      symbol: 'xPLENTY',
+      name: 'xPLENTY',
+      thumbnailUri: 'https://raw.githubusercontent.com/Plenty-DeFi/Plenty-Logo/main/xPlenty.png'
+    }
+  ],
+  [
+    'KT1SjXiUX63QvdNMcM2m492f7kuf8JxXRLp4_0',
+    {
+      decimals: 6,
+      symbol: 'ctez',
+      name: 'Ctez',
+      thumbnailUri: 'ipfs://Qme4ybadbY4H84h5WLPjdo47YQUxxVoJHWZrwYq2JZriM4'
+    }
+  ],
+  [
+    'KT19DUSZw7mfeEATrbWVPHRrWNVbNnmfFAE6_0',
+    {
+      decimals: 8,
+      symbol: 'PAUL',
+      name: 'PAUL Token',
+      thumbnailUri: 'ipfs://QmeoZ5ZnGnCMq8iGPeBjoS628c526DR37jnDstqEnTfkwC'
+    }
+  ],
+  [
+    'KT1CS2xKGHNPTauSh5Re4qE3N9PCfG5u4dPx_0',
+    {
+      decimals: 6,
+      symbol: 'SPI',
+      name: 'Spice Token',
+      thumbnailUri: 'ipfs://QmYQdCWFh26rPoaYNLFBa3mQg3EyXyfgJ5ktG4YrkbD4AZ'
+    }
+  ],
+  [
+    'KT1PnUZCp3u2KzWr93pn4DD7HAJnm3rWVrgn_0',
+    {
+      decimals: 6,
+      symbol: 'WTZ',
+      name: 'WTZ',
+      thumbnailUri: 'ipfs://bafybeidwsid6fvv4vxbqja7er3b4exsht5r7umv6hpz7rc3ujg7xilhwv4'
+    }
+  ],
+  [
+    'KT1Wa8yqRBpFCusJWgcQyjhRz7hUQAmFxW7j_0',
+    { decimals: 6, symbol: 'FLAME', name: 'FLAME', thumbnailUri: 'https://spacefarm.xyz/images/flamelogo.png' }
+  ],
+  [
+    'KT1KPoyzkj82Sbnafm6pfesZKEhyCpXwQfMc_0',
+    { decimals: 6, symbol: 'fDAO', name: 'fDAO', thumbnailUri: 'https://spacefarm.xyz/images/fdaologo.png' }
+  ],
+  [
+    'KT1F1mn2jbqQCJcsNgYKVAQjvenecNMY2oPK_0',
+    {
+      decimals: 6,
+      symbol: 'PXL',
+      name: 'Pixel',
+      thumbnailUri: 'https://cloudflare-ipfs.com/ipfs/Qma4wzNogtUkuhMgzyKBqzaYBqabLVZrHcLbLmJE38B5XH'
+    }
+  ],
+  [
+    'KT19ovJhcsUn4YU8Q5L3BGovKSixfbWcecEA_1',
+    {
+      decimals: 0,
+      symbol: 'sDAO',
+      name: 'Salsa DAO',
+      thumbnailUri: 'ipfs://QmPJ7dMS3T6McqPjjBioKhHqtEUEBhAXpcRf3aicaLNPtV'
+    }
+  ],
+  [
+    'KT1AM3PV1cwmGRw28DVTgsjjsjHvmL6z4rGh_0',
+    {
+      decimals: 6,
+      symbol: 'akaDAO',
+      name: 'akaSwap DAO',
+      thumbnailUri: 'ipfs://QmSpZ1rPgHMPpxUsBCFUM7oERHmDVXUz219no27jWqceH6'
+    }
+  ],
+  [
+    'KT1ErKVqEhG9jxXgUG2KGLW3bNM7zXHX8SDF_1',
+    {
+      decimals: 9,
+      symbol: 'MIN',
+      name: 'Minerals',
+      thumbnailUri: 'ipfs://QmXJdLDEec56EfJVtWW2XRcYBfkmWVHbCRBDAi7YTigisB'
+    }
+  ],
+  [
+    'KT1ErKVqEhG9jxXgUG2KGLW3bNM7zXHX8SDF_2',
+    {
+      decimals: 9,
+      symbol: 'ENR',
+      name: 'Energy',
+      thumbnailUri: 'ipfs://QmVjjDCrjBkETrWgXeL8NDg4DG2Qbi2hdQRpnWiCdS4qwg'
+    }
+  ],
+  [
+    'KT1ErKVqEhG9jxXgUG2KGLW3bNM7zXHX8SDF_3',
+    {
+      decimals: 9,
+      symbol: 'MCH',
+      name: 'Machinery',
+      thumbnailUri: 'ipfs://QmUYksqRFqUA5LUfxFs1oWLmP8Ti2SdyPAq5u72aAQLN7D'
+    }
+  ],
+  [
+    'KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW_2',
+    {
+      decimals: 12,
+      symbol: 'uBTC',
+      name: 'youves uBTC',
+      thumbnailUri: 'ipfs://Qmbev41h4axBqVzxsXP2NSaAF996bJjJBPb8FFZVqTvJTY'
+    }
+  ],
+  [
+    'KT1KRvNVubq64ttPbQarxec5XdS6ZQU4DVD2_0',
+    {
+      decimals: 6,
+      symbol: 'MTRIA',
+      name: 'Materia',
+      thumbnailUri: 'ipfs://QmRrvFBwRKK8cQ6yKXEX38Q34wrWK6FBbCvQPzun5e4kjP'
+    }
+  ],
+  [
+    'KT1Ha4yFVeyzw6KRAdkzq6TxDHB97KG4pZe8_0',
+    {
+      decimals: 5,
+      symbol: 'DOGA',
+      name: 'DOGAMI',
+      thumbnailUri: 'ipfs://QmW8sa5UygUKg58LLzK7NoEDtCRyAQU4wZh1rbpFa6j7kP'
+    }
   ]
 ]);
