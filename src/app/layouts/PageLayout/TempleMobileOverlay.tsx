075bd7958f7d6e984e17be484426a0a24e1d5898
LendiHop <mishin.danil2017@gmail.com>
2022-06-02 18:50:07 +0200

    Download Temple Mobile overlay + icon

--- src/app/layouts/PageLayout/TempleMobileOverlay.tsx
+++ src/app/layouts/PageLayout/TempleMobileOverlay.tsx
@@ -3,29 +3,32 @@ import React, { FC } from 'react';
 import classNames from 'clsx';
 
 import { Button } from 'app/atoms/Button';
-import { openInFullPage, useAppEnv } from 'app/env';
+import { useAppEnv } from 'app/env';
 import ContentContainer from 'app/layouts/ContentContainer';
 import { T } from 'lib/i18n/react';
 import { useTempleClient } from 'lib/temple/front';
 
+import { AnalyticsEventCategory, useAnalytics } from '../../../lib/analytics';
 import TempleMobileOverlayBgImg from './assets/temple-mobile-overlay-bg.png';
 import TempleMobileOverlayDevicesImg from './assets/temple-mobile-overlay-devices.png';
 import { useTempleMobile } from './hooks/useTempleMobile.hook';
+import { TempleMobileSelectors } from './TempleMobile.selectors';
 
 export const TempleMobileOverlay: FC = () => {
   const { popup } = useAppEnv();
   const { ready } = useTempleClient();
+  const { trackEvent } = useAnalytics();
   const { showTempleMobileOverlay, setShowTempleMobileOverlay, setIsTempleMobileOverlaySkipped } = useTempleMobile();
 
-  const handleSkipButtonClick = () => {
-    openInFullPage();
-    //setShowTempleMobileOverlay(false);
-    //setIsTempleMobileOverlaySkipped(true);
+  const handleSkip = () => {
+    setShowTempleMobileOverlay(false);
+    setIsTempleMobileOverlaySkipped(true);
+    trackEvent(TempleMobileSelectors.Skip, AnalyticsEventCategory.ButtonPress);
   };
-  const handleDownloadButtonClick = () => {
-    openInFullPage();
-    //setShowTempleMobileOverlay(false);
-    //setIsTempleMobileOverlaySkipped(false);
+  const handleDownload = () => {
+    setShowTempleMobileOverlay(false);
+    setIsTempleMobileOverlaySkipped(false);
+    trackEvent(TempleMobileSelectors.Download, AnalyticsEventCategory.ButtonPress);
   };
   const popupClassName = popup ? 'inset-0' : 'top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2';
 
@@ -37,13 +40,13 @@ export const TempleMobileOverlay: FC = () => {
         padding={!popup}
       >
         <div
-          className={classNames('flex flex-col bg-white rounded-md shadow-lg', popup && 'h-full')}
+          className={classNames('flex flex-col text-center bg-white shadow-lg', popup ? 'h-full' : 'rounded-md')}
           style={{
             backgroundImage: `url(${TempleMobileOverlayBgImg})`
           }}
         >
           <Button
-            onClick={handleSkipButtonClick}
+            onClick={handleSkip}
             className="font-inter font-normal text-sm text-gray-600 self-end mt-3 mr-6"
             style={{
               maxWidth: 'max-content'
@@ -54,26 +57,32 @@ export const TempleMobileOverlay: FC = () => {
           <img
             src={TempleMobileOverlayDevicesImg}
             alt="TempleMobileOverlayDevicesImg"
+            className={classNames('mx-auto', popup && 'mt-5')}
             style={{
-              width: 250,
-              height: 359
+              maxWidth: 250,
+              maxHeight: 359
             }}
           />
-          <p className="text-xl">
+          <p className="text-xl mt-6 font-inter font-semibold" style={{ fontSize: 28, color: '#FF5B00' }}>
             <T id="templeIsOnMobile" />
           </p>
-          <p className="text-sm">
+          <p className="mb-3 font-normal font-inter" style={{ fontSize: 12 }}>
             <T id="getTempleMobileAndroid" />
           </p>
-          <Button
-            className="mb-8 py-2 px-4 bg-primary-orange text-white"
-            onClick={handleDownloadButtonClick}
+          <a
+            href="https://templewallet.com/download"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mb-8 py-2 px-4 text-white font-inter rounded font-semibold uppercase mx-auto"
+            onClick={handleDownload}
             style={{
-              maxWidth: '7rem'
+              fontSize: 13,
+              maxWidth: '7rem',
+              backgroundColor: '#FF5B00'
             }}
           >
             <T id="download" />
-          </Button>
+          </a>
         </div>
       </ContentContainer>
     </>
