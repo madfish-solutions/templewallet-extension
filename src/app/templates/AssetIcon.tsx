7f8e47e41e49ca888e97e47872b4d92f62da523a
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-13 09:50:40 +0200

    Asset icon fallback (#666)
    * added loading
    
    * upd
    
    * fix
    
    * attempt to fix gifs
    
    * remove dev logs

--- src/app/templates/AssetIcon.tsx
+++ src/app/templates/AssetIcon.tsx
@@ -2,7 +2,12 @@ import React, { FC, useState } from 'react';
 
 import Identicon from 'app/atoms/Identicon';
 import { ReactComponent as CollectiblePlaceholder } from 'app/icons/collectible-placeholder.svg';
-import { formatCollectibleUri, formatIpfsUri, formatTokenUri } from 'lib/image-uri';
+import {
+  formatCollectibleObjktBigUri,
+  formatCollectibleObjktMediumUri,
+  formatIpfsUri,
+  formatTokenUri
+} from 'lib/image-uri';
 import { AssetMetadata, getAssetSymbol, useAssetMetadata } from 'lib/temple/front';
 
 interface AssetIconPlaceholderProps {
@@ -26,17 +31,55 @@ interface AssetIconProps {
   size?: number;
 }
 
+interface LoadStrategy {
+  type: string;
+  uri: (value: string) => string;
+  field: 'thumbnailUri' | 'artifactUri' | 'displayUri' | 'assetSlug';
+}
+
+const tokenLoadStrategy: Array<LoadStrategy> = [
+  { type: 'token', uri: formatTokenUri, field: 'thumbnailUri' },
+  { type: 'thumbnailUri', uri: formatIpfsUri, field: 'thumbnailUri' }
+];
+const collectibleLoadStrategy: Array<LoadStrategy> = [
+  { type: 'objktBig', uri: formatCollectibleObjktBigUri, field: 'assetSlug' },
+  { type: 'objktMed', uri: formatCollectibleObjktMediumUri, field: 'assetSlug' },
+  { type: 'displayUri', uri: formatIpfsUri, field: 'displayUri' },
+  { type: 'artifactUri', uri: formatIpfsUri, field: 'artifactUri' },
+  { type: 'thumbnailUri', uri: formatIpfsUri, field: 'thumbnailUri' }
+];
+
+type ImageRequestObject = (AssetMetadata | null) & { assetSlug: string };
+
+const getFirstFallback = (
+  strategy: Array<LoadStrategy>,
+  currentState: Record<string, boolean>,
+  metadata: ImageRequestObject
+): LoadStrategy => {
+  for (const strategyItem of strategy) {
+    if (metadata && metadata[strategyItem.field] && !currentState[strategyItem.type]) {
+      return strategyItem;
+    }
+  }
+  return strategy[0];
+};
+
 export const AssetIcon: FC<AssetIconProps> = ({ assetSlug, className, size }) => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [isLoadingFailed, setIsLoadingFailed] = useState(false);
   const metadata: AssetMetadata | null = useAssetMetadata(assetSlug);
   const isCollectible = Boolean(metadata?.artifactUri);
+  const loadStrategy = isCollectible ? collectibleLoadStrategy : tokenLoadStrategy;
+  const [isLoadingFailed, setIsLoadingFailed] = useState(
+    loadStrategy.reduce<Record<string, boolean>>((acc, cur) => ({ ...acc, [cur.type]: false }), {})
+  );
 
-  const imageSrc = isLoadingFailed
-    ? formatIpfsUri(metadata?.thumbnailUri)
-    : isCollectible
-    ? formatCollectibleUri(assetSlug)
-    : formatTokenUri(metadata?.thumbnailUri);
+  const imageRequestObject: ImageRequestObject = { ...metadata, assetSlug };
+  const currentFallback = getFirstFallback(loadStrategy, isLoadingFailed, imageRequestObject);
+  const imageSrc = currentFallback.uri(imageRequestObject[currentFallback.field] ?? assetSlug);
+
+  const handleLoadingFailed = () => {
+    setIsLoadingFailed(prevState => ({ ...prevState, [currentFallback.type]: true }));
+  };
 
   return (
     <div className={className}>
@@ -53,7 +96,7 @@ export const AssetIcon: FC<AssetIconProps> = ({ assetSlug, className, size }) =>
           height={size}
           width={size}
           onLoad={() => setIsLoaded(true)}
-          onError={() => setIsLoadingFailed(true)}
+          onError={handleLoadingFailed}
         />
       )}
       {(!isLoaded || !metadata || imageSrc === '') && <AssetIconPlaceholder metadata={metadata} size={size} />}
