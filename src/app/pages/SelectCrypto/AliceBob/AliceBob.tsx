46d6b197134f39d83c72d1f5a1146550f3f8a57d
LendiHop <mishin.danil2017@gmail.com>
2022-06-20 17:33:31 +0200

    wip

--- src/app/pages/SelectCrypto/AliceBob/AliceBob.tsx
+++ src/app/pages/SelectCrypto/AliceBob/AliceBob.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 
 import classNames from 'clsx';
 
 import { AnalyticsEventCategory, useAnalytics } from 'lib/analytics';
 
 import { T } from '../../../../lib/i18n/react';
+import { Button } from '../../../atoms/Button';
 import { TopUpInput } from '../../../atoms/TopUpInput/TopUpInput';
 import PageLayout from '../../../layouts/PageLayout';
 import styles from '../../BuyCrypto/BuyCrypto.module.css';
@@ -12,6 +13,22 @@ import { SelectCryptoSelectors } from '../SelectCrypto.selectors';
 
 export const AliceBob = () => {
   const { trackEvent } = useAnalytics();
+
+  const [amount, setAmount] = useState(0);
+  const [disabledProceed, setDisableProceed] = useState(true);
+
+  const onAmountChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setDisableProceed(true);
+      setAmount(Number(e.target.value));
+    },
+    [setAmount, setDisableProceed]
+  );
+
+  const onButtonClick = useCallback(() => {
+    trackEvent(SelectCryptoSelectors.AliceBob, AnalyticsEventCategory.ButtonPress);
+    window.open('https://www.google.com/', '_blank');
+  }, [trackEvent]);
 
   return (
     <PageLayout
@@ -21,48 +38,55 @@ export const AliceBob = () => {
         </div>
       }
     >
-      <h3>
-        <T id="enterAmount" />
-      </h3>
-      <TopUpInput type="fiat" currency="UAH" minAmount="500.00" maxAmount="29500.00" />
-      <a
-        className={classNames(
-          'shadow-sm hover:shadow focus:shadow',
-          'py-2 px-4 rounded mt-4',
-          'border-2',
-          'border-blue-500 hover:border-blue-600 focus:border-blue-600',
-          'flex items-center justify-center',
-          'text-white',
-          'text-base font-medium',
-          'transition ease-in-out duration-300',
-          'bg-blue-500',
-          'w-full'
-        )}
-        href=""
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={() => trackEvent(SelectCryptoSelectors.AliceBob, AnalyticsEventCategory.ButtonPress)}
-      >
-        <T id="next" />
-      </a>
-      <div>
-        <p>
-          <T
-            id="privacyAndPolicyLinks"
-            substitutions={[
-              <T id={'next'} />,
-              <a className={styles['link']} rel="noreferrer" href="https://exolix.com/terms" target="_blank">
-                <T id={'termsOfUse'} />
-              </a>,
-              <a className={styles['link']} rel="noreferrer" href="https://exolix.com/privacy" target="_blank">
-                <T id={'privacyPolicy'} />
-              </a>
-            ]}
-          />
-        </p>
-        <p className="mt-6 text-gray-600">
-          <T id={'warningTopUpServiceMessage'} />
-        </p>
+      <div className="flex" style={{ maxWidth: 360 }}>
+        <h3>
+          <T id="enterAmount" />
+        </h3>
+        <TopUpInput
+          type="fiat"
+          currency="UAH"
+          minAmount="500.00"
+          maxAmount="29500.00"
+          amount={amount}
+          onChangeInputHandler={onAmountChange}
+        />
+        <Button
+          className={classNames(
+            'shadow-sm hover:shadow focus:shadow',
+            'py-2 px-4 rounded mt-4',
+            'border-2',
+            'border-blue-500 hover:border-blue-600 focus:border-blue-600',
+            'flex items-center justify-center',
+            'text-white',
+            'text-base font-medium',
+            'transition ease-in-out duration-300',
+            'bg-blue-500',
+            'w-full'
+          )}
+          disabled={disabledProceed}
+          onClick={onButtonClick}
+        >
+          <T id="next" />
+        </Button>
+        <div>
+          <p>
+            <T
+              id="privacyAndPolicyLinks"
+              substitutions={[
+                <T id={'next'} />,
+                <a className={styles['link']} rel="noreferrer" href="https://exolix.com/terms" target="_blank">
+                  <T id={'termsOfUse'} />
+                </a>,
+                <a className={styles['link']} rel="noreferrer" href="https://exolix.com/privacy" target="_blank">
+                  <T id={'privacyPolicy'} />
+                </a>
+              ]}
+            />
+          </p>
+          <p className="mt-6 text-gray-600">
+            <T id={'warningTopUpServiceMessage'} />
+          </p>
+        </div>
       </div>
     </PageLayout>
   );
