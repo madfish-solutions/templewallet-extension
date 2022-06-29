5ff1ecc8d8ff7ccd0c30b12bc9395db2ce0dec3b
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-29 08:21:16 +0100

    fix types

--- src/lib/tzkt/client.ts
+++ src/lib/tzkt/client.ts
@@ -14,7 +14,7 @@ import { TzktAccountTokenBalance, TzktTokenTransfer } from '.';
 
 export const TZKT_API_BASE_URLS = new Map([
   [TempleChainId.Mainnet, 'https://api.tzkt.io/v1'],
-  [TempleChainId.Ithacanet, 'https://api.ithacanet.tzkt.io/v1']
+  [TempleChainId.Ghostnet, 'https://api.ghostnet.tzkt.io/v1']
 ]);
 
 const api = axios.create();
