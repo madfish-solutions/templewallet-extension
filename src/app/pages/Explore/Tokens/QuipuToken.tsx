17d431615d0082dbee762f12271018659fe278be
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-07-01 11:08:39 +0100

    remove quipu apy

--- src/app/pages/Explore/Tokens/QuipuToken.tsx
+++ src/app/pages/Explore/Tokens/QuipuToken.tsx
@@ -1,48 +1,5 @@
-import React, { FC, useCallback, useEffect } from 'react';
-
-import classNames from 'clsx';
-
-import { T } from 'lib/i18n/react';
-import { useRetryableSWR } from 'lib/swr';
-import { usePassiveStorage } from 'lib/temple/front';
-import { getQuipuStakingInfo } from 'lib/templewallet-api/quipu-staking';
-
-import styles from './Tokens.module.css';
-
-const QUIPU_DEFAULT_PERCENTAGE = '13.5';
+import React, { FC } from 'react';
 
 export const QuipuToken: FC = () => {
-  const getQuipuStaking = useCallback(async () => {
-    return await getQuipuStakingInfo();
-  }, []);
-  const { data: quipuStakingInfo, isValidating: loadingQuipuStaking } = useRetryableSWR(
-    ['baking-history'],
-    getQuipuStaking,
-    { suspense: true, revalidateOnFocus: false, revalidateOnReconnect: false }
-  );
-  const [quipuApy, setQuipuApy] = usePassiveStorage('quipu_apy', QUIPU_DEFAULT_PERCENTAGE);
-
-  useEffect(() => {
-    if (!loadingQuipuStaking && quipuStakingInfo && quipuStakingInfo.item) {
-      setQuipuApy(quipuStakingInfo.item.apy.toFixed(2));
-    }
-  }, [quipuStakingInfo, loadingQuipuStaking, setQuipuApy]);
-
-  return (
-    <a
-      className={classNames('ml-1 px-2 py-1', styles['apyBadge'])}
-      href="https://quipuswap.com/farming/3"
-      target="_blank"
-      rel="noopener noreferrer"
-      onClick={e => e.stopPropagation()}
-    >
-      <T id="tokenApy">
-        {message => (
-          <span>
-            {message}: {quipuApy}%{' '}
-          </span>
-        )}
-      </T>
-    </a>
-  );
+  return <></>;
 };
