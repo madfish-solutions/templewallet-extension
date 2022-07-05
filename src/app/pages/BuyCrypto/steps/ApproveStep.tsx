b1991646a749ee53facbc4d37d9aca174b05224f
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-07-05 12:41:52 +0100

    timer fix

--- src/app/pages/BuyCrypto/steps/ApproveStep.tsx
+++ src/app/pages/BuyCrypto/steps/ApproveStep.tsx
@@ -73,7 +73,7 @@ const ApproveStep: FC<Props> = ({ exchangeData, setExchangeData, setStep, isErro
                 {props.minutes}:{props.seconds < 10 ? '0' + props.seconds : props.seconds}
               </p>
             )}
-            date={exchangeData.created_at * 1000 + 3600000}
+            date={exchangeData.created_at + 3600000}
             onComplete={async () => {
               const data = await getExchangeData(exchangeData.id);
               setExchangeData(data);
