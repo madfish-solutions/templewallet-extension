6e2dbdc30312cdf2f5cfe9c12362726e83739d08
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-07-14 10:24:18 +0100

    remove dev localhost

--- src/lib/templewallet-api/templewallet-query.ts
+++ src/lib/templewallet-api/templewallet-query.ts
@@ -1,4 +1,3 @@
 import makeBuildQueryFn from 'lib/makeBuildQueryFn';
 
-// export const templewalletQuery = makeBuildQueryFn<Record<string, unknown>, any>('https://api.templewallet.com/api');
-export const templewalletQuery = makeBuildQueryFn<Record<string, unknown>, any>('http://localhost:8080/api');
+export const templewalletQuery = makeBuildQueryFn<Record<string, unknown>, any>('https://api.templewallet.com/api');
