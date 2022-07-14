309dcba6b818292a1a52f1b57f9bc22717798810
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-07-14 10:49:10 +0100

    fix dev console error of nested links

--- src/app/pages/Explore/Tokens/TezosToken.tsx
+++ src/app/pages/Explore/Tokens/TezosToken.tsx
@@ -2,13 +2,21 @@ import React, { FC } from 'react';
 
 import classNames from 'clsx';
 
+import { Button } from 'app/atoms/Button';
 import { T } from 'lib/i18n/react';
-import { Link } from 'lib/woozie';
+import { navigate } from 'lib/woozie';
 
 import styles from './Tokens.module.css';
 
 export const TezosToken: FC = () => (
-  <Link to="/explore/tez/?tab=delegation" className={classNames('ml-1 px-2 py-1', styles['apyBadge'])}>
+  <Button
+    onClick={e => {
+      e.preventDefault();
+      e.stopPropagation();
+      navigate('/explore/tez/?tab=delegation');
+    }}
+    className={classNames('ml-1 px-2 py-1', styles['apyBadge'])}
+  >
     {<T id="delegate" />}
-  </Link>
+  </Button>
 );
