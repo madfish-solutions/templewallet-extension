d14cf4d8743371af1331d25bdbbd7f6c0d2017e8
letier3110 <letier3110@gmail.com>
2022-07-18 14:25:56 +0100

    fix abtest

--- src/lib/temple/front/ab-test.provider.ts
+++ src/lib/temple/front/ab-test.provider.ts
@@ -3,38 +3,34 @@ import { useEffect, useMemo } from 'react';
 import constate from 'constate';
 
 import { useAnalyticsState } from 'lib/analytics/use-analytics-state.hook';
-import { useRetryableSWR } from 'lib/swr';
 import { ABTestGroup, getABGroup } from 'lib/templewallet-api';
 
 import { usePassiveStorage } from './storage';
 
 export function useAB() {
-  const { data: abGroup, loading } = useABGroup();
+  const abGroup = useABGroup();
   const { analyticsState } = useAnalyticsState();
-
-  const [localABGroup, setLocalABGroup] = usePassiveStorage<ABTestGroup>('ab-test-value', ABTestGroup.Unknown);
-
-  useEffect(() => {
-    if (!loading && !localABGroup) {
-      setLocalABGroup(abGroup);
-    }
-  }, [localABGroup, loading, setLocalABGroup, abGroup]);
 
   return useMemo(() => {
     if (analyticsState.enabled) {
-      return localABGroup;
+      return abGroup;
     }
     return ABTestGroup.Unknown;
-  }, [analyticsState, localABGroup]);
+  }, [analyticsState, abGroup]);
 }
 
 export const [ABTestGroupProvider, useABGroup] = constate((params: { suspense?: boolean }) => {
-  const { data, isValidating: loading } = useRetryableSWR('ab-test-group', fetchABGroup, {
-    revalidateOnMount: false,
-    dedupingInterval: 30_000,
-    suspense: params.suspense
-  });
-  return { data: data?.ab ?? ABTestGroup.Unknown, loading };
+  const [localABGroup, setLocalABGroup] = usePassiveStorage<ABTestGroup>('ab-test-value', ABTestGroup.Unknown);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchABGroup();
+      if (localABGroup === ABTestGroup.Unknown) setLocalABGroup(data?.ab ?? ABTestGroup.A);
+    };
+    getData();
+  }, [setLocalABGroup, localABGroup]);
+
+  return localABGroup;
 });
 
 async function fetchABGroup() {
