8d78d406b8a2951df3c44925256dcbde587620e5
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-07-20 08:58:41 +0100

    fix ab container

--- src/app/atoms/ABContainer.tsx
+++ src/app/atoms/ABContainer.tsx
@@ -4,15 +4,13 @@ import { useAB } from 'lib/temple/front';
 import { ABTestGroup } from 'lib/templewallet-api';
 
 interface ABContainerProps {
-  noAnalyticsComponent: ReactNode;
   groupAComponent: ReactNode;
   groupBComponent: ReactNode;
 }
 
-const ABContainer: FC<ABContainerProps> = ({ noAnalyticsComponent, groupAComponent, groupBComponent }) => {
+const ABContainer: FC<ABContainerProps> = ({ groupAComponent, groupBComponent }) => {
   const abGroup = useAB();
-  if (abGroup === ABTestGroup.Unknown) return <>{noAnalyticsComponent}</>;
-  return abGroup === ABTestGroup.A ? <>{groupAComponent}</> : <>{groupBComponent}</>;
+  return abGroup === ABTestGroup.B ? <>{groupBComponent}</> : <>{groupAComponent}</>;
 };
 
 export default ABContainer;
