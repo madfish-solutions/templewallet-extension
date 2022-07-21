42b4d6a6eeca426af6773ec0c4bc8403c7677a5e
Andrii Ivaniv <unixvb@gmail.com>
2022-07-21 16:52:08 +0300

    ba;ance revalidation fix

--- src/app/a11y/BootAnimation.tsx
+++ src/app/a11y/BootAnimation.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useLayoutEffect, useState } from 'react';
 
-import classNames from 'clsx';
 import CSSTransition from 'react-transition-group/CSSTransition';
 
 import { PropsWithChildren } from 'lib/props-with-children';
@@ -13,15 +12,7 @@ const BootAnimation: FC<PropsWithChildren> = ({ children }) => {
   }, [setBooted]);
 
   return (
-    <CSSTransition
-      in={booted}
-      timeout={200}
-      classNames={{
-        enter: 'opacity-0',
-        enterActive: classNames('opacity-100', 'transition ease-out duration-200'),
-        exit: classNames('opacity-0', 'transition ease-in duration-200')
-      }}
-    >
+    <CSSTransition in={booted} timeout={200}>
       {children}
     </CSSTransition>
   );
