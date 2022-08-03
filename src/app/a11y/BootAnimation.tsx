e8934400ff16ae4e8dc105ca7d7c8ba273372777
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-08-03 12:22:44 +0100

    fix

--- src/app/a11y/BootAnimation.tsx
+++ src/app/a11y/BootAnimation.tsx
@@ -1,16 +1,20 @@
-import React, { FC, useLayoutEffect, useState, useTransition } from 'react';
+import React, { FC, useLayoutEffect, useRef, useState } from 'react';
 
 import CSSTransition from 'react-transition-group/CSSTransition';
 
 import { PropsWithChildren } from 'lib/props-with-children';
 
 const BootAnimation: FC<PropsWithChildren> = ({ children }) => {
-  const [, startTransition] = useTransition();
+  const firstRenderRef = useRef(true);
   const [booted, setBooted] = useState(false);
 
   useLayoutEffect(() => {
-    startTransition(() => setBooted(true));
-  }, []);
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false;
+      return;
+    }
+    setBooted(true);
+  }, [setBooted]);
 
   return (
     <CSSTransition in={booted} timeout={200}>
