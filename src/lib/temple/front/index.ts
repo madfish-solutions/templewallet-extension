914f5e57dc18ca20a9eb70100a9293b8ac3e29fc
alex-seleznov <alex.seleznov@gmail.com>
2022-09-29 16:24:51 +0300

    TW-249: Find deadcode. Refactor

--- src/lib/temple/front/index.ts
+++ src/lib/temple/front/index.ts
@@ -1,4 +1,9 @@
 export { useLocalStorage } from './local-storage';
+
+export { useStorage, fetchFromStorage, putToStorage } from './storage';
+
+export { useTempleClient, request, assertResponse } from './client';
+
 export {
   ReactiveTezosToolkit,
   ActivationStatus,
@@ -14,10 +19,7 @@ export {
   useRelevantAccounts,
   useCustomChainId
 } from './ready';
-export { useStorage, fetchFromStorage, putToStorage } from './storage';
-export { useTempleClient, request, assertResponse } from './client';
-export { useAssetUSDPrice, useUSDPrices } from './usdprice';
-export { useBlockTriggers, useOnBlock } from './chain';
+
 export {
   useAssetMetadata,
   useTokensMetadata,
@@ -28,21 +30,40 @@ export {
   searchAssets,
   useGetTokenMetadata
 } from './assets';
+
+export { useAssetUSDPrice, useUSDPrices } from './usdprice';
+
+export { useBlockTriggers, useOnBlock } from './chain';
+
 export { useBalance, useBalanceSWRKey, getBalanceSWRKey } from './balance';
+
 export { useContacts, searchContacts } from './address-book';
+
 export { useTezosDomainsClient, isDomainNameValid } from './tzdns';
+
 export type { Baker } from './baking';
 export { getRewardsStats, useKnownBaker, useKnownBakers, useDelegate } from './baking';
+
 export { activateAccount } from './activate-account';
+
 export type { BlockExplorer } from './blockexplorer';
 export { BLOCK_EXPLORERS, useBlockExplorer, useExplorerBaseUrls } from './blockexplorer';
+
 export type { RawOperationAssetExpense, RawOperationExpenses } from './expenses';
 export { tryParseExpenses } from './expenses';
+
 export { useFungibleTokensBalances } from './fungible-tokens-balances';
+
 export { useNonFungibleTokensBalances } from './non-fungible-tokens-balances';
+
 export { TempleProvider } from './provider';
+
 export { ABTestGroupProvider, useAB } from './ab-test.provider';
+
 export { validateDelegate } from './validate-delegate';
+
 export { useSecretState } from './use-secret-state.hook';
+
 export { useFilteredContacts } from './use-filtered-contacts.hook';
+
 export { decryptKukaiSeedPhrase } from './kukai';
