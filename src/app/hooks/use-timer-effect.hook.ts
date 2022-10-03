1fa4cabc323c70d078d57118c267d375ac2a9166
letier3110 <letier3110@gmail.com>
2022-10-03 13:40:28 +0100

    fix review

--- src/app/hooks/use-timer-effect.hook.ts
+++ src/app/hooks/use-timer-effect.hook.ts
@@ -1,8 +1,6 @@
 import { DependencyList, useEffect } from 'react';
 
-import { EmptyFn } from 'app/config/general';
-
-// import { useIsAuthorisedSelector } from '../store/wallet/wallet-selectors';
+import { EmptyFn } from 'app/utils/function.util';
 
 export const useTimerEffect = (callback: EmptyFn, refreshInterval: number, deps: DependencyList = []) =>
   useEffect(() => {
@@ -13,18 +11,3 @@ export const useTimerEffect = (callback: EmptyFn, refreshInterval: number, deps:
     return () => clearInterval(interval);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [...deps]);
-
-// export const useAuthorisedTimerEffect = (callback: EmptyFn, refreshInterval: number, deps: DependencyList = []) => {
-//   const isAuthorised = useIsAuthorisedSelector();
-
-//   useEffect(() => {
-//     if (isAuthorised) {
-//       callback();
-
-//       const interval = setInterval(callback, refreshInterval);
-
-//       return () => clearInterval(interval);
-//     }
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [isAuthorised, ...deps]);
-// };
