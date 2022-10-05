0b0271d46aaa4a72fb335b1923b28b7582d1efa8
letier3110 <letier3110@gmail.com>
2022-10-05 12:45:51 +0100

    fix api: localhost

--- src/lib/templewallet-api/templewallet-query.ts
+++ src/lib/templewallet-api/templewallet-query.ts
@@ -1,6 +1,5 @@
 import makeBuildQueryFn from 'lib/makeBuildQueryFn';
 
-// const templeWalletApi = 'https://api.templewallet.com/api';
-const templeWalletApi = 'http://localhost:3000/api';
+const templeWalletApi = 'https://api.templewallet.com/api';
 
 export const templewalletQuery = makeBuildQueryFn<Record<string, unknown>, any>(templeWalletApi);
