ecec542003cdb8d6d72c65da75051e4fc974810b
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-10-21 11:21:24 +0100

    TW-307: remove giganode rpc (#782)
    * remove giganode RPC
    
    * fix naming
    
    * added testnet and smartpy fix
    
    * testnets

--- src/lib/temple/networks.ts
+++ src/lib/temple/networks.ts
@@ -27,17 +27,8 @@ export const NETWORKS: TempleNetwork[] = [
     disabled: false
   },
   {
-    id: 'giganode-mainnet',
-    name: 'Mainnet @giganode.io',
-    description: 'Highly available Tezos Mainnet nodes operated by Giganode',
-    type: 'main',
-    rpcBaseURL: 'https://mainnet-tezos.giganode.io',
-    color: '#059669',
-    disabled: false
-  },
-  {
     id: 'smartpy-mainnet',
-    name: 'Mainnet @smartpy.io',
+    name: 'SmartPy Mainnet',
     description: 'SmartPy Mainnet',
     type: 'main',
     rpcBaseURL: 'https://mainnet.smartpy.io',
@@ -46,7 +37,7 @@ export const NETWORKS: TempleNetwork[] = [
   },
   {
     id: 'tezie-mainnet',
-    name: 'Mainnet @api.tez.ie',
+    name: 'ECAD Labs Mainnet',
     description: 'Highly available Tezos Mainnet nodes operated by ECAD Labs',
     type: 'main',
     rpcBaseURL: 'https://mainnet.api.tez.ie',
@@ -100,7 +91,7 @@ export const NETWORKS: TempleNetwork[] = [
   },
   {
     id: 'monday',
-    name: 'MondayNet',
+    name: 'MondayNet Testnet',
     description: `MondayNet ${getLastMonday()}`,
     type: 'test',
     rpcBaseURL: `https://rpc.mondaynet-${getLastMonday()}.teztnets.xyz/`,
@@ -109,7 +100,7 @@ export const NETWORKS: TempleNetwork[] = [
   },
   {
     id: 'daily',
-    name: 'DailyNet',
+    name: 'DailyNet Testnet',
     description: 'DailyNet',
     type: 'test',
     rpcBaseURL: `https://rpc.dailynet-${formatDateToRPCFormat(new Date())}.teztnets.xyz/`,
