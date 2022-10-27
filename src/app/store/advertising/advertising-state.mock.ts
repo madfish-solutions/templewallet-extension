f9100f8bda1b149c9e060fae8d87f70b529261f9
Andrii Ivaniv <unixvb@gmail.com>
2022-10-27 14:22:34 +0300

    fixing conflicts

--- src/app/store/advertising/advertising-state.mock.ts
+++ src/app/store/advertising/advertising-state.mock.ts
@@ -2,6 +2,7 @@ import { advertisingPromotionMock } from '../../interfaces/advertising-promotion
 import { createEntity } from '../create-entity';
 import { AdvertisingState } from './advertising-state';
 
+// ts-prune-ignore-next
 export const mockAdvertisingState: AdvertisingState = {
   activePromotion: createEntity(advertisingPromotionMock)
 };
