df4ab39611f301887ce512894135b1e4d117353a
alex-seleznov <alex.seleznov@gmail.com>
2022-12-06 21:35:03 +0200

    TW-400: Delegate tab fix for no delegations yet

--- src/lib/temple/front/baking.ts
+++ src/lib/temple/front/baking.ts
@@ -1,5 +1,6 @@
 import { useCallback, useMemo } from 'react';
 
+import { HttpResponseError, HttpRequestFailed } from '@taquito/http-utils';
 import BigNumber from 'bignumber.js';
 
 import {
@@ -19,12 +20,20 @@ export function useDelegate(address: string, suspense = true) {
   const getDelegate = useCallback(async () => {
     try {
       return await tezos.rpc.getDelegate(address);
-    } catch (err: any) {
-      if (err.status === 404) {
-        return null;
+    } catch (error: unknown) {
+      if (error instanceof HttpResponseError) {
+        if (error.status === 404) return null;
+      }
+      if (error instanceof HttpRequestFailed) {
+        /*
+          `@taquito/http-utils` package uses `@vespaiach/axios-fetch-adapter`,
+          which throws a SyntaxError in case of 404 response.
+          See: https://github.com/vespaiach/axios-fetch-adapter/issues/25
+        */
+        if (error.message.includes('SyntaxError: Unexpected end of JSON input')) return null;
       }
 
-      throw err;
+      throw error;
     }
   }, [address, tezos]);
 
