243925017576c282226ccaa65f491a2f4912dcaa
alex-seleznov <alex.seleznov@gmail.com>
2022-12-13 21:42:50 +0200

    TW-355: Tokens APY tags. Refactor

--- src/app/store/d-apps/utils.ts
+++ src/app/store/d-apps/utils.ts
@@ -1,74 +1,23 @@
-import { gql } from '@apollo/client';
-import { map, of, catchError } from 'rxjs';
+import { map } from 'rxjs';
 
-import { getApolloConfigurableClient } from 'lib/apollo';
+import { fetchKordFiTzBtcApy$ } from 'lib/apis/kord-fi';
+import { fetchApyFromYupana$ } from 'lib/apis/yupana';
 import { KNOWN_TOKENS_SLUGS } from 'lib/temple/assets';
 
 export const YUPANA_LEND_LINK = 'https://app.yupana.finance/lending';
 export const KORDFI_LEND_LINK = 'https://kord.fi/lend';
 
-// YUPANA API
+export const fetchKUSDApy$ = () => {
+  const slug = KNOWN_TOKENS_SLUGS.KUSD;
+  return fetchApyFromYupana$('KUSD').pipe(map(val => ({ [slug]: val })));
+};
 
-const YUPANA_API = 'https://mainnet-api.yupana.finance/v1/graphql/';
+export const fetchUSDTApy$ = () => {
+  const slug = KNOWN_TOKENS_SLUGS.USDT;
+  return fetchApyFromYupana$('USDT').pipe(map(val => ({ [slug]: val })));
+};
 
-const apolloYupanaClient = getApolloConfigurableClient(YUPANA_API);
-
-interface GetApyFromYupanaResponse {
-  asset: [{ rates: { supply_apy: string }[] }];
-}
-
-export const fetchKUSDApy$ = () => fetchApyFromYupana$(KNOWN_TOKENS_SLUGS.KUSD, 'KUSD', 2);
-
-export const fetchUSDTApy$ = () => fetchApyFromYupana$(KNOWN_TOKENS_SLUGS.USDT, 'USDT', 6);
-
-const fetchApyFromYupana$ = (slug: string, yName: string, yId: number) =>
-  apolloYupanaClient.query<GetApyFromYupanaResponse>(buildGetApyFromYupanaGqlQuery(yName, yId)).pipe(
-    map(data => {
-      const { rates } = data.asset[0];
-      const { supply_apy } = rates[0];
-      const apy = Number(supply_apy) / 10000000000000000;
-
-      return {
-        [slug]: Number(apy.toFixed(2))
-      };
-    }),
-    catchError(() => of({ [slug]: 0 }))
-  );
-
-const buildGetApyFromYupanaGqlQuery = (yName: string, yId: number) => gql`
-query Get${yName}Apy {
-  asset(where: { ytoken: { _eq: ${yId} } }) {
-    rates {
-      supply_apy
-    }
-  }
-}
-`;
-
-// KORD-FI API
-
-const KORD_FI_API = 'https://back-mainnet.kord.fi/v1/graphql';
-
-interface GetTzBtcApy {
-  contractInfo: [{ tzbtcDepositRate: number }];
-}
-
-const apolloKordFiClient = getApolloConfigurableClient(KORD_FI_API);
-
-export const fetchTzBtcApy$ = () =>
-  apolloKordFiClient.query<GetTzBtcApy>(getTzBtcApyQuery).pipe(
-    map(data => {
-      const { tzbtcDepositRate = 0 } = data?.contractInfo?.[0] ?? {};
-
-      return { [KNOWN_TOKENS_SLUGS.tzBTC]: Number(tzbtcDepositRate.toFixed(2)) };
-    }),
-    catchError(() => of({ [KNOWN_TOKENS_SLUGS.tzBTC]: 0 }))
-  );
-
-const getTzBtcApyQuery = gql`
-  query GetTzBtcApy {
-    contractInfo {
-      tzbtcDepositRate
-    }
-  }
-`;
+export const fetchTzBtcApy$ = () => {
+  const slug = KNOWN_TOKENS_SLUGS.tzBTC;
+  return fetchKordFiTzBtcApy$().pipe(map(val => ({ [slug]: val })));
+};
---
+++ src/lib/apis/yupana.ts
@@ -0,0 +1,44 @@
+import { gql } from '@apollo/client';
+import { map, of, catchError } from 'rxjs';
+
+import { getApolloConfigurableClient } from 'lib/apollo';
+
+const YUPANA_API = 'https://mainnet-api.yupana.finance/v1/graphql/';
+
+const apolloYupanaClient = getApolloConfigurableClient(YUPANA_API);
+
+enum TOKENS_IDS {
+  /** KT1XnTn74bUtxHfDtBmm2bGZAQfhPbvKWR8o_0 */
+  'USDT' = 6,
+  /** KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV_0 */
+  'KUSD' = 2
+}
+
+interface GetApyFromYupanaResponse {
+  asset: [{ rates: { supply_apy: string }[] }];
+}
+
+export const fetchApyFromYupana$ = (symbol: keyof typeof TOKENS_IDS) => {
+  const request = buildGetApyFromYupanaGqlQuery(TOKENS_IDS[symbol]);
+
+  return apolloYupanaClient.query<GetApyFromYupanaResponse>(request).pipe(
+    map(data => {
+      const { rates } = data.asset[0];
+      const { supply_apy } = rates[0];
+      const apy = Number(supply_apy) / 10000000000000000;
+
+      return Number(apy.toFixed(2));
+    }),
+    catchError(() => of(0))
+  );
+};
+
+const buildGetApyFromYupanaGqlQuery = (yId: number) => gql`
+query GetTokenApy {
+  asset(where: { ytoken: { _eq: ${yId} } }) {
+    rates {
+      supply_apy
+    }
+  }
+}
+`;
