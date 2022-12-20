45274509f8fb4e3ad743a60804984ef28fb7d5e3
alex-seleznov <alex.seleznov@gmail.com>
2022-12-20 17:47:37 +0200

    TW-320: Manifest v3 JSON. Keeping BG worker alive from extension pages too

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import 'lib/lock-up/run-checks';
-import 'lib/ledger/proxy/foreground';
-
 import { App } from 'app/App';
 import { WindowType, isPopupWindow, openInFullPage } from 'app/env';
 import { isPopupModeEnabled } from 'lib/popup-mode';
