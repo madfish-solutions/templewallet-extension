b5d47f64d3c6f5d0b60c0090a4ff3b1d98974229
alex-seleznov <alex.seleznov@gmail.com>
2022-12-29 01:38:19 +0200

    [e2e] Setup. Pipeline. Gave up 'mujo-code/puppeteer-headful'

--- e2e/src/utils/browser.utils.ts
+++ e2e/src/utils/browser.utils.ts
@@ -6,7 +6,5 @@ const EXTENSION_PATH = path.resolve(__dirname, '../../../dist/chrome_unpacked');
 export const initBrowser = () =>
   launch({
     headless: false,
-    /* See: https://github.com/mujo-code/puppeteer-headful */
-    executablePath: process.env.PUPPETEER_EXEC_PATH,
-    args: ['--no-sandbox', `--disable-extensions-except=${EXTENSION_PATH}`, `--load-extension=${EXTENSION_PATH}`]
+    args: [`--disable-extensions-except=${EXTENSION_PATH}`, `--load-extension=${EXTENSION_PATH}`]
   });
