a04c1a7ab070f1924e09e4b4f957a00679cc2c22
Alex <alex.seleznov@gmail.com>
2023-06-06 11:29:23 +0300

    TW-503: Buy section. Binance Connect. Fix api base URL

--- src/lib/apis/temple-static/axios-api.ts
+++ src/lib/apis/temple-static/axios-api.ts
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 import { EnvVars } from 'lib/env';
 
-export const axiosApi = axios.create({ baseURL: EnvVars.TEMPLE_WALLET_STATIC_API_URL + '/api' });
+export const axiosApi = axios.create({ baseURL: new URL('/api', EnvVars.TEMPLE_WALLET_STATIC_API_URL).href });
