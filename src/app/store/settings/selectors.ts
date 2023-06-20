99aedbc599fade85ddfa41f57f22f2f6c59a669d
svyatoslavtt <stiven22235@gmail.com>
2023-06-20 15:50:22 +0300

    TW-707: Remove unused code

--- src/app/store/settings/selectors.ts
+++ src/app/store/settings/selectors.ts
@@ -9,5 +9,3 @@ export const useBalanceModeSelector = () => useSelector(({ settings }) => settin
 export const useIsEnabledAdsBannerSelector = () => useSelector(({ settings }) => settings.isEnableAdsBanner);
 
 export const useOnRampPossibilitySelector = () => useSelector(({ settings }) => settings.isOnRampPossibility);
-
-export const useLastProjectBuildVersionSelector = () => useSelector(({ settings }) => settings.buildVersion);
