dd12f291954bdc023371b114c0fbb002d2b265b5
razbeikov <razbeikov.oleksandr@gmail.com>
2023-07-06 15:42:10 +0300

    delete useless code

--- src/lib/apis/objkt/queries.ts
+++ src/lib/apis/objkt/queries.ts
@@ -1,20 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const buildGetAllUserCollectiblesQuery = (address: string) => {
-  return gql`
-    query MyQuery {
-      token(where: {holders: {holder_address: {_eq: "${address}"}}}) {
-        fa_contract
-        token_id
-        listings_active(order_by: {price_xtz: asc}) {
-          currency_id
-          price
-        }
-      }
-    }
-  `;
-};
-
 export const buildGetCollectibleByAddressAndIdQuery = (address: string, tokenId: string) => gql`
   query MyQuery {
     token(where: { fa_contract: { _eq: "${address}" }, token_id: { _eq: "${tokenId}" } }) {
