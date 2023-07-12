2851121028d9867662bc83e7048efc0a33edcfaf
Alex <alex.seleznov@gmail.com>
2023-07-12 18:07:47 +0300

    TW-832: NFT page. Main info. Refactor

--- src/app/store/collectibles/state.ts
+++ src/app/store/collectibles/state.ts
@@ -3,18 +3,24 @@ import { createEntity, LoadableEntityState } from 'lib/store';
 
 export interface CollectibleDetails extends Pick<UserObjktCollectible, 'fa' | 'description'> {
   /** Cheepest listing */
-  listing: null | {
-    /** In atoms */
-    floorPrice: number;
-    currencyId: number;
-  };
-  creators: {
-    address: string;
-    tzDomain: string;
-  }[];
-  galleries: {
-    title: string;
-  }[];
+  listing: null | CollectibleDetailsListing;
+  creators: CollectibleDetailsCreator[];
+  galleries: CollectibleDetailsGallery[];
+}
+
+interface CollectibleDetailsListing {
+  /** In atoms */
+  floorPrice: number;
+  currencyId: number;
+}
+
+interface CollectibleDetailsCreator {
+  address: string;
+  tzDomain: string;
+}
+
+interface CollectibleDetailsGallery {
+  title: string;
 }
 
 export type CollectibleDetailsRecord = Record<string, CollectibleDetails>;
