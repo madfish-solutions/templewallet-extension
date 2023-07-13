4651c0afadf3c62e943b4647838b85b392076160
herkoss <herkodmytro@gmail.com>
2023-07-13 10:48:34 +0300

    TW-838 Fixed linter errors

--- src/app/store/tokens-metadata/epics.ts
+++ src/app/store/tokens-metadata/epics.ts
@@ -64,7 +64,7 @@ const loadOneTokenMetadataEpic = (action$: Observable<Action>) =>
     )
   );
 
-export const checkTokensMetadata$ = (tokensMetadata: TokenMetadata[], accountPublicKeyHash: string) =>
+const checkTokensMetadata$ = (tokensMetadata: TokenMetadata[], accountPublicKeyHash: string) =>
   fetchUserAdultCollectibles$(accountPublicKeyHash).pipe(
     map(adultCollectibles => {
       if (isNonEmptyArray(adultCollectibles)) {
