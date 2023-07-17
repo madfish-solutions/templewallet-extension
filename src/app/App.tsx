e05cfd6595bf5609b2f27371b541e66e0f187ac7
lendihop <mishin.danil2017@gmail.com>
2023-07-17 18:32:28 +0200

    workflow changes + refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,36 +17,31 @@ import { TempleProvider } from 'lib/temple/front';
 import { DialogsProvider } from 'lib/ui/dialog';
 import * as Woozie from 'lib/woozie';
 
-import { usePushNotifications } from './hooks/use-push-notifications';
 import { StoreProvider } from './store/provider';
 
 interface Props extends React.PropsWithChildren {
   env: ComponentProps<typeof AppEnvProvider>;
 }
 
-export const App: FC<Props> = ({ env }) => {
-  usePushNotifications();
+export const App: FC<Props> = ({ env }) => (
+  <ErrorBoundary whileMessage="booting a wallet" className="min-h-screen">
+    <DialogsProvider>
+      <Suspense fallback={<RootSuspenseFallback />}>
+        <AppProvider env={env}>
+          <Dialogs />
 
-  return (
-    <ErrorBoundary whileMessage="booting a wallet" className="min-h-screen">
-      <DialogsProvider>
-        <Suspense fallback={<RootSuspenseFallback />}>
-          <AppProvider env={env}>
-            <Dialogs />
+          <DisableOutlinesForClick />
 
-            <DisableOutlinesForClick />
+          <AwaitI18N />
 
-            <AwaitI18N />
-
-            <AwaitFonts name="Inter" weights={[300, 400, 500, 600]} className="antialiased font-inter">
-              <BootAnimation>{env.confirmWindow ? <ConfirmPage /> : <PageRouter />}</BootAnimation>
-            </AwaitFonts>
-          </AppProvider>
-        </Suspense>
-      </DialogsProvider>
-    </ErrorBoundary>
-  );
-};
+          <AwaitFonts name="Inter" weights={[300, 400, 500, 600]} className="antialiased font-inter">
+            <BootAnimation>{env.confirmWindow ? <ConfirmPage /> : <PageRouter />}</BootAnimation>
+          </AwaitFonts>
+        </AppProvider>
+      </Suspense>
+    </DialogsProvider>
+  </ErrorBoundary>
+);
 
 const AppProvider: FC<Props> = ({ children, env }) => (
   <AppEnvProvider {...env}>
--- src/app/hooks/use-push-notifications.ts
+++ src/app/hooks/use-push-notifications.ts
@@ -4,23 +4,35 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken } from 'firebase/messaging';
 import browser from 'webextension-polyfill';
 
+import { AnalyticsEventCategory, useAnalytics } from 'lib/analytics';
 import { EnvVars } from 'lib/env';
+import { useLocalStorage } from 'lib/ui/local-storage';
 
 export const usePushNotifications = () => {
+  const [fcmToken, setFcmToken] = useLocalStorage<string | undefined>('fcmToken', undefined);
+  const { trackEvent } = useAnalytics();
+
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator && 'Notification' in window) {
       initializeApp(JSON.parse(EnvVars.TEMPLE_FIREBASE_CONFIG));
       const messaging = getMessaging();
 
       Notification.requestPermission().then(async permission => {
         if (permission === 'granted') {
-          const token = await getToken(messaging, {
-            vapidKey: EnvVars.TEMPLE_FIREBASE_MESSAGING_VAPID_KEY,
-            serviceWorkerRegistration: await navigator.serviceWorker.getRegistration(
-              browser.runtime.getURL('background/index.js')
-            )
-          });
-          console.log(token);
+          if (!fcmToken) {
+            const token = await getToken(messaging, {
+              vapidKey: EnvVars.TEMPLE_FIREBASE_MESSAGING_VAPID_KEY,
+              serviceWorkerRegistration: await navigator.serviceWorker.getRegistration(
+                browser.runtime.getURL('background/index.js')
+              )
+            });
+
+            if (token) {
+              setFcmToken(token);
+            }
+
+            await trackEvent('PUSH_NOTIFICATIONS_ENABLED', AnalyticsEventCategory.General);
+          }
         }
       });
     }
