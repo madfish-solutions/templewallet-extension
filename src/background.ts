042cfcc248188883852de507f4d98607d8166aa1
lendihop <mishin.danil2017@gmail.com>
2023-07-18 17:48:03 +0200

    - concatenation

--- src/background.ts
+++ src/background.ts
@@ -25,7 +25,7 @@ globalThis.addEventListener('notificationclick', event => {
   // @ts-ignore
   event.notification.close();
   // @ts-ignore
-  event.waitUntil(clients.openWindow(event.target.registration.scope + 'fullpage.html'));
+  event.waitUntil(clients.openWindow(`${event.target.registration.scope}fullpage.html`));
 });
 
 const firebase = initializeApp(JSON.parse(EnvVars.TEMPLE_FIREBASE_CONFIG));
