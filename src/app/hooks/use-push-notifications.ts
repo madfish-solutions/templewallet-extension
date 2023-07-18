5816bce35a40ce8ffccd9605984f1928fc588a5d
lendihop <mishin.danil2017@gmail.com>
2023-07-18 17:16:02 +0200

    review fixes

--- src/app/hooks/use-push-notifications.ts
+++ src/app/hooks/use-push-notifications.ts
@@ -36,5 +36,5 @@ export const usePushNotifications = () => {
         }
       });
     }
-  }, []);
+  }, [trackEvent]);
 };
--- src/background.ts
+++ src/background.ts
@@ -2,17 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getMessaging } from 'firebase/messaging/sw';
 import browser from 'webextension-polyfill';
 
+import { EnvVars } from 'lib/env';
 import { start } from 'lib/temple/back/main';
-
-import { EnvVars } from './lib/env';
-
-// eslint-disable-next-line no-restricted-globals
-self.addEventListener('notificationclick', function (event) {
-  // @ts-ignore
-  event.notification.close();
-  // @ts-ignore
-  event.waitUntil(clients.openWindow(event.target.registration.scope + 'fullpage.html'));
-});
 
 browser.runtime.onInstalled.addListener(({ reason }) => (reason === 'install' ? openFullPage() : null));
 
@@ -29,6 +20,13 @@ function openFullPage() {
     url: browser.runtime.getURL('fullpage.html')
   });
 }
+
+globalThis.addEventListener('notificationclick', event => {
+  // @ts-ignore
+  event.notification.close();
+  // @ts-ignore
+  event.waitUntil(clients.openWindow(event.target.registration.scope + 'fullpage.html'));
+});
 
 const firebase = initializeApp(JSON.parse(EnvVars.TEMPLE_FIREBASE_CONFIG));
 getMessaging(firebase);
