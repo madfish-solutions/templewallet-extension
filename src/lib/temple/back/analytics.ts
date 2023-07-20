3be36dea205e1d3c82714f9a2acee6ac4eca4338
lendihop <mishin.danil2017@gmail.com>
2023-07-20 11:38:27 +0200

    source_ip strict

--- src/lib/temple/back/analytics.ts
+++ src/lib/temple/back/analytics.ts
@@ -9,7 +9,7 @@ const { TEMPLE_WALLET_JITSU_WRITE_KEY: WRITE_KEY, TEMPLE_WALLET_JITSU_TRACKING_H
 const client = jitsuClient({
   key: WRITE_KEY,
   tracking_host: TRACKING_HOST,
-  privacy_policy: 'strict'
+  ip_policy: 'strict'
 });
 
 export const trackEvent = async ({
