bf088d5866335fb33deada5d9b6a41ef857e36be
Alex <alex.seleznov@gmail.com>
2023-08-02 12:45:39 +0300

    TW-934: [bug] Redux persist. Refactor

--- src/lib/local-storage/index.ts
+++ src/lib/local-storage/index.ts
@@ -1,3 +1,5 @@
+import { isDefined } from '@rnw-community/shared';
+
 export const getLocalStorageUsageDetails = () => {
   const recordsNumber = localStorage.length;
   const keys = new Array(recordsNumber).fill(null).map((_, i) => localStorage.key(i));
@@ -5,7 +7,7 @@ export const getLocalStorageUsageDetails = () => {
   const keysTotalChars = keys.reduce((acc, key) => acc + (key?.length ?? 0), 0);
 
   const valuesTotalChars = keys.reduce((acc, key) => {
-    const value = key == null ? null : localStorage.getItem(key);
+    const value = isDefined(key) ? localStorage.getItem(key) : null;
     return acc + (value?.length ?? 0);
   }, 0);
 
