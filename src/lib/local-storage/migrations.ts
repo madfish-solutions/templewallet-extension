75b36ff4c8f81c63033b7614a10fbef4ed76fd22
Alex <alex.seleznov@gmail.com>
2023-08-02 09:49:00 +0300

    TW-934: [bug] Redux persist. + localStorage migrations

---
+++ src/lib/local-storage/migrations.ts
@@ -0,0 +1,23 @@
+import { isNotEmptyString } from '@rnw-community/shared';
+import * as localStorageMigrator from 'localstorage-migrator';
+import type { IMigration } from 'rstolsmark-json-migrator';
+
+const MIGRATIONS: IMigration[] = [
+  {
+    name: '1.17.4',
+    up: () => {
+      const match = (key: string) =>
+        /** `no_metadata_${slug}` */
+        key.startsWith('no_metadata_') ||
+        /** `${Rpc URL}_${Contract address}` // Deprecated cache from class FastRpcClient */
+        key.match(/https?:\/\/.*_KT[-a-zA-Z0-9]*$/);
+
+      const keys = new Array(localStorage.length).fill(null).map((_, i) => localStorage.key(i));
+      for (const key of keys) {
+        if (isNotEmptyString(key) && match(key)) localStorage.removeItem(key);
+      }
+    }
+  }
+];
+
+localStorageMigrator.migrate(MIGRATIONS);
