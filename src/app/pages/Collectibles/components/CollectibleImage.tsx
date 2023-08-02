9ec63ed334b41dab5c083c0843ed43ae78ced021
lendihop <mishin.danil2017@gmail.com>
2023-08-02 13:24:45 +0300

    merge conflicts resolve

--- src/app/pages/Collectibles/components/CollectibleImage.tsx
+++ src/app/pages/Collectibles/components/CollectibleImage.tsx
@@ -1,5 +1,6 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 
+import { ReactComponent as RevealEyeSvg } from 'app/icons/reveal-eye.svg';
 import { useAllCollectiblesDetailsLoadingSelector } from 'app/store/collectibles/selectors';
 import { AssetImage } from 'app/templates/AssetImage';
 import { AssetMetadataBase } from 'lib/metadata';
@@ -9,6 +10,7 @@ import { AnimatedSvg } from './AnimatedSvg';
 import { AudioCollectible } from './AudioCollectible';
 import { CollectibleImageFallback } from './CollectibleImageFallback';
 import { CollectibleImageLoader } from './CollectibleImageLoader';
+import BlurImageSrc from './CollectibleItemImage/Blur.png';
 import { ModelViewer } from './ModelViewer';
 import { VideoCollectible } from './VideoCollectible';
 
@@ -17,6 +19,7 @@ interface Props {
   metadata?: AssetMetadataBase;
   mime?: string | null;
   objktArtifactUri?: string;
+  isAdultContent?: boolean;
   large?: boolean;
   className?: string;
   style?: React.CSSProperties;
@@ -29,12 +32,27 @@ export const CollectibleImage: FC<Props> = ({
   assetSlug,
   large,
   className,
-  style
+  style,
+  isAdultContent = false
 }) => {
   const [isRenderFailedOnce, setIsRenderFailedOnce] = useState(false);
   const isDetailsLoading = useAllCollectiblesDetailsLoadingSelector();
 
+  const [shouldShowBlur, setShouldShowBlur] = useState<boolean>(isAdultContent);
+  const handleBlurClick = useCallback(() => setShouldShowBlur(false), []);
+
+  useEffect(() => setShouldShowBlur(isAdultContent), [isAdultContent]);
+
   const handleError = useCallback(() => setIsRenderFailedOnce(true), []);
+
+  if (shouldShowBlur) {
+    return (
+      <button onClick={handleBlurClick} className="relative flex justify-center items-center h-full w-full">
+        <img src={BlurImageSrc} alt="Adult content" className="h-full w-full" />
+        <RevealEyeSvg className="absolute z-10" color="#718096" />
+      </button>
+    );
+  }
 
   if (large && isDetailsLoading) {
     return <CollectibleImageLoader large={large} />;
