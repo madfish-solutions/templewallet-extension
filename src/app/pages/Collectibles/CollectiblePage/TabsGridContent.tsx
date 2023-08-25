52b344ecfe37cce0f4ab4343a256b052bf100bef
Alex <alex.seleznov@gmail.com>
2023-08-25 17:52:05 +0300

    TW-994: [QA Epic] NFT update. Fix QA. Attributes words break

--- src/app/pages/Collectibles/CollectiblePage/TabsGridContent.tsx
+++ src/app/pages/Collectibles/CollectiblePage/TabsGridContent.tsx
@@ -17,7 +17,7 @@ export const AttributesItems: FC<AttributesItemsProps> = ({ details }) => {
       {details?.attributes.map(attribute => (
         <div
           key={attribute.id}
-          className="flex flex-col items-center gap-y-1 p-2 border border-gray-300 rounded-md text-center"
+          className="flex flex-col justify-between gap-y-1 p-2 border border-gray-300 rounded-md text-center break-words"
         >
           <span className="text-xs text-gray-600 leading-5">{attribute.name}</span>
           <h6 className="text-base font-semibold leading-5">{attribute.value}</h6>
@@ -61,7 +61,7 @@ export const PropertiesItems: FC<PropertiesItemsProps> = ({ assetSlug, accountPk
 
   const itemClassName = 'flex flex-col gap-y-2 p-3 border border-gray-300 rounded-md';
   const itemTitleClassName = 'text-xs text-gray-600 leading-5';
-  const itemValueClassName = 'text-base font-semibold leading-5';
+  const itemValueClassName = 'text-base font-semibold leading-5 break-words';
 
   return (
     <>
