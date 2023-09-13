d89fd3f81c44e8616b071ab9e14338899f16f6e7
lendihop <mishin.danil2017@gmail.com>
2023-09-14 00:44:19 +0200

    added delay

--- e2e/src/step-definitions/change-node.steps.ts
+++ e2e/src/step-definitions/change-node.steps.ts
@@ -1,7 +1,7 @@
 import { Given } from '@cucumber/cucumber';
 
 import { Pages } from 'e2e/src/page-objects';
-import { MEDIUM_TIMEOUT, SHORT_TIMEOUT } from 'e2e/src/utils/timing.utils';
+import { MEDIUM_TIMEOUT, SHORT_TIMEOUT, sleep } from 'e2e/src/utils/timing.utils';
 
 Given(
   /I select (.*) node in the networks drop-down list on the Header page/,
@@ -13,6 +13,8 @@ Given(
 
 Given(/I check that (.*) node is selected correctly/, { timeout: MEDIUM_TIMEOUT }, async (networkName: string) => {
   await Pages.Header.selectedNetworkButtonName.waitForText(networkName, SHORT_TIMEOUT);
+  // dropdown close animation
+  await sleep(100);
 
   await Pages.Header.templeLogoButton.waitForDisplayed();
   await Pages.Header.accountIconButton.waitForDisplayed();
