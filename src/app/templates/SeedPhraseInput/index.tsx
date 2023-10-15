4557a9b8037f206bbdbe12d41d259d945e92b8d9
lendihop <mishin.danil2017@gmail.com>
2023-10-15 16:17:16 +0200

    fix Validation error  ‘Make sure the words spelled correctly’ doesn’t disappear after selecting seed phrase with less words if validation appeared for hidden inputs

--- src/app/templates/SeedPhraseInput/index.tsx
+++ src/app/templates/SeedPhraseInput/index.tsx
@@ -172,6 +172,7 @@ export const SeedPhraseInput: FC<SeedPhraseInputProps> = ({
               onSeedChange(newDraftSeed);
               reset();
               setSeedError('');
+              setWordSpellingError('');
             }}
           />
         </div>
