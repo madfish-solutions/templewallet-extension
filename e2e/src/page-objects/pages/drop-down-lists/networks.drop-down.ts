08d29b2cd680aa9bae8227b4f99811a1872a7307
lendihop <mishin.danil2017@gmail.com>
2023-11-01 14:29:49 +0100

    fix retry

--- e2e/src/page-objects/pages/drop-down-lists/networks.drop-down.ts
+++ e2e/src/page-objects/pages/drop-down-lists/networks.drop-down.ts
@@ -5,6 +5,8 @@ import { Page } from 'e2e/src/classes/page.class';
 import { createPageElement, findElement } from 'e2e/src/utils/search.utils';
 import { ONE_SECOND, RETRY_OPTIONS } from 'e2e/src/utils/timing.utils';
 
+const STILL_OPENED_ERROR_MESSAGE = 'Networks dropdown is still opened';
+
 export class NetworksDropDown extends Page {
   networkItemButton = createPageElement(NetworkSelectSelectors.networkItemButton);
 
@@ -12,22 +14,28 @@ export class NetworksDropDown extends Page {
     await this.networkItemButton.waitForDisplayed(timeout);
   }
 
+  async isClosed() {
+    retry(async bail => {
+      try {
+        await this.isVisible(ONE_SECOND);
+        throw new Error(STILL_OPENED_ERROR_MESSAGE);
+      } catch (e: any) {
+        if (e.message === STILL_OPENED_ERROR_MESSAGE) {
+          throw e;
+        } else {
+          bail(e); // Abort retry for other errors
+        }
+      }
+    }, RETRY_OPTIONS).catch(e => {
+      if (e.message === STILL_OPENED_ERROR_MESSAGE) {
+        throw e;
+      }
+    });
+  }
+
   async selectNetwork(name: string) {
     const networkItemElem = await findElement(NetworkSelectSelectors.networkItemButton, { name });
 
     await networkItemElem.click();
-  }
-
-  async isClosed() {
-    await retry(
-      () =>
-        this.isVisible(ONE_SECOND).then(
-          () => {
-            throw new Error(`Networks dropdown is still opened`);
-          },
-          () => undefined
-        ),
-      RETRY_OPTIONS
-    );
   }
 }
