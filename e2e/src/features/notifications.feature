8264612fe6f1213ba96a8dcbd95172cbd42e21e6
Vlad Novak <purch2442@gmail.com>
2023-11-07 17:41:07 +0200

    request created

---
+++ e2e/src/features/notifications.feature
@@ -0,0 +1,6 @@
+Feature: notif
+  @dev
+  Scenario: as aasd;d
+    Given I make request for creating a notification
+    And I have imported an existing account
+    And I press Notification Icon Button on the Home page
--- e2e/src/step-definitions/common.steps.ts
+++ e2e/src/step-definitions/common.steps.ts
@@ -1,6 +1,8 @@
 import { Given } from '@cucumber/cucumber';
 import axios from 'axios';
 
+import { envVars } from 'e2e/src/utils/env.utils';
+
 import { BrowserContext } from '../classes/browser-context.class';
 import { Pages } from '../page-objects';
 import { iEnterValues, IEnterValuesKey } from '../utils/input-data.utils';
@@ -65,20 +67,33 @@ Given(
 );
 
 Given(/I make request for creating a notification/, { timeout: MEDIUM_TIMEOUT }, async () => {
-  try {
-    const requestBody = {
-      key1: 'value1',
-      key2: 'value2'
-    };
+  const currentDate = new Date();
+  const currentDateISO = new Date().toISOString();
+  const expirationDateISO = new Date(currentDate.getTime() + 180000).toISOString(); // Notification will be deleted in 3 minutes
 
-    const response = await axios.post('https://example.com/api', requestBody, {
-      headers: {
-        'Content-Type': 'application/json' // Set the appropriate content type
-      }
-    });
+  const requestBody = {
+    mobile: 'off',
+    extension: 'on',
+    type: 'News',
+    title: 'Test Title',
+    description: 'Test description',
+    extensionImageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtIvsRFAUjlUKqlsLnrrJnWtcx98vOncHTXQ&usqp=CAU',
+    mobileImageUrl: '',
+    content: 'Test content',
+    date: currentDateISO,
+    expirationDate: expirationDateISO
+  };
 
-    console.log('Status Code:', response.status);
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  const response = await axios.post('https://temple-api-mainnet.stage.madfish.xyz/api/notifications', requestBody, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: envVars.NOTIFICATION_AUTHORIZATION
+    }
+  });
+
+  if (response.status !== 200)
+    throw new Error(
+      `Some problems with backend server. Server returns ${response.statusText} with ${response.status} status code`
+    );
 });
