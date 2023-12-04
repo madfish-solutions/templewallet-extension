1f8a4653ace3abfad9c9fb8950fcb47a5e934ae5
Inokentii Mazhara <keshan3262@gmail.com>
2023-12-04 14:19:27 +0200

    TW-1180 Fix slot id generation

--- src/lib/slise/get-slot-id.ts
+++ src/lib/slise/get-slot-id.ts
@@ -3,7 +3,7 @@ export const getSlotId = () => {
   const serviceId = hostnameParts[0];
   const pathnameParts = window.parent.location.pathname
     .split('/')
-    .filter(part => part !== '' && !/(0x)?[0-9a-f]+/i.test(part) && !/[0-9a-z]{30,}/i.test(part));
+    .filter(part => part !== '' && !/^(0x)?[0-9a-f]+$/i.test(part) && !/^[0-9a-z]{30,}$/i.test(part));
 
   return [serviceId, ...pathnameParts].join('-');
 };
