3586e9f8de2c54d93d3bd5e39f218e20b12b2332
Danyl Mishyn <35381314+lendihop@users.noreply.github.com>
2023-12-14 10:22:01 +0100

    TW-1219: Christmas theme  (#1035)
    * christmas theme applied
    
    * change bg image resolution
    
    * review fixes
    
    * style object memoization
    
    * after-test fixes
    
    * apply review suggestions
    
    * apply review suggestions

--- src/app/layouts/SimplePageLayout.tsx
+++ src/app/layouts/SimplePageLayout.tsx
@@ -6,43 +6,50 @@ import DocBg from 'app/a11y/DocBg';
 import Logo from 'app/atoms/Logo';
 import { useAppEnv } from 'app/env';
 import ContentContainer from 'app/layouts/ContentContainer';
+import ChristmasBgPopupImg from 'app/misc/christmas-popup-bg.png';
+
+const logoStyles = { height: 64, marginTop: 0, marginBottom: 0 };
 
 interface SimplePageLayoutProps extends PropsWithChildren {
   title: ReactNode;
 }
 
 const SimplePageLayout: FC<SimplePageLayoutProps> = ({ title, children }) => {
-  const appEnv = useAppEnv();
+  const { popup } = useAppEnv();
 
   return (
     <>
-      {!appEnv.fullPage && <DocBg bgClassName="bg-primary-white" />}
+      {popup && <DocBg bgClassName="bg-primary-white" />}
 
-      <ContentContainer className={classNames('min-h-screen', 'flex flex-col', !appEnv.fullPage && 'bg-primary-white')}>
-        <div className={classNames('mt-12 mb-10', 'flex flex-col items-center justify-center')}>
-          <div className="flex items-center">
-            <Logo hasTitle />
+      <ContentContainer
+        className={classNames('min-h-screen flex flex-col', popup && 'bg-gray-100 bg-no-repeat bg-contain')}
+        style={popup ? { backgroundImage: `url(${ChristmasBgPopupImg})` } : undefined}
+      >
+        <div className="mt-12 mb-10 flex flex-col items-center justify-center">
+          <div className={classNames('flex items-center', popup && 'bg-gray-100')}>
+            <Logo hasTitle style={logoStyles} />
           </div>
 
-          <div className={classNames('mt-4', 'text-center', 'text-2xl font-light leading-tight', 'text-gray-700')}>
+          <div
+            className={classNames(
+              'pt-2 text-center text-2xl font-normal leading-tight text-gray-700',
+              popup && 'bg-gray-100'
+            )}
+          >
             {title}
           </div>
         </div>
 
         <div
           className={classNames(
-            appEnv.fullPage
-              ? classNames('w-full mx-auto max-w-md', 'rounded-md')
-              : classNames('-mx-4', 'border-t border-gray-200'),
-            'px-4',
-            'bg-white',
-            'shadow-md'
+            popup ? '-mx-4 border-t border-gray-300' : 'w-full mx-auto max-w-md rounded-md',
+            'px-4 bg-white shadow-md'
           )}
         >
           {children}
         </div>
 
-        <div className={classNames('flex-1', !appEnv.fullPage && '-mx-4 px-4 bg-white')} />
+        <div className={classNames('flex-1', popup && '-mx-4 px-4 bg-white')} />
       </ContentContainer>
     </>
   );
