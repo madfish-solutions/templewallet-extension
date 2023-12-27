c646a1e7bfbb917707ca3a92f4e6048b632c3b2a
Alex <alex.seleznov@gmail.com>
2023-12-27 18:16:18 +0200

    TW-993: Assets rework. NFTs pagination. Overcome OBJKT API byte-size payload limit

--- src/lib/apis/objkt/constants.ts
+++ src/lib/apis/objkt/constants.ts
@@ -4,8 +4,12 @@ const OBJKT_API = 'https://data.objkt.com/v3/graphql/';
 
 export const apolloObjktClient = buildApolloClient(OBJKT_API);
 
-/** See: https://public-api-v3-20221206.objkt.com/docs/#limits */
-export const MAX_OBJKT_QUERY_RESPONSE_ITEMS = 500;
+/** See: https://data.objkt.com/docs/#limits
+ *
+ * Although, API sets limit of 500 items, there is also an implicit byte-size payload limit.
+ * Which can be reached easily, if passing large token IDs in items, resulting in error 413.
+ */
+export const MAX_OBJKT_QUERY_RESPONSE_ITEMS = 250;
 
 export const OBJKT_CONTRACT = 'KT1WvzYHCNBvDSdwafTHv7nJ1dWmZ8GCYuuC';
 
