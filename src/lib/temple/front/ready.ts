5495dacef3ed6e5516077be3ec87661f4ea6d36f
Alex <alex.seleznov@gmail.com>
2024-02-13 13:52:48 +0200

    TW-1309: [hotfix] Update @taquito

--- src/lib/temple/front/ready.ts
+++ src/lib/temple/front/ready.ts
@@ -130,6 +130,7 @@ function useReadyTemple() {
     t.setSignerProvider(createTaquitoSigner(pkh));
     t.setWalletProvider(createTaquitoWallet(pkh, rpc));
     t.setPackerProvider(michelEncoder);
+
     return t;
   }, [createTaquitoSigner, createTaquitoWallet, network, account]);
 
