77cf56639eadeec36c9d4a7b0c25faf8ed9bb57e
Alex <alex.seleznov@gmail.com>
2024-02-15 14:18:15 +0200

    TW-1291: Accelerate operations confirmations. Fix total gas balance on unknown chains

--- src/app/pages/Home/OtherComponents/MainBanner/use-total-balance.ts
+++ src/app/pages/Home/OtherComponents/MainBanner/use-total-balance.ts
@@ -1,30 +1,25 @@
 import { useMemo } from 'react';
 
 import { isDefined } from '@rnw-community/shared';
-import { BigNumber } from 'bignumber.js';
 
 import { useSelector } from 'app/store/root-state.selector';
 import { TEZ_TOKEN_SLUG } from 'lib/assets';
 import { useEnabledAccountTokensSlugs } from 'lib/assets/hooks';
 import { useGetCurrentAccountTokenOrGasBalanceWithDecimals } from 'lib/balances/hooks';
-import { useFiatToUsdRate } from 'lib/fiat-currency';
-import { useGasTokenMetadata } from 'lib/metadata';
 import { isTruthy } from 'lib/utils';
+import { ZERO } from 'lib/utils/numbers';
 
-/** Total fiat volume of displayed tokens */
+/** Total balance in dollar value of displayed tokens, taken from store */
 export const useTotalBalance = () => {
   const tokensSlugs = useEnabledAccountTokensSlugs();
-  const gasMetadata = useGasTokenMetadata();
 
   const getBalance = useGetCurrentAccountTokenOrGasBalanceWithDecimals();
   const allUsdToTokenRates = useSelector(state => state.currency.usdToTokenRates.data);
 
-  const fiatToUsdRate = useFiatToUsdRate();
-
   const slugs = useMemo(() => [TEZ_TOKEN_SLUG, ...tokensSlugs], [tokensSlugs]);
 
-  const totalBalanceInDollar = useMemo(() => {
-    let dollarValue = new BigNumber(0);
+  return useMemo(() => {
+    let dollarValue = ZERO;
 
     for (const slug of slugs) {
       const balance = getBalance(slug);
@@ -33,22 +28,6 @@ export const useTotalBalance = () => {
       dollarValue = dollarValue.plus(tokenDollarValue);
     }
 
-    return dollarValue;
+    return dollarValue.toString();
   }, [slugs, getBalance, allUsdToTokenRates]);
-
-  const totalBalanceInFiat = useMemo(() => {
-    if (!isTruthy(fiatToUsdRate)) return new BigNumber(0);
-
-    return totalBalanceInDollar.times(fiatToUsdRate);
-  }, [totalBalanceInDollar, fiatToUsdRate]);
-
-  const totalBalanceInGasToken = useMemo(() => {
-    const tezosToUsdRate = allUsdToTokenRates[TEZ_TOKEN_SLUG];
-
-    if (!isTruthy(tezosToUsdRate)) return new BigNumber(0);
-
-    return totalBalanceInDollar.dividedBy(tezosToUsdRate).decimalPlaces(gasMetadata.decimals) || new BigNumber(0);
-  }, [totalBalanceInDollar, allUsdToTokenRates, gasMetadata.decimals]);
-
-  return { totalBalanceInFiat, totalBalanceInGasToken };
 };
