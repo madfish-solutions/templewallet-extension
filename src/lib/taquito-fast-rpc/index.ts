6660e77bcbc3afa991d660f0fada42b22ae05c51
Alex <alex.seleznov@gmail.com>
2024-02-20 02:40:56 +0200

    TW-1280: Operations counter value error. + Cached FastRpcClient.getChainId()

--- src/lib/taquito-fast-rpc/index.ts
+++ src/lib/taquito-fast-rpc/index.ts
@@ -1,11 +1,8 @@
 import { EntrypointsResponse, RpcClient, RPCOptions } from '@taquito/rpc';
-import debounce from 'debounce';
 import memoizee from 'memoizee';
 
-interface CachedEntrypointsItem {
-  key: string;
-  entrypoints: EntrypointsResponse;
-}
+import { makeCachedChainIdKey, getCachedChainId, setCachedChainId } from './chain-ids-cache';
+import { getCachedEntrypoints, setCachedEntrypoints } from './entrypoints-cache';
 
 interface LatestBlock {
   hash: string;
@@ -19,9 +16,6 @@ const BLOCK_REFRESH_MIN_INTERVAL = 1_000;
 /** 1.5 min */
 const MEMOIZE_MAX_AGE = 90_000;
 
-const ENTRYPOINTS_CACHE_KEY = 'FastRpcClient.cachedEntrypoints';
-const ENTRYPOINTS_CACHE_SIZE = 30;
-
 /**
  * Alternative to Taquito's `RpcClientCache`.
  *
@@ -32,10 +26,22 @@ const ENTRYPOINTS_CACHE_SIZE = 30;
  * block life time +- provided TTL. This strategy further reduces the number of
  * requests made within block life time.
  *
- * Additionally, persisting contracts' entrypoints data.
+ * Additionally, persisting chain IDs and contracts' entrypoints data.
  */
 export class FastRpcClient extends RpcClient {
   private latestBlock?: LatestBlock;
+
+  async getChainId() {
+    const cacheKey = makeCachedChainIdKey(this.url, this.chain);
+
+    const cached = getCachedChainId(cacheKey);
+    if (cached) return cached;
+
+    const result = await super.getChainId();
+    setCachedChainId(cacheKey, result);
+
+    return result;
+  }
 
   async getBlockHash(opts?: RPCOptions) {
     if (wantsHead(opts)) {
@@ -113,83 +119,20 @@ export class FastRpcClient extends RpcClient {
     promise: true
   });
 
-  // <Entrypoints>
-
   async getEntrypoints(contract: string, opts?: RPCOptions): Promise<EntrypointsResponse> {
     const chainID = await this.getChainId();
     const cacheKey = `${chainID}:${contract}`;
 
-    const cached = this.getCachedEntrypoints(cacheKey);
+    const cached = getCachedEntrypoints(cacheKey);
     if (cached) return cached;
 
     opts = await this.withLatestBlock(opts);
-    const result = await this.getEntrypointsMemo(contract, opts);
+    const result = await super.getEntrypoints(contract, opts);
 
-    this.setCachedEntrypoints(cacheKey, result);
+    setCachedEntrypoints(cacheKey, result);
 
     return result;
   }
-
-  getEntrypointsMemo = memoizee(super.getEntrypoints.bind(this), {
-    normalizer: ([contract, opts]) => `${toOptsKey(opts)}${contract}`,
-    maxAge: MEMOIZE_MAX_AGE,
-    promise: true
-  });
-
-  private cachedEntrypoints = this.readCachedEntrypoints();
-
-  private readCachedEntrypoints(): CachedEntrypointsItem[] {
-    if (typeof localStorage === 'undefined') return [];
-
-    try {
-      const cache = localStorage.getItem(ENTRYPOINTS_CACHE_KEY);
-      return cache ? JSON.parse(cache) : [];
-    } catch (error) {
-      console.error(error);
-    }
-    return [];
-  }
-
-  private commitCachedEntrypoints = debounce(() => {
-    if (typeof localStorage === 'undefined') return;
-    this.cachedEntrypoints = this.cachedEntrypoints.slice(0, ENTRYPOINTS_CACHE_SIZE * 3);
-
-    try {
-      localStorage.setItem(
-        ENTRYPOINTS_CACHE_KEY,
-        JSON.stringify(this.cachedEntrypoints.slice(0, ENTRYPOINTS_CACHE_SIZE))
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  }, 2_000);
-
-  private getCachedEntrypoints(key: string): EntrypointsResponse | undefined {
-    const index = this.cachedEntrypoints.findIndex(item => item.key === key);
-    const item = this.cachedEntrypoints[index];
-
-    // Moving used caches to the head of the list
-    if (index > 0) {
-      this.cachedEntrypoints.splice(index, 1);
-      this.cachedEntrypoints.unshift(item!);
-
-      this.commitCachedEntrypoints();
-    }
-
-    return item?.entrypoints;
-  }
-
-  private setCachedEntrypoints(key: string, entrypoints: EntrypointsResponse) {
-    const index = this.cachedEntrypoints.findIndex(item => item.key === key);
-
-    // Moving used caches to the head of the list
-    if (index >= 0) this.cachedEntrypoints.splice(index, 1);
-    this.cachedEntrypoints.unshift({ key, entrypoints });
-
-    this.commitCachedEntrypoints();
-  }
-
-  // </Entrypoints>
 
   async getManagerKey(address: string, opts?: RPCOptions) {
     opts = await this.withLatestBlock(opts);
@@ -278,8 +221,6 @@ export class FastRpcClient extends RpcClient {
     maxAge: MEMOIZE_MAX_AGE,
     promise: true
   });
-
-  getChainId = memoizee(super.getChainId.bind(this), { promise: true });
 
   private async withLatestBlock(opts?: RPCOptions): Promise<RPCOptions | undefined> {
     if (!wantsHead(opts)) return opts;
