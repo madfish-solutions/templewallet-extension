334a6a5f324ef0bc6311f4b781a07dc95e5ff87e
lendihop <mishin.danil2017@gmail.com>
2024-02-26 17:52:33 +0100

    empty line

--- src/app/pages/Home/OtherComponents/ScamTokenAlert.tsx
+++ src/app/pages/Home/OtherComponents/ScamTokenAlert.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 
 import { T } from 'lib/i18n';
 import { AlertTriangleIcon } from 'lib/icons';
+
 export const ScamTokenAlert = memo(() => (
   <div
     className="w-full max-w-sm mx-auto py-3 px-4 rounded-md border border-red-700 mb-4 flex flex-row items-center"
