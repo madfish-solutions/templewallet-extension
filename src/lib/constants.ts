371808badc0a0d69bdb2b4c9980a3e18e2cb0df7
Inokentii Mazhara <keshan3262@gmail.com>
2024-03-21 14:04:20 +0200

    TW-1400 Resolve merge conflicts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -20,8 +20,6 @@ export const AD_HIDING_TIMEOUT = 12 * 3600 * 1000;
 
 export const AD_SEEN_THRESHOLD = 0.5;
 
-export const TKEY_AD_PLACEMENT_SLUG = 'tkey_ad_placement';
-
 export const HYPELAB_STUB_CAMPAIGN_SLUG = 'e55d2795d2';
 
 const isMacOS = /Mac OS/.test(navigator.userAgent);
