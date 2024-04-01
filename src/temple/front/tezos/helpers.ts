59d9445af1990b362a7bb1ad706c88068a78769b
Alex <alex.seleznov@gmail.com>
2024-04-01 03:29:28 +0300

    TW-1345: Seed derivation for EVM. Fix unit tests

--- src/temple/front/tezos/helpers.ts
+++ src/temple/front/tezos/helpers.ts
@@ -2,9 +2,9 @@ import { getMessage } from 'lib/i18n';
 import { isValidTezosAddress, isTezosContractAddress } from 'lib/tezos';
 
 export function validateTezosContractAddress(value: string) {
-  if (!isTezosContractAddress(value)) return getMessage('onlyKTContractAddressAllowed');
-
   if (!isValidTezosAddress(value)) return getMessage('invalidAddress');
+
+  if (!isTezosContractAddress(value)) return getMessage('onlyKTContractAddressAllowed');
 
   return true;
 }
