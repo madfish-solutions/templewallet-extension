494c8bd3b93eb77f4dbc9eb807b291e7b2aabac1
Inokentii Mazhara <keshan3262@gmail.com>
2024-04-12 14:39:22 +0300

    TW-1364 Remove extra firebase modules

--- audit-deps.js
+++ audit-deps.js
@@ -1,23 +1,11 @@
 const { exec } = require('child_process');
 
-const ignoreList = ['protobufjs'];
-const vunerablePackageLineRegex = /^[^\s]+\s+Package\s+[^\s]+\s+([^\s]+)\s+[^\s]+$/;
-
 exec('yarn audit --level high', (error, stdout, stderr) => {
   if (error) {
     console.log(stdout);
 
     if (stdout.includes('High') || stdout.includes('Critical')) {
-      const lines = stdout.split('\n');
-      const vunerablePackages = new Set(
-        lines
-          .filter((line) => vunerablePackageLineRegex.test(line))
-          .map((line) => vunerablePackageLineRegex.exec(line)[1])
-          .filter((packageName) => !ignoreList.includes(packageName))
-      );
-      if (vunerablePackages.size > 0) {
-        throw new Error('Audit failed');
-      }
+      throw new Error('Audit failed');
     }
   }
 });
