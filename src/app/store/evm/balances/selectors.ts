e34d1b3a1dc2afbf262116161020e8b07af5ca07
lendihop <mishin.danil2017@gmail.com>
2024-04-25 11:38:24 +0200

    fix ts-prune

--- src/app/store/evm/balances/selectors.ts
+++ src/app/store/evm/balances/selectors.ts
@@ -4,7 +4,7 @@ import { useSelector } from '../../root-state.selector';
 
 export const useAllEvmBalancesSelector = () => useSelector(state => state.evmBalances.balancesAtomic);
 
-export const useAccountEvmBalancesSelector = (publicKeyHash: HexString) =>
+const useAccountEvmBalancesSelector = (publicKeyHash: HexString) =>
   useSelector(state => state.evmBalances.balancesAtomic[publicKeyHash] ?? {});
 
 export const useAccountEvmTokenBalanceSelector = (publicKeyHash: HexString, assetSlug: string, chainId: number) =>
