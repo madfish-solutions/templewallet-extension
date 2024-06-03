c046c9d09ac6b75ec22127e25537ff31e10d53f9
Alex <alex.seleznov@gmail.com>
2024-06-03 20:05:52 +0300

    TW-1457: Tezos Staking. + <StakeAmountField>

--- src/app/pages/Staking/NewStake.tsx
+++ src/app/pages/Staking/NewStake.tsx
@@ -2,34 +2,37 @@ import React, { memo, useCallback, useMemo, useState } from 'react';
 
 import { WalletOperation } from '@taquito/taquito';
 import BigNumber from 'bignumber.js';
-import clsx from 'clsx';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import { Alert } from 'app/atoms';
-import AssetField from 'app/atoms/AssetField';
 import { StakeButton } from 'app/atoms/BakingButtons';
-import { ConvertedInputAssetAmount } from 'app/atoms/ConvertedInputAssetAmount';
 import { useUnstakeRequests } from 'app/hooks/use-baking-hooks';
-import { ReactComponent as ChevronDownIcon } from 'app/icons/chevron-down.svg';
-import { ReactComponent as ChevronUpIcon } from 'app/icons/chevron-up.svg';
 import { BakerBanner, BAKER_BANNER_CLASSNAME } from 'app/templates/BakerBanner';
 import OperationStatus from 'app/templates/OperationStatus';
+import { StakeAmountField, FormData, convertFiatToAssetAmount } from 'app/templates/StakeAmountInput';
 import { useFormAnalytics } from 'lib/analytics';
 import { TEZ_TOKEN_SLUG } from 'lib/assets';
 import { useBalance } from 'lib/balances';
 import { useFiatCurrency, useAssetFiatCurrencyPrice } from 'lib/fiat-currency';
-import { t, toLocalFixed } from 'lib/i18n';
-import { TEZOS_METADATA, useGasTokenMetadata } from 'lib/metadata';
-import { useAccount, useDelegate, useKnownBaker, useNetwork, useTezos } from 'lib/temple/front';
+import { t } from 'lib/i18n';
+import { useGasTokenMetadata } from 'lib/metadata';
+import { useAccount, useDelegate, useKnownBaker, useTezos } from 'lib/temple/front';
 import { TempleAccountType } from 'lib/temple/types';
-import { useDidUpdate, useSafeState } from 'lib/ui/hooks';
+import { useSafeState } from 'lib/ui/hooks';
 
 export const NewStakeTab = memo(() => {
   const acc = useAccount();
   const cannotDelegate = acc.type === TempleAccountType.WatchOnly;
 
   const tezos = useTezos();
-  const network = useNetwork();
+
+  const { value: balance } = useBalance(TEZ_TOKEN_SLUG, acc.publicKeyHash);
+
+  const maxAmountInTezos = useMemo(() => {
+    if (!balance) return null;
+
+    return BigNumber.max(balance.minus(MINIMAL_FEE), 0);
+  }, [balance]);
 
   const { data: myBakerPkh } = useDelegate(acc.publicKeyHash, true, false);
   const { data: knownBaker } = useKnownBaker(myBakerPkh || null, false);
@@ -48,66 +51,21 @@ export const NewStakeTab = memo(() => {
     return finalizable.length ? finalizable.some(r => r.delegate !== myBakerPkh) : false;
   }, [requestsSwr.data, myBakerPkh]);
 
-  const { handleSubmit, errors, control, setValue, reset, triggerValidation, getValues, watch } = useForm<FormData>({
+  const form = useForm<FormData>({
     mode: 'onChange'
   });
+  const { handleSubmit, errors, reset } = form;
 
   const { trackSubmitSuccess, trackSubmitFail } = useFormAnalytics('STAKE_FOR_BAKER_FORM');
 
   const [inFiat, setInFiat] = useState(false);
 
   const {
-    selectedFiatCurrency: { symbol: fiatSymbol, name: fiatName }
+    selectedFiatCurrency: { name: fiatName }
   } = useFiatCurrency();
   const assetPrice = useAssetFiatCurrencyPrice(TEZ_TOKEN_SLUG);
-  const canEnterInFiat = network.type === 'main' && assetPrice.gt(0);
-
-  useDidUpdate(() => {
-    if (!canEnterInFiat) setInFiat(false);
-  }, [canEnterInFiat]);
 
   const { symbol, decimals } = useGasTokenMetadata();
-
-  const toAssetAmount = useCallback(
-    (fiatAmount: BigNumber.Value) =>
-      new BigNumber(fiatAmount).dividedBy(assetPrice).toFormat(decimals, BigNumber.ROUND_FLOOR, {
-        decimalSeparator: '.'
-      }),
-    [assetPrice, decimals]
-  );
-
-  const { value: balance } = useBalance(TEZ_TOKEN_SLUG, acc.publicKeyHash);
-
-  const maxAmount = useMemo(() => {
-    if (!balance) return null;
-
-    const maxAmountInTezos = BigNumber.max(balance.minus(MINIMAL_FEE), 0);
-    if (!inFiat) return maxAmountInTezos;
-
-    return maxAmountInTezos.times(assetPrice).decimalPlaces(2, BigNumber.ROUND_FLOOR);
-  }, [balance, inFiat, assetPrice]);
-
-  const amountRules = useMemo(
-    () => ({
-      validate: (val?: string) => {
-        if (val == null) return t('required');
-        if (Number(val) === 0) {
-          return t('amountMustBePositive');
-        }
-        if (!maxAmount) return true;
-        const vBN = new BigNumber(val);
-        return vBN.isLessThanOrEqualTo(maxAmount) || t('maximalAmount', toLocalFixed(maxAmount));
-      }
-    }),
-    [maxAmount]
-  );
-
-  const handleSetMaxAmount = useCallback(() => {
-    if (maxAmount) {
-      setValue('amount', maxAmount.toString());
-      triggerValidation('amount');
-    }
-  }, [setValue, maxAmount, triggerValidation]);
 
   const onSubmit = useCallback(
     ({ amount }: FormData) => {
@@ -117,7 +75,7 @@ export const NewStakeTab = memo(() => {
         provider: knownBakerName
       };
 
-      const tezosAmount = inFiat ? toAssetAmount(amount) : amount;
+      const tezosAmount = inFiat ? convertFiatToAssetAmount(amount, assetPrice, decimals) : amount;
 
       tezos.wallet
         .stake({ amount: Number(tezosAmount) })
@@ -141,84 +99,14 @@ export const NewStakeTab = memo(() => {
       reset,
       trackSubmitSuccess,
       trackSubmitFail,
-      toAssetAmount,
       inFiat,
-      symbol,
+      assetPrice,
       fiatName,
-      knownBakerName
+      knownBakerName,
+      symbol,
+      decimals
     ]
   );
-
-  const amountValue = watch('amount');
-
-  const labelDescription = useMemo(() => {
-    if (!maxAmount) return null;
-
-    const maxAmountFormatted = toLocalFixed(maxAmount);
-
-    return (
-      <div className="flex flex-col gap-y-1">
-        <div>
-          <span>Available (max): </span>
-
-          <button type="button" className="underline" onClick={handleSetMaxAmount}>
-            {inFiat ? `${fiatSymbol}${maxAmountFormatted}` : `${maxAmountFormatted} ${symbol}`}
-          </button>
-        </div>
-
-        {amountValue ? (
-          <ConvertedInputAssetAmount
-            assetSlug={TEZ_TOKEN_SLUG}
-            assetMetadata={TEZOS_METADATA}
-            amountValue={inFiat ? toAssetAmount(amountValue) : amountValue}
-            toFiat={!inFiat}
-          />
-        ) : null}
-      </div>
-    );
-  }, [inFiat, maxAmount, handleSetMaxAmount, toAssetAmount, symbol, fiatSymbol, amountValue]);
-
-  const assetFieldSymbolNode = useMemo(() => {
-    if (!canEnterInFiat) return symbol;
-
-    const handleFiatToggle: React.MouseEventHandler<HTMLButtonElement> = event => {
-      event.preventDefault();
-
-      const newInFiat = !inFiat;
-      setInFiat(newInFiat);
-      const amountStr = getValues().amount;
-      if (!amountStr) return;
-
-      const amount = new BigNumber(amountStr);
-      setValue(
-        'amount',
-        (newInFiat ? amount.multipliedBy(assetPrice) : amount.div(assetPrice)).toFormat(
-          newInFiat ? 2 : decimals,
-          BigNumber.ROUND_FLOOR,
-          { decimalSeparator: '.' }
-        )
-      );
-    };
-
-    return (
-      <button
-        onClick={handleFiatToggle}
-        className={clsx(
-          'px-1 rounded-md flex items-center gap-x-1 font-light',
-          'hover:bg-black hover:bg-opacity-5',
-          'trasition ease-in-out duration-200',
-          'pointer-events-auto'
-        )}
-      >
-        {inFiat ? fiatSymbol : symbol}
-
-        <div className="h-4 flex flex-col justify-between">
-          <ChevronUpIcon className="h-2 w-auto stroke-current stroke-2" />
-          <ChevronDownIcon className="h-2 w-auto stroke-current stroke-2" />
-        </div>
-      </button>
-    );
-  }, [canEnterInFiat, inFiat, symbol, fiatSymbol, decimals, assetPrice, getValues, setValue]);
 
   const errorsInForm = Boolean(errors.amount);
   console.log('D:', myBakerPkh, requestsSwr.data?.unfinalizable.delegate);
@@ -243,18 +131,13 @@ export const NewStakeTab = memo(() => {
       </div>
 
       <form className="flex flex-col gap-y-4" onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          as={AssetField}
-          name="amount"
-          control={control}
-          rules={amountRules}
-          assetSymbol={assetFieldSymbolNode}
-          assetDecimals={inFiat ? 2 : decimals}
-          label={`Stake ${symbol}`}
-          labelDescription={labelDescription}
-          placeholder={t('amountPlaceholder')}
-          errorCaption={errors.amount?.message}
-          autoFocus
+        <StakeAmountField
+          inFiat={inFiat}
+          maxAmountInTezos={maxAmountInTezos}
+          assetPrice={assetPrice}
+          accountPkh={acc.publicKeyHash}
+          setInFiat={setInFiat}
+          {...form}
         />
 
         <StakeButton type="submit" disabled={disableSubmit} />
@@ -262,10 +145,6 @@ export const NewStakeTab = memo(() => {
     </div>
   );
 });
-
-interface FormData {
-  amount: string;
-}
 
 /** Just to be able to apply 'Max Amount' & not fail estimation on confirm */
 const MINIMAL_FEE = 1e-4;
--- src/app/pages/Staking/RequestUnstakeModal.tsx
+++ src/app/pages/Staking/RequestUnstakeModal.tsx
@@ -1,17 +1,18 @@
-import React, { memo, useCallback, useMemo } from 'react';
+import React, { memo, useCallback, useMemo, useState } from 'react';
 
-import BigNumber from 'bignumber.js';
 import clsx from 'clsx';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import { FormSecondaryButton, FormSubmitButton } from 'app/atoms';
-import AssetField from 'app/atoms/AssetField';
 import CustomModal from 'app/atoms/CustomModal';
 import { useAppEnv } from 'app/env';
 import { useStakedAmount } from 'app/hooks/use-baking-hooks';
+import { StakeAmountField, FormData, convertFiatToAssetAmount } from 'app/templates/StakeAmountInput';
 import { useFormAnalytics } from 'lib/analytics';
-import { t, T, toLocalFixed } from 'lib/i18n';
-import { TEZOS_METADATA } from 'lib/metadata';
+import { TEZ_TOKEN_SLUG } from 'lib/assets';
+import { useAssetFiatCurrencyPrice, useFiatCurrency } from 'lib/fiat-currency';
+import { T } from 'lib/i18n';
+import { TEZOS_METADATA, useGasTokenMetadata } from 'lib/metadata';
 import { useAccountPkh, useTezos } from 'lib/temple/front';
 import { atomsToTokens } from 'lib/temple/helpers';
 
@@ -30,48 +31,40 @@ export const RequestUnstakeModal = memo<Props>(({ knownBakerName, close }) => {
 
   const { data: stakedAmount } = useStakedAmount(tezos.rpc.getRpcUrl(), accountPkh);
 
-  const maxAmount = stakedAmount ? atomsToTokens(stakedAmount, TEZOS_METADATA.decimals) : null;
+  const [inFiat, setInFiat] = useState(false);
 
-  const { handleSubmit, errors, control, setValue, triggerValidation } = useForm<FormData>({
+  const { symbol, decimals } = useGasTokenMetadata();
+  const {
+    selectedFiatCurrency: { name: fiatName }
+  } = useFiatCurrency();
+
+  const maxAmountInTezos = useMemo(
+    () => (stakedAmount ? atomsToTokens(stakedAmount, TEZOS_METADATA.decimals) : null),
+    [stakedAmount]
+  );
+
+  const assetPrice = useAssetFiatCurrencyPrice(TEZ_TOKEN_SLUG);
+
+  const form = useForm<FormData>({
     mode: 'onChange'
   });
 
+  const { handleSubmit, errors } = form;
+
   const { trackSubmitSuccess, trackSubmitFail } = useFormAnalytics('UNSTAKE_REQUEST_FORM');
-
-  const amountRules = useMemo(
-    () => ({
-      validate: (val?: string) => {
-        if (val == null) return t('required');
-        if (Number(val) === 0) {
-          return t('amountMustBePositive');
-        }
-        if (!maxAmount) return true;
-        const vBN = new BigNumber(val);
-        return vBN.isLessThanOrEqualTo(maxAmount) || t('maximalAmount', toLocalFixed(maxAmount));
-      }
-    }),
-    [maxAmount]
-  );
-
-  const handleSetMaxAmount = useCallback(() => {
-    if (maxAmount) {
-      setValue('amount', maxAmount.toString());
-      triggerValidation('amount');
-    }
-  }, [setValue, maxAmount, triggerValidation]);
 
   const onSubmit = useCallback(
     ({ amount }: FormData) => {
-      const amountNumber = Number(amount);
-
       const analyticsProps = {
-        inputAsset: 'TEZ',
-        inputAmount: amountNumber,
+        inputAsset: inFiat ? fiatName : symbol,
+        inputAmount: amount,
         provider: knownBakerName
       };
 
+      const tezosAmount = inFiat ? convertFiatToAssetAmount(amount, assetPrice, decimals) : amount;
+
       tezos.wallet
-        .unstake({ amount: amountNumber })
+        .unstake({ amount: Number(tezosAmount) })
         .send()
         .then(
           () => {
@@ -85,22 +78,8 @@ export const RequestUnstakeModal = memo<Props>(({ knownBakerName, close }) => {
           }
         );
     },
-    [tezos, close, trackSubmitSuccess, trackSubmitFail, knownBakerName]
+    [inFiat, tezos, close, trackSubmitSuccess, trackSubmitFail, knownBakerName, assetPrice, decimals, symbol, fiatName]
   );
-
-  const labelDescription = useMemo(() => {
-    if (!maxAmount) return null;
-
-    return (
-      <>
-        <span>Available amount: </span>
-
-        <button type="button" className="underline" onClick={handleSetMaxAmount}>
-          {`${toLocalFixed(maxAmount)} ${TEZOS_METADATA.symbol}`}
-        </button>
-      </>
-    );
-  }, [maxAmount, handleSetMaxAmount]);
 
   const errorsInForm = Boolean(errors.amount);
   const disableSubmit = errorsInForm;
@@ -108,18 +87,14 @@ export const RequestUnstakeModal = memo<Props>(({ knownBakerName, close }) => {
   return (
     <CustomModal isOpen={true} onRequestClose={close} className={clsx('w-full max-w-md', fullPage ? 'p-6' : 'p-4')}>
       <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          as={AssetField}
-          name="amount"
-          control={control}
-          rules={amountRules}
-          assetSymbol={TEZOS_METADATA.symbol}
-          assetDecimals={TEZOS_METADATA.decimals}
-          label={`Unstake ${TEZOS_METADATA.symbol}`}
-          labelDescription={labelDescription}
-          placeholder={t('amountPlaceholder')}
-          errorCaption={errors.amount?.message}
-          autoFocus
+        <StakeAmountField
+          forUnstake
+          inFiat={inFiat}
+          maxAmountInTezos={maxAmountInTezos}
+          assetPrice={assetPrice}
+          accountPkh={accountPkh}
+          setInFiat={setInFiat}
+          {...form}
         />
 
         <div className="text-xs leading-5 text-gray-500">
@@ -146,7 +121,3 @@ export const RequestUnstakeModal = memo<Props>(({ knownBakerName, close }) => {
     </CustomModal>
   );
 });
-
-interface FormData {
-  amount: string;
-}
