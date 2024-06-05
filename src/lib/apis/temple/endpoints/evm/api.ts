8ab2bb6f5f32fa603d8a7cf994080cacc402eeac
lendihop <mishin.danil2017@gmail.com>
2024-06-05 17:13:12 +0200

    update evm endpoints

--- src/lib/apis/temple/endpoints/evm/api.ts
+++ src/lib/apis/temple/endpoints/evm/api.ts
@@ -3,18 +3,18 @@ import { BalancesResponse, ChainID, NftAddressBalanceNftResponse } from 'lib/api
 import { templeWalletApi } from '../templewallet.api';
 
 export const getEvmBalances = (walletAddress: string, chainId: ChainID) =>
-  buildEvmRequest<BalancesResponse>('/evm-balances', walletAddress, chainId);
+  buildEvmRequest<BalancesResponse>('/balances', walletAddress, chainId);
 
 // Response also contains exchange rates
 export const getEvmTokensMetadata = (walletAddress: string, chainId: ChainID) =>
-  buildEvmRequest<BalancesResponse>('/evm-tokens-metadata', walletAddress, chainId);
+  buildEvmRequest<BalancesResponse>('/tokens-metadata', walletAddress, chainId);
 
 export const getEvmCollectiblesMetadata = (walletAddress: string, chainId: ChainID) =>
-  buildEvmRequest<NftAddressBalanceNftResponse>('/evm-collectibles-metadata', walletAddress, chainId);
+  buildEvmRequest<NftAddressBalanceNftResponse>('/collectibles-metadata', walletAddress, chainId);
 
 const buildEvmRequest = <T>(url: string, walletAddress: string, chainId: ChainID) =>
   templeWalletApi
-    .get<T>(url, {
+    .get<T>(`evm/${url}`, {
       params: { walletAddress, chainId }
     })
     .then(
