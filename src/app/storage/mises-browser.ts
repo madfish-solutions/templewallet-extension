469974092b235092764c5adc4fc3f17247d0bca1
Alex <alex.seleznov@gmail.com>
2024-07-29 11:31:51 +0300

    TW-1504: [Mises] Temple ads native prompt. + === true check

--- src/app/storage/mises-browser.ts
+++ src/app/storage/mises-browser.ts
@@ -3,4 +3,4 @@ import { fetchFromStorage } from 'lib/storage';
 export const MISES_INSTALL_ENABLED_ADS_STORAGE_KEY = 'MISES_ACCEPT_TOS';
 
 export const getMisesInstallEnabledAds = () =>
-  fetchFromStorage<'true'>(MISES_INSTALL_ENABLED_ADS_STORAGE_KEY).then(r => r === 'true');
+  fetchFromStorage<'true'>(MISES_INSTALL_ENABLED_ADS_STORAGE_KEY).then(r => r === 'true' || r === true);
