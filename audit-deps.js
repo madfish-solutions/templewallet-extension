ecbcd5e2ee53a3d8fb90b800dbfb3eadcb8984c4
Alex <alex.seleznov@gmail.com>
2024-08-13 13:00:29 +0300

    TW-1343: [epic][EVM] Arch prep. Temporarily allow axios dep vulnerability

--- audit-deps.js
+++ audit-deps.js
@@ -1,11 +1,30 @@
 const { exec } = require('child_process');
 
+/** TODO: Discard ASAP */
+const exception = 'https://www.npmjs.com/advisories/1098582';
+
 exec('yarn audit --level high', (error, stdout, stderr) => {
   if (error) {
     console.log(stdout);
 
     if (stdout.includes('High') || stdout.includes('Critical')) {
-      throw new Error('Audit failed');
+      const countHigh = stdout.match(/Severity: .* (\d+) High/)?.[1];
+      const countCritical = stdout.match(/Severity: .* (\d+) Critical/)?.[1];
+      const count = (countHigh ? Number(countHigh) : 0) + (countCritical ? Number(countCritical) : 0);
+
+      if (
+        exception
+        && countSubstrings(stdout, exception) === count
+        && countSubstrings(stdout, 'No patch available') === count
+      ) return;
+
+      throw new Error(`Audit failed with ${count} vulnerabilities`);
     }
   }
 });
+
+function countSubstrings(str, substr) {
+  const match = str.match( new RegExp(substr, 'g') );
+
+  return match?.length ?? 0;
+}
