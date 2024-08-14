190322439111dab5175ce4821f5140d87577e0dd
Alex <alex.seleznov@gmail.com>
2024-08-14 11:39:12 +0300

    TW-1343: [epic][EVM] Arch prep. Fix audit-deps.js. Upgraded axios

--- audit-deps.js
+++ audit-deps.js
@@ -1,8 +1,5 @@
 const { exec } = require('child_process');
 
-/** TODO: Discard ASAP */
-const exception = 'https://www.npmjs.com/advisories/1098582';
-
 exec('yarn audit --level high', (error, stdout, stderr) => {
   if (error) {
     console.log(stdout);
@@ -12,19 +9,7 @@ exec('yarn audit --level high', (error, stdout, stderr) => {
       const countCritical = stdout.match(/Severity: .* (\d+) Critical/)?.[1];
       const count = (countHigh ? Number(countHigh) : 0) + (countCritical ? Number(countCritical) : 0);
 
-      if (
-        exception
-        && countSubstrings(stdout, exception) === count
-        && countSubstrings(stdout, 'No patch available') === count
-      ) return;
-
       throw new Error(`Audit failed with ${count} vulnerabilities`);
     }
   }
 });
-
-function countSubstrings(str, substr) {
-  const match = str.match( new RegExp(substr, 'g') );
-
-  return match?.length ?? 0;
-}
