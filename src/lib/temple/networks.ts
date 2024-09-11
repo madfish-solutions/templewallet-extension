65730e4076e61a7d0d4e085f879b54096ea30fde
Inokentii Mazhara <keshan3262@gmail.com>
2024-09-11 13:35:18 +0300

    TW-1539 Remove unused functions

--- src/lib/temple/networks.ts
+++ src/lib/temple/networks.ts
@@ -1,15 +1,5 @@
 import { TempleChainId, TempleNetwork } from 'lib/temple/types';
 
-const formatDateToRPCFormat = (date: Date) => date.toLocaleDateString('en-GB').split('/').reverse().join('-');
-
-const getLastMonday = (date = new Date()) => {
-  const dateCopy = new Date(date.getTime() - 604800000);
-
-  const nextMonday = new Date(dateCopy.setDate(dateCopy.getDate() + ((7 - dateCopy.getDay() + 1) % 7 || 7)));
-
-  return formatDateToRPCFormat(nextMonday);
-};
-
 export const NETWORK_IDS = new Map<string, string>([
   [TempleChainId.Mainnet, 'mainnet'],
   [TempleChainId.Ghostnet, 'ghostnet'],
