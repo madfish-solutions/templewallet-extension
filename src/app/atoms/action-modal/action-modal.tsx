9cdeb5c6e361bcbf77b815c138fdac24f7b394eb
Inokentii Mazhara <keshan3262@gmail.com>
2024-09-17 11:53:29 +0300

    TW-1555 Some more bugfixes

--- src/app/atoms/action-modal/action-modal.tsx
+++ src/app/atoms/action-modal/action-modal.tsx
@@ -1,8 +1,15 @@
 import React, { memo } from 'react';
 
+import clsx from 'clsx';
+
 import { Button, IconBase } from 'app/atoms';
 import CustomModal from 'app/atoms/CustomModal';
+import { useAppEnv } from 'app/env';
 import { ReactComponent as CloseIcon } from 'app/icons/base/x.svg';
+import { LAYOUT_CONTAINER_CLASSNAME } from 'app/layouts/containers';
+// import { APP_CONTENT_PAPER_DOM_ID } from 'app/layouts/containers';
+
+import actionModalStyles from './action-modal.module.css';
 
 interface ActionModalProps {
   hasCloseButton?: boolean;
@@ -12,16 +19,32 @@ interface ActionModalProps {
   className?: string;
 }
 
-export const ActionModal = memo<ActionModalProps>(({ onClose, children, hasCloseButton = true, title }) => (
-  <CustomModal isOpen overlayClassName="backdrop-blur-xs" onRequestClose={onClose}>
-    <div className="relative p-3 border-b-0.5 border-lines w-modal">
-      <h1 className="text-center text-font-regular-bold mx-9">{title}</h1>
-      {hasCloseButton && (
-        <Button className="absolute top-3 right-3" onClick={onClose}>
-          <IconBase Icon={CloseIcon} size={16} className="text-grey-2" />
-        </Button>
+// const parentSelector = () => document.getElementById(APP_CONTENT_PAPER_DOM_ID)!;
+
+export const ActionModal = memo<ActionModalProps>(({ onClose, children, hasCloseButton = true, title }) => {
+  const { fullPage } = useAppEnv();
+
+  return (
+    <CustomModal
+      // parentSelector={parentSelector}
+      isOpen
+      className="rounded-lg"
+      overlayClassName={clsx(
+        'backdrop-blur-xs',
+        fullPage && actionModalStyles.fullPageOverlay,
+        fullPage && `top-9 bottom-8 min-h-80 rounded-md ${LAYOUT_CONTAINER_CLASSNAME}`
       )}
-    </div>
-    {children}
-  </CustomModal>
-));
+      onRequestClose={onClose}
+    >
+      <div className="relative p-3 border-b-0.5 border-lines w-modal">
+        <h1 className="text-center text-font-regular-bold mx-9">{title}</h1>
+        {hasCloseButton && (
+          <Button className="absolute top-3 right-3" onClick={onClose}>
+            <IconBase Icon={CloseIcon} size={16} className="text-grey-2" />
+          </Button>
+        )}
+      </div>
+      {children}
+    </CustomModal>
+  );
+});
