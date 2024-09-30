e82af79825d15b4b17066903df791edd4906cd30
Inokentii Mazhara <keshan3262@gmail.com>
2024-09-30 14:47:29 +0300

    TW-1545 Change logic for auto-lock time storage

--- src/lib/lock-up/index.ts
+++ src/lib/lock-up/index.ts
@@ -1,31 +1,13 @@
 import { AUTOLOCK_TIME_STORAGE_KEY } from 'lib/constants';
-import { DEFAULT_LOCK_UP_ENABLED, DEFAULT_WALLET_AUTOLOCK_TIME } from 'lib/fixed-times';
+import { DEFAULT_WALLET_AUTOLOCK_TIME } from 'lib/fixed-times';
 import { fetchFromStorage } from 'lib/storage';
 import { useStorage } from 'lib/temple/front';
-import { TempleSharedStorageKey } from 'lib/temple/types';
-import { useLocalStorage } from 'lib/ui/local-storage';
 
-const LEGACY_STORAGE_KEY = TempleSharedStorageKey.LockUpEnabled;
-
-export const getLockUpTimeout = async () => {
-  const newVersionTimeout = await fetchFromStorage<number>(AUTOLOCK_TIME_STORAGE_KEY);
-
-  if (newVersionTimeout) {
-    return newVersionTimeout;
-  }
-
-  const legacyStored = localStorage.getItem(LEGACY_STORAGE_KEY);
-
-  return legacyStored === String(DEFAULT_LOCK_UP_ENABLED) || legacyStored === null
-    ? DEFAULT_WALLET_AUTOLOCK_TIME
-    : Infinity;
-};
+export const getLockUpTimeout = async () =>
+  (await fetchFromStorage<number>(AUTOLOCK_TIME_STORAGE_KEY)) ?? DEFAULT_WALLET_AUTOLOCK_TIME;
 
 export const useLockUpTimeout = () => {
   const [newVersionTimeout, setNewVersionTimeout] = useStorage<number>(AUTOLOCK_TIME_STORAGE_KEY);
-  const [legacyIsLockup] = useLocalStorage(LEGACY_STORAGE_KEY, DEFAULT_LOCK_UP_ENABLED);
 
-  const lockUpTimeout = newVersionTimeout ?? (legacyIsLockup ? DEFAULT_WALLET_AUTOLOCK_TIME : Infinity);
-
-  return [lockUpTimeout, setNewVersionTimeout] as const;
+  return [newVersionTimeout ?? DEFAULT_WALLET_AUTOLOCK_TIME, setNewVersionTimeout] as const;
 };
