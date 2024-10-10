aefd6ae685a9a30222a995f112a507c7689a1fa6
Alex <alex.seleznov@gmail.com>
2024-10-10 11:54:55 +0300

    TW-1479: [EVM] Transactions history. Trigger pipeline

--- src/app/templates/SettingsGeneral/index.tsx
+++ src/app/templates/SettingsGeneral/index.tsx
@@ -2,11 +2,11 @@ import React, { memo } from 'react';
 
 import { NotificationsSettings } from 'lib/notifications/components';
 
-import AnalyticsSettings from './components/AnalyticsSettings';
-import FiatCurrencySelect from './components/FiatCurrencySelect';
-import LocaleSelect from './components/LocaleSelect';
-import LockUpSettings from './components/LockUpSettings';
-import PopupSettings from './components/PopupSettings';
+import AnalyticsSettings from './Components/AnalyticsSettings';
+import FiatCurrencySelect from './Components/FiatCurrencySelect';
+import LocaleSelect from './Components/LocaleSelect';
+import LockUpSettings from './Components/LockUpSettings';
+import PopupSettings from './Components/PopupSettings';
 
 const GeneralSettings = memo(() => (
   <div className="w-full max-w-sm mx-auto my-8">
