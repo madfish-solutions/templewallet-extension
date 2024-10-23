3dac5bace57881953d4649bf7c6048c2b78262d2
Inokentii Mazhara <keshan3262@gmail.com>
2024-10-23 17:11:21 +0300

    TW-1571 Ignore dependencies issues caused by @types modules

--- audit-deps.js
+++ audit-deps.js
@@ -4,7 +4,18 @@ exec('yarn audit --level high', (error, stdout, stderr) => {
   if (error) {
     console.log(stdout);
 
-    if (stdout.includes('High') || stdout.includes('Critical')) {
+    const tooSevereVunerabilitiesCount = [/(\d+) High/, /(\d+) Critical/]
+      .map(regex => {
+        const match = regex.exec(stdout);
+
+        return match ? parseInt(match[1]) : 0;
+      })
+      .reduce((a, b) => a + b, 0);
+    const tooSevereTypesVunerabilitiesCount = Array
+      .from(stdout.matchAll(/Dependency of\s+\x1B\[90m│\x1B\[39m\s+@types/g))
+      .length;
+
+    if (tooSevereVunerabilitiesCount - tooSevereTypesVunerabilitiesCount > 0) {
       throw new Error('Audit failed');
     }
   }
