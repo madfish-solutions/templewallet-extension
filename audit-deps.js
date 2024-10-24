bae8a94b5eea48b5256e22aae2b1f0c1597859e8
Inokentii Mazhara <keshan3262@gmail.com>
2024-10-24 16:54:46 +0300

    TW-1571 Revert changes to audit script

--- audit-deps.js
+++ audit-deps.js
@@ -1,45 +1,11 @@
 const { exec } = require('child_process');
 
-/** TODO: Discard ASAP */
-const advisoryException = 'https://www.npmjs.com/advisories/1100223';
-
 exec('yarn audit --level high', (error, stdout, stderr) => {
   if (error) {
     console.log(stdout);
 
-    const unescapedStdout = stdout.replace(/\x1B\[\d+m/g, '');
-    const vunerabilitiesTablesBodies = Array.from(unescapedStdout.matchAll(/┌.+┐\n([^└]+)\n└/g)).map(match => match[1]);
-    const vunerabilitiesTraits = vunerabilitiesTablesBodies.map(body => {
-      console.log(JSON.stringify(body));
-      const tableRows = body
-        .split(/\n*├─+┼─+┤\n*/g)
-        .filter(row => row)
-        .map(row => {
-          const cellsParts = row
-            .split('\n')
-            .map(line => line.split('│').map(cellPart => cellPart.trim()).slice(1, -1));
-          return cellsParts.slice(1).reduce(
-            (acc, lineCellsParts) => {
-              lineCellsParts.forEach((cellPart, index) => {
-                if (cellPart) {
-                  acc[index] = (acc[index] || '') + ' ' + cellPart;
-                }
-              });
-
-              return acc;
-            },
-            cellsParts[0]
-          )
-        });
-
-      return {
-        advisory: tableRows.find(entry => entry[1].includes('https://www.npmjs.com/advisories'))?.[1],
-        patchAvailable: tableRows.every(entry => !entry[1].includes('No patch available'))
-      };
-    });
-
-    if (vunerabilitiesTraits.some(({ advisory, patchAvailable }) => advisory !== advisoryException && patchAvailable)) {
-      throw new Error(`Audit failed with ${vunerabilitiesTablesBodies.length} vulnerabilities`);
+    if (stdout.includes('High') || stdout.includes('Critical')) {
+      throw new Error('Audit failed');
     }
   }
 });
