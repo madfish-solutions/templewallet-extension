498f5c2fc3ca38825544deacda811c858a1f2160
Inokentii Mazhara <keshan3262@gmail.com>
2024-10-25 12:14:30 +0300

    TW-1571 Fix the crash of 'Swap' section after upgrade

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -24,7 +24,7 @@ const persistConfigBlacklist: (keyof RootState)[] = SLICES_BLACKLIST;
 const persistedReducer = persistReducer<RootState>(
   {
     key: 'temple-root',
-    version: 3,
+    version: 4,
     ...storageConfig,
     stateReconciler: autoMergeLevel2,
     blacklist: persistConfigBlacklist,
