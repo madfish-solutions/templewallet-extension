9ca36e9200118df7663fd940341e4da70ac394c4
Alex <alex.seleznov@gmail.com>
2024-11-04 02:05:18 +0200

    TW-1479: [EVM] Transactions history. Bundle modal. + Lazy render

--- src/app/templates/activity/ActivityItem/BundleModal.tsx
+++ src/app/templates/activity/ActivityItem/BundleModal.tsx
@@ -1,24 +1,39 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 
 import { ActionsButtonsBox } from 'app/atoms/PageModal';
 import { ScrollView } from 'app/atoms/ScrollView';
+import { SimpleInfiniteScroll } from 'app/atoms/SimpleInfiniteScroll';
 import { StyledButtonAnchor } from 'app/atoms/StyledButton';
 import { T, formatDate } from 'lib/i18n';
 
-interface Props extends PropsWithChildren {
+interface Props {
   addedAt: string;
   blockExplorerUrl?: string;
+  children: React.ReactElement[];
 }
 
-export const BundleModalContent: FC<Props> = ({ addedAt, blockExplorerUrl, children }) => {
+const CHUNK_SIZE = 20;
+const SCROLLABLE_ELEM_ID = 'ACTIVITY_BUNDLE_MODAL_SCROLL';
+
+export const BundleModalContent: FC<Props> = ({ addedAt, blockExplorerUrl, children: items }) => {
   const title = useMemo(() => formatDate(addedAt, 'PP'), [addedAt]);
+
+  const [slicedItems, setSlicedItems] = useState(() => items.slice(0, CHUNK_SIZE));
+
+  const loadNext = useCallback(() => {
+    if (slicedItems.length === items.length) return;
+
+    setSlicedItems(items.slice(0, slicedItems.length + CHUNK_SIZE));
+  }, [slicedItems.length, items]);
 
   return (
     <>
-      <ScrollView className="p-4 pb-15">
-        <div className="mb-1 p-1 text-font-description-bold">{title}</div>
+      <ScrollView id={SCROLLABLE_ELEM_ID} className="p-4 pb-15">
+        <SimpleInfiniteScroll loadNext={loadNext} scrollableTargetId={SCROLLABLE_ELEM_ID}>
+          <div className="mb-1 p-1 text-font-description-bold">{title}</div>
 
-        {children}
+          {slicedItems}
+        </SimpleInfiniteScroll>
       </ScrollView>
 
       <ActionsButtonsBox>
