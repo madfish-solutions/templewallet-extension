d7ef4fa8ebb5bf2f3eac3ed809a5ec868a2f90f2
Inokentii Mazhara <keshan3262@gmail.com>
2024-11-13 17:30:36 +0200

    TW-1563 Implement theming for native ads that support it

--- src/content-scripts/replace-ads/ads-stack.iframe.ts
+++ src/content-scripts/replace-ads/ads-stack.iframe.ts
@@ -1,11 +1,21 @@
 import { configureAds } from 'lib/ads/configure-ads';
 import { importExtensionAdsModule } from 'lib/ads/import-extension-ads-module';
-import { ADS_META_SEARCH_PARAM_NAME, AD_CATEGORIES_PARAM_NAME, ORIGIN_SEARCH_PARAM_NAME } from 'lib/constants';
+import {
+  ADS_META_SEARCH_PARAM_NAME,
+  AD_CATEGORIES_PARAM_NAME,
+  FONT_SIZE_SEARCH_PARAM_NAME,
+  LINE_HEIGHT_SEARCH_PARAM_NAME,
+  ORIGIN_SEARCH_PARAM_NAME,
+  THEME_COLOR_SEARCH_PARAM_NAME
+} from 'lib/constants';
 
 import { getRulesFromStorage } from './ads-rules';
 
 const usp = new URLSearchParams(window.location.search);
 const id = usp.get('id');
+const rawThemeColor = usp.get(THEME_COLOR_SEARCH_PARAM_NAME);
+const rawFontSize = usp.get(FONT_SIZE_SEARCH_PARAM_NAME);
+const rawLineHeight = usp.get(LINE_HEIGHT_SEARCH_PARAM_NAME);
 const origin = usp.get(ORIGIN_SEARCH_PARAM_NAME) ?? window.location.href;
 const adsMetadataIds = usp.getAll(ADS_META_SEARCH_PARAM_NAME).map(value => JSON.parse(value));
 const adCategories = usp.getAll(AD_CATEGORIES_PARAM_NAME);
@@ -16,13 +26,16 @@ configureAds()
   .then(({ renderAdsStack, rules }) => {
     const { blacklistedHypelabCampaignsSlugs, permanentAdPlacesRules, adPlacesRules } = rules;
 
-    renderAdsStack(
-      id ?? '',
-      adsMetadataIds,
+    renderAdsStack({
+      id: id ?? '',
+      initialAdsMetadata: adsMetadataIds,
       origin,
-      permanentAdPlacesRules.length > 0 || adPlacesRules.length > 0,
+      pageHasPlacesRules: permanentAdPlacesRules.length > 0 || adPlacesRules.length > 0,
       adCategories,
-      blacklistedHypelabCampaignsSlugs
-    );
+      hypelabBlacklistedCampaignsSlugs: blacklistedHypelabCampaignsSlugs,
+      themeColor: rawThemeColor ?? undefined,
+      fontSize: rawFontSize ? Number(rawFontSize) : undefined,
+      lineHeight: rawLineHeight ? Number(rawLineHeight) : undefined
+    });
   })
   .catch(error => console.error(error));
