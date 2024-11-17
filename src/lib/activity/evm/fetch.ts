6e1d657c03861ad276cf6c1162a3de2c91804c75
Alex <alex.seleznov@gmail.com>
2024-11-17 03:29:02 +0200

    TW-1587: [research] Activity History on Alchemy. Transfers

--- src/lib/activity/evm/fetch.ts
+++ src/lib/activity/evm/fetch.ts
@@ -1,10 +1,22 @@
+import {
+  Alchemy,
+  AssetTransfersCategory,
+  Network,
+  AssetTransfersWithMetadataParams,
+  SortingOrder,
+  AssetTransfersWithMetadataResult
+} from 'alchemy-sdk';
+import { groupBy, uniqBy } from 'lodash';
+
 import { getEvmERC20Transfers, getEvmTransactions } from 'lib/apis/temple/endpoints/evm';
 import { fromAssetSlug } from 'lib/assets';
 import { EVM_TOKEN_SLUG } from 'lib/assets/defaults';
+import { TempleChainKind } from 'temple/types';
 
-import { EvmActivity } from '../types';
+import { ActivityStatus, EvmActivity } from '../types';
 
 import { parseGoldRushTransaction, parseGoldRushERC20Transfer } from './parse';
+import { parseTransfer } from './parse/alchemy';
 
 export async function getEvmAssetTransactions(
   walletAddress: string,
@@ -57,3 +69,157 @@ export async function getEvmAssetTransactions(
     nextPage
   };
 }
+
+export async function getEvmActivities(
+  chainId: number,
+  accAddress: string,
+  olderThanBlockHeight?: string,
+  signal?: AbortSignal
+) {
+  const accAddressLowercased = accAddress.toLowerCase();
+
+  const allTransfers = await fetchTransfers(chainId, accAddress, olderThanBlockHeight, signal);
+  if (!allTransfers.length) return [];
+
+  const groups = Object.entries(groupBy(allTransfers, 'hash'));
+
+  const activities: EvmActivity[] = [];
+
+  for (const [hash, transfers] of groups) {
+    const firstTransfer = transfers.at(0)!;
+
+    const operations = transfers.map(transfer => parseTransfer(transfer, accAddressLowercased));
+
+    const activity: EvmActivity = {
+      chain: TempleChainKind.EVM,
+      chainId,
+      hash,
+      status: ActivityStatus.applied,
+      addedAt: firstTransfer.metadata.blockTimestamp,
+      operations,
+      operationsCount: operations.length,
+      blockHeight: `${Number(firstTransfer.blockNum)}`
+    };
+
+    activities.push(activity);
+  }
+
+  return activities;
+}
+
+async function fetchTransfers(
+  chainId: number,
+  accAddress: string,
+  olderThanBlockHeight?: string,
+  signal?: AbortSignal
+): Promise<AssetTransfersWithMetadataResult[]> {
+  const chainName = CHAINS_NAMES[chainId];
+  if (!chainName) return [];
+
+  const alchemy = new Alchemy({
+    apiKey: process.env._ALCHEMY_API_KEY, // TODO: To EnvVars
+    network: chainName
+  });
+
+  const reqOptions: AssetTransfersWithMetadataParams = {
+    order: SortingOrder.DESCENDING,
+    category: Object.values(AssetTransfersCategory),
+    excludeZeroValue: true,
+    withMetadata: true,
+    toBlock: olderThanBlockHeight ? '0x' + (BigInt(olderThanBlockHeight) - BigInt(1)).toString(16) : undefined,
+    maxCount: 50
+  };
+
+  const [transfersFrom, transfersTo] = await Promise.all([
+    alchemy.core.getAssetTransfers({ ...reqOptions, fromAddress: accAddress }).then(r => r.transfers),
+    alchemy.core.getAssetTransfers({ ...reqOptions, toAddress: accAddress }).then(r => r.transfers)
+  ]);
+
+  const allTransfers = transfersFrom
+    .concat(transfersTo)
+    .toSorted((a, b) => (a.metadata.blockTimestamp > b.metadata.blockTimestamp ? -1 : 1));
+
+  if (!allTransfers.length) return [];
+
+  const sameTrailingHashes = calcSameTrailingHashes(allTransfers);
+
+  /** Will need to filter those transfers, that r made from & to the same address */
+  const uniqByKey: keyof (typeof allTransfers)[number] = 'uniqueId';
+
+  if (sameTrailingHashes === allTransfers.length) {
+    // const moreTransfers = []; // TODO: Fetch more transfers until reach another hash ?
+    // return uniqBy(allTransfers.concat(moreTransfers), uniqByKey);
+  }
+
+  allTransfers.splice(allTransfers.length - sameTrailingHashes, sameTrailingHashes);
+
+  return uniqBy(allTransfers, uniqByKey);
+}
+
+function calcSameTrailingHashes(transfers: AssetTransfersWithMetadataResult[]) {
+  const trailingHash = transfers.at(transfers.length - 1)!.hash;
+  if (transfers.at(0)!.hash === trailingHash) return transfers.length; // All are same, saving runtime
+
+  if (transfers.length === 2) return 1; // Preposition for further math
+
+  const sameTrailingHashes = transfers.length - 1 - transfers.findLastIndex(tr => tr.hash !== trailingHash);
+
+  return sameTrailingHashes;
+}
+
+/** TODO: Verify this mapping */
+const CHAINS_NAMES: Record<number, Network> = {
+  1: Network.ETH_MAINNET,
+  5: Network.ETH_GOERLI,
+  10: Network.OPT_MAINNET,
+  30: Network.ROOTSTOCK_MAINNET,
+  31: Network.ROOTSTOCK_TESTNET,
+  56: Network.BNB_MAINNET,
+  97: Network.BNB_TESTNET,
+  100: Network.GNOSIS_MAINNET,
+  137: Network.MATIC_MAINNET,
+  204: Network.OPBNB_MAINNET,
+  250: Network.FANTOM_MAINNET,
+  300: Network.ZKSYNC_SEPOLIA,
+  324: Network.ZKSYNC_MAINNET,
+  360: Network.SHAPE_MAINNET,
+  420: Network.OPT_GOERLI,
+  480: Network.WORLDCHAIN_MAINNET,
+  592: Network.ASTAR_MAINNET,
+  1088: Network.METIS_MAINNET,
+  1101: Network.POLYGONZKEVM_MAINNET,
+  1442: Network.POLYGONZKEVM_TESTNET,
+  1946: Network.SONEIUM_MINATO,
+  2442: Network.POLYGONZKEVM_CARDONA,
+  4002: Network.FANTOM_TESTNET,
+  4801: Network.WORLDCHAIN_SEPOLIA,
+  5000: Network.MANTLE_MAINNET,
+  5003: Network.MANTLE_SEPOLIA,
+  5611: Network.OPBNB_TESTNET,
+  7000: Network.ZETACHAIN_MAINNET,
+  7001: Network.ZETACHAIN_TESTNET,
+  8453: Network.BASE_MAINNET,
+  10200: Network.GNOSIS_CHIADO,
+  11011: Network.SHAPE_SEPOLIA,
+  42161: Network.ARB_MAINNET,
+  42220: Network.CELO_MAINNET,
+  43113: Network.AVAX_FUJI,
+  43114: Network.AVAX_MAINNET,
+  42170: Network.ARBNOVA_MAINNET,
+  44787: Network.CELO_ALFAJORES,
+  59141: Network.LINEA_SEPOLIA,
+  59144: Network.LINEA_MAINNET,
+  80001: Network.MATIC_MUMBAI,
+  80002: Network.MATIC_AMOY,
+  80084: Network.BERACHAIN_BARTIO,
+  81457: Network.BLAST_MAINNET,
+  84531: Network.BASE_GOERLI,
+  84532: Network.BASE_SEPOLIA,
+  421613: Network.ARB_GOERLI,
+  421614: Network.ARB_SEPOLIA,
+  534351: Network.SCROLL_SEPOLIA,
+  534352: Network.SCROLL_MAINNET,
+  11155111: Network.ETH_SEPOLIA,
+  11155420: Network.OPT_SEPOLIA,
+  168587773: Network.BLAST_SEPOLIA
+};
---
+++ src/lib/activity/evm/parse/alchemy.ts
@@ -0,0 +1,186 @@
+import { AssetTransfersCategory, AssetTransfersWithMetadataResult } from 'alchemy-sdk';
+
+import { ActivityOperKindEnum, ActivityOperTransferType, EvmActivityAsset, EvmOperation } from 'lib/activity/types';
+import { EVM_TOKEN_SLUG } from 'lib/assets/defaults';
+
+export function parseTransfer(transfer: AssetTransfersWithMetadataResult, accAddress: string): EvmOperation {
+  const fromAddress = transfer.from;
+  const toAddress = transfer.to;
+
+  if (!fromAddress || !toAddress) return buildInteraction(transfer, accAddress);
+
+  const contractAddress = transfer.rawContract.address;
+
+  // TODO: to/from contract/account recognition
+  const type = fromAddress === accAddress ? ActivityOperTransferType.send : ActivityOperTransferType.receive;
+
+  if (transfer.category === AssetTransfersCategory.EXTERNAL) {
+    // fromAddress is an account's address for 'external' transfers
+    const type = toAddress === accAddress ? ActivityOperTransferType.receiveFromAccount : ActivityOperTransferType.send;
+
+    const { decimal, value } = transfer.rawContract;
+
+    const amount = value ? hexToStringInteger(value) : undefined;
+    const amountSigned = amount ? (fromAddress === accAddress ? `-${amount}` : amount) : undefined;
+
+    const asset: EvmActivityAsset = {
+      contract: EVM_TOKEN_SLUG,
+      amountSigned,
+      symbol: transfer.asset ?? undefined,
+      decimals: decimal ? Number(decimal) : undefined
+    };
+
+    return {
+      kind: ActivityOperKindEnum.transfer,
+      type,
+      fromAddress,
+      toAddress,
+      asset
+    };
+  }
+
+  if (transfer.category === AssetTransfersCategory.INTERNAL) {
+    // fromAddress is contract address for 'internal' transfers
+    const type = toAddress === accAddress ? ActivityOperTransferType.receive : ActivityOperTransferType.send;
+
+    const { decimal, value } = transfer.rawContract;
+
+    const amount = value ? hexToStringInteger(value) : undefined;
+    const amountSigned = amount ? (fromAddress === accAddress ? `-${amount}` : amount) : undefined;
+
+    const asset: EvmActivityAsset = {
+      contract: EVM_TOKEN_SLUG,
+      amountSigned,
+      symbol: transfer.asset ?? undefined,
+      decimals: decimal ? Number(decimal) : undefined
+    };
+
+    return {
+      kind: ActivityOperKindEnum.transfer,
+      type,
+      fromAddress,
+      toAddress,
+      asset
+    };
+  }
+
+  if (transfer.category === AssetTransfersCategory.ERC721) {
+    if (!contractAddress) return buildInteraction(transfer, accAddress);
+
+    const tokenId = transfer.erc721TokenId;
+
+    const asset: EvmActivityAsset = {
+      contract: contractAddress,
+      tokenId: tokenId ? hexToStringInteger(tokenId) : undefined,
+      amountSigned: fromAddress === accAddress ? '-1' : '1',
+      symbol: transfer.asset ?? undefined,
+      decimals: 0,
+      nft: true
+    };
+
+    return {
+      kind: ActivityOperKindEnum.transfer,
+      type,
+      fromAddress,
+      toAddress,
+      asset
+    };
+  }
+
+  if (transfer.category === AssetTransfersCategory.ERC1155) {
+    const erc1155Metadata = transfer.erc1155Metadata?.at(0);
+
+    if (transfer.hash === '0xf1ec44d7cbff7eca47ec34675eab1b1a36a2b0fa3e480acc441c71290fff2508')
+      console.log('FUCK:', 1, contractAddress, erc1155Metadata, transfer);
+
+    if (!contractAddress || !erc1155Metadata) return buildInteraction(transfer, accAddress);
+
+    const { tokenId, value } = erc1155Metadata;
+
+    const amount = hexToStringInteger(value);
+
+    const asset: EvmActivityAsset = {
+      contract: contractAddress,
+      tokenId: tokenId ? hexToStringInteger(tokenId) : undefined,
+      amountSigned: fromAddress === accAddress ? `-${amount}` : amount,
+      symbol: transfer.asset ?? undefined,
+      decimals: 0,
+      nft: true
+    };
+
+    if (transfer.hash === '0xf1ec44d7cbff7eca47ec34675eab1b1a36a2b0fa3e480acc441c71290fff2508')
+      console.log('FUCK:', 2, type, asset);
+
+    return {
+      kind: ActivityOperKindEnum.transfer,
+      type,
+      fromAddress,
+      toAddress,
+      asset
+    };
+  }
+
+  if (transfer.category === AssetTransfersCategory.ERC20) {
+    if (!contractAddress) return buildInteraction(transfer, accAddress);
+
+    const { decimal, value } = transfer.rawContract;
+
+    const amount = value ? hexToStringInteger(value) : undefined;
+    const amountSigned = amount ? (fromAddress === accAddress ? `-${amount}` : amount) : undefined;
+
+    const asset: EvmActivityAsset = {
+      contract: contractAddress,
+      amountSigned,
+      symbol: transfer.asset ?? undefined,
+      decimals: decimal ? Number(decimal) : undefined
+    };
+
+    return {
+      kind: ActivityOperKindEnum.transfer,
+      type,
+      fromAddress,
+      toAddress,
+      asset
+    };
+  }
+
+  if (transfer.category === AssetTransfersCategory.SPECIALNFT) {
+    if (!contractAddress) return buildInteraction(transfer, accAddress);
+
+    const tokenId = transfer.tokenId;
+
+    const { decimal, value } = transfer.rawContract;
+
+    const amount = value ? hexToStringInteger(value) : undefined;
+    const amountSigned = amount ? (fromAddress === accAddress ? `-${amount}` : amount) : undefined;
+
+    const asset: EvmActivityAsset = {
+      contract: contractAddress,
+      tokenId: tokenId ? hexToStringInteger(tokenId) : undefined,
+      amountSigned,
+      symbol: transfer.asset ?? undefined,
+      decimals: decimal ? Number(decimal) : undefined,
+      nft: true
+    };
+
+    return {
+      kind: ActivityOperKindEnum.transfer,
+      type,
+      fromAddress,
+      toAddress,
+      asset
+    };
+  }
+
+  return buildInteraction(transfer, accAddress);
+}
+
+function buildInteraction(transfer: AssetTransfersWithMetadataResult, accAddress: string): EvmOperation {
+  const withAddress = transfer.from === accAddress ? transfer.to ?? undefined : transfer.from;
+
+  return { kind: ActivityOperKindEnum.interaction, withAddress };
+}
+
+function hexToStringInteger(hex: string) {
+  return BigInt(hex).toString();
+}
