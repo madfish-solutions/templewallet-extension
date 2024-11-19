7109ca5749f40193913034b05a46261d3970c2ba
Inokentii Mazhara <keshan3262@gmail.com>
2024-11-19 10:50:28 +0200

    TW-1579 Add some more countries flags

--- src/app/atoms/Flag.tsx
+++ src/app/atoms/Flag.tsx
@@ -1,15 +1,108 @@
-import React, { HTMLAttributes, memo, useCallback, useState } from 'react';
+import React, { HTMLAttributes, memo, useCallback, useMemo, useState } from 'react';
 
 import classNames from 'clsx';
+import browser from 'webextension-polyfill';
 
-type FlagProps = {
+const atlasCountriesCodes = [
+  'ae',
+  'ar',
+  'au',
+  'az',
+  'bg',
+  'bh',
+  'br',
+  'ca',
+  'ch',
+  'cl',
+  'cn',
+  'co',
+  'cr',
+  'cz',
+  'de',
+  'dk',
+  'do',
+  'eg',
+  'eu',
+  'fr',
+  'gb',
+  'ge',
+  'gt',
+  'hk',
+  'hn',
+  'hr',
+  'hu',
+  'id',
+  'il',
+  'in',
+  'jo',
+  'jp',
+  'kn',
+  'kr',
+  'kw',
+  'kz',
+  'lk',
+  'md',
+  'mx',
+  'my',
+  'ng',
+  'no',
+  'nz',
+  'om',
+  'pe',
+  'ph',
+  'pl',
+  'pt',
+  'py',
+  'qa',
+  'ro',
+  'rw',
+  'sa',
+  'se',
+  'th',
+  'tr',
+  'tw',
+  'ua',
+  'us',
+  'uy',
+  'vn',
+  'za'
+];
+const atlasRowSize = 7;
+const imageWidth = 40;
+const imageHeight = 30;
+
+export const canUseAtlasFlag = (countryCode: string) => atlasCountriesCodes.includes(countryCode);
+
+interface FlagProps {
   alt: string;
+  countryCode?: string;
   className?: string;
   src?: string;
-};
+}
 
-const Flag = memo<FlagProps>(({ alt, className, src }) => {
+export const Flag = memo<FlagProps>(({ alt, className, countryCode, src }) => {
   const [error, setError] = useState(false);
+
+  const bgFromAtlasStyle = useMemo(() => {
+    if (src || !countryCode) {
+      return undefined;
+    }
+
+    const index = atlasCountriesCodes.indexOf(countryCode);
+
+    if (index === -1) {
+      return undefined;
+    }
+
+    const row = Math.floor(index / atlasRowSize);
+    const col = index % atlasRowSize;
+
+    return {
+      backgroundImage: `url(${browser.runtime.getURL('/misc/country-flags/atlas.png')})`,
+      backgroundPosition: `${-(col * imageWidth) / 2}px ${-(row * imageHeight) / 2}px`,
+      backgroundSize: `${atlasRowSize * 1.25}rem`
+    };
+  }, [countryCode, src]);
 
   const handleError = useCallback(() => {
     setError(true);
@@ -17,9 +110,13 @@ const Flag = memo<FlagProps>(({ alt, className, src }) => {
 
   return (
     <div className={classNames('w-6 h-6 flex justify-center items-center', className)}>
-      {src ? (
+      {src || bgFromAtlasStyle ? (
         <>
-          <img alt={alt} className={classNames({ hidden: error }, 'w-5 h-auto')} src={src} onError={handleError} />
+          {src ? (
+            <img alt={alt} className={classNames({ hidden: error }, 'w-5 h-auto')} src={src} onError={handleError} />
+          ) : (
+            <div className="w-5 aspect-[4/3]" style={bgFromAtlasStyle} />
+          )}
           {error && <FlagStub className="w-6 h-auto" />}
         </>
       ) : (
@@ -28,8 +125,6 @@ const Flag = memo<FlagProps>(({ alt, className, src }) => {
     </div>
   );
 });
-
-export default Flag;
 
 const FlagStub = memo((props: HTMLAttributes<unknown>) => (
   <svg
