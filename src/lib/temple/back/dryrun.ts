958ca58abe6af97f064d09835971edb7b1d54aa5
Inokentii Mazhara <keshan3262@gmail.com>
2024-12-26 12:54:03 +0200

    TW-1622 Implement estimation retry if 'gas_exhausted' error is received

--- src/lib/temple/back/dryrun.ts
+++ src/lib/temple/back/dryrun.ts
@@ -1,7 +1,8 @@
 import { localForger } from '@taquito/local-forging';
 import { ForgeOperationsParams } from '@taquito/rpc';
-import { Estimate, TezosToolkit } from '@taquito/taquito';
+import { Estimate, TezosOperationError, TezosToolkit } from '@taquito/taquito';
 
+import { FEE_PER_GAS_UNIT } from 'lib/constants';
 import { formatOpParamsBeforeSend, michelEncoder, loadFastRpcClient } from 'lib/temple/helpers';
 import { ReadOnlySigner } from 'lib/temple/read-only-signer';
 
@@ -10,6 +11,8 @@ type DryRunParams = {
   networkRpc: string;
   sourcePkh: string;
   sourcePublicKey: string;
+  attemptCounter?: number;
+  prevFailedOperationIndex?: number;
 };
 
 export interface DryRunResult {
@@ -26,7 +29,9 @@ export async function dryRunOpParams({
   opParams,
   networkRpc,
   sourcePkh,
-  sourcePublicKey
+  sourcePublicKey,
+  attemptCounter = 0,
+  prevFailedOperationIndex = -1
 }: DryRunParams): Promise<DryRunResult | null> {
   try {
     const tezos = new TezosToolkit(loadFastRpcClient(networkRpc));
@@ -46,6 +51,36 @@ export async function dryRunOpParams({
       const [estimationResult] = await Promise.allSettled([tezos.estimate.batch(formatted)]);
       const [contractBatchResult] = await Promise.allSettled([tezos.contract.batch(formatted).send()]);
       if (estimationResult.status === 'rejected' && contractBatchResult.status === 'rejected') {
+        if (
+          estimationResult.reason instanceof TezosOperationError &&
+          estimationResult.reason.errors.some(error => error.id.includes('gas_exhausted'))
+        ) {
+          const { operationsWithResults } = estimationResult.reason;
+          const firstSkippedOperationIndex = operationsWithResults.findIndex(
+            op =>
+              'metadata' in op && 'operation_result' in op.metadata && op.metadata.operation_result.status === 'skipped'
+          );
+          // An internal operation of this operation may be marked as failed but this one as backtracked
+          const failedOperationIndex =
+            firstSkippedOperationIndex === -1 ? operationsWithResults.length - 1 : firstSkippedOperationIndex - 1;
+          const failedOperationWithResult = operationsWithResults[failedOperationIndex];
+          if ('gas_limit' in failedOperationWithResult) {
+            const newOpParams = Array.from(opParams);
+            newOpParams[failedOperationIndex].gasLimit =
+              Math.max(opParams[failedOperationIndex].gasLimit ?? 0, Number(failedOperationWithResult.gas_limit)) * 2;
+
+            if (attemptCounter < 3) {
+              return dryRunOpParams({
+                opParams: newOpParams,
+                networkRpc,
+                sourcePkh,
+                sourcePublicKey,
+                attemptCounter: failedOperationIndex > prevFailedOperationIndex ? 0 : attemptCounter + 1,
+                prevFailedOperationIndex: Math.max(failedOperationIndex, prevFailedOperationIndex)
+              });
+            }
+          }
+        }
         error = [
           { ...estimationResult.reason, isError: true },
           { ...contractBatchResult.reason, isError: true }
@@ -61,7 +96,9 @@ export async function dryRunOpParams({
               consumedMilligas: e.consumedMilligas,
               gasLimit: e.gasLimit,
               minimalFeeMutez: e.minimalFeeMutez,
-              suggestedFeeMutez: e.suggestedFeeMutez,
+              suggestedFeeMutez:
+                e.suggestedFeeMutez +
+                (opParams[i]?.gasLimit ? Math.ceil((opParams[i].gasLimit - e.gasLimit) * FEE_PER_GAS_UNIT) : 0),
               storageLimit: opParams[i]?.storageLimit ? +opParams[i].storageLimit : e.storageLimit,
               totalCost: e.totalCost,
               usingBaseFeeMutez: e.usingBaseFeeMutez
