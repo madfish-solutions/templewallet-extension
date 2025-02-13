0e0137bd5a1e7c22753e10b67356fbdfa173cd09
lendihop <mishin.danil2017@gmail.com>
2025-02-13 14:56:15 +0100

    put back unrelated changes

--- src/lib/swr/index.ts
+++ src/lib/swr/index.ts
@@ -1,16 +1,18 @@
-import useSWR, { Key, SWRConfiguration, SWRResponse } from 'swr';
+import useSWR, { Key, SWRConfiguration } from 'swr';
 import { FetcherResponse } from 'swr/_internal';
 
 type Fetcher<Data = unknown, SWRKey extends Key = Key> = (arg: SWRKey) => FetcherResponse<Data>;
 
+/** Fetcher must not return (awaited) `undefined` value - results in endless fetching. */
 export const useTypedSWR = <Data, Error = any, SWRKey extends Key = Key>(
   key: SWRKey,
   fetcher: Fetcher<Data, SWRKey> | null,
   config?: SWRConfiguration<Data, Error, Fetcher<Data, SWRKey>>
-): SWRResponse<Data, Error> => useSWR(key, fetcher, config);
+) => useSWR(key, fetcher, config);
 
+/** Fetcher must not return (awaited) `undefined` value - results in endless fetching. */
 export const useRetryableSWR = <Data, Error = any, SWRKey extends Key = Key>(
   key: SWRKey,
   fetcher: Fetcher<Data, SWRKey> | null,
   config?: SWRConfiguration<Data, Error, Fetcher<Data, SWRKey>>
-): SWRResponse<Data, Error> => useSWR(key, fetcher, { errorRetryCount: 2, ...config });
+) => useSWR(key, fetcher, { errorRetryCount: 2, ...config });
