f956892fdbf1f71839279b58b1f2b8e0746d9d21
lendihop <mishin.danil2017@gmail.com>
2025-02-16 17:59:02 +0100

    apply last changes to select currency layout

--- src/app/pages/Market/debit-credit-card/contents/SelectCurrency.tsx
+++ src/app/pages/Market/debit-credit-card/contents/SelectCurrency.tsx
@@ -1,14 +1,12 @@
-import React, { FC, useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import React, { FC, useCallback, useLayoutEffect, useMemo, useState } from 'react';
 
 import { useFormContext } from 'react-hook-form-v7';
 import { useDebounce } from 'use-debounce';
 
 import { EmptyState } from 'app/atoms/EmptyState';
 import { BackButton } from 'app/atoms/PageModal';
-import { RadioButton } from 'app/atoms/RadioButton';
 import { SearchBarField } from 'app/templates/SearchField';
 import { t } from 'lib/i18n';
-import { useScrollIntoViewOnMount } from 'lib/ui/use-scroll-into-view';
 import { isSearchStringApplicable, searchAndFilterItems } from 'lib/utils/search-items';
 
 import { ModalHeaderConfig } from '../../types';
@@ -26,25 +24,17 @@ export const SelectCurrency: FC<Props> = ({ setModalHeaderConfig, onGoBack }) =>
   const [searchValueDebounced] = useDebounce(searchValue, 300);
   const inSearch = isSearchStringApplicable(searchValueDebounced);
 
-  const [attractSelectedCurrency, setAttractSelectedCurrency] = useState(true);
-
-  const { watch, setValue } = useFormContext<FormData>();
-
-  const activeCurrency = watch('inputCurrency');
+  const { setValue } = useFormContext<FormData>();
 
   useLayoutEffect(
     () => void setModalHeaderConfig({ title: t('selectCurrency'), titleLeft: <BackButton onClick={onGoBack} /> }),
-    []
+    [onGoBack, setModalHeaderConfig]
   );
 
   const searchedCurrencies = useMemo(
     () => (inSearch ? searchAndFilterCurrencies(currenciesMock, searchValueDebounced) : currenciesMock),
     [inSearch, searchValueDebounced]
   );
-
-  useEffect(() => {
-    if (searchValueDebounced) setAttractSelectedCurrency(false);
-  }, [searchValueDebounced]);
 
   const onCurrencySelect = useCallback(
     (currency: TopUpInputInterface) => {
@@ -57,21 +47,14 @@ export const SelectCurrency: FC<Props> = ({ setModalHeaderConfig, onGoBack }) =>
   return (
     <>
       <div className="p-4">
-        <SearchBarField value={searchValue} onValueChange={setSearchValue} className="p-4" />
+        <SearchBarField value={searchValue} defaultRightMargin={false} onValueChange={setSearchValue} className="p-4" />
       </div>
 
       <div className="px-4 pb-1 flex-grow flex flex-col overflow-y-auto">
         {searchedCurrencies.length === 0 && <EmptyState />}
 
         {searchedCurrencies.map(currency => (
-          <Currency
-            key={currency.code}
-            currency={currency}
-            activeCurrency={activeCurrency}
-            attractSelf={attractSelectedCurrency}
-            showBalance
-            onClick={onCurrencySelect}
-          />
+          <Currency key={currency.code} currency={currency} onClick={onCurrencySelect} />
         ))}
       </div>
     </>
@@ -80,37 +63,29 @@ export const SelectCurrency: FC<Props> = ({ setModalHeaderConfig, onGoBack }) =>
 
 interface CurrencyProps {
   currency: TopUpInputInterface;
-  activeCurrency: TopUpInputInterface;
-  attractSelf?: boolean;
-  showBalance?: boolean;
   iconSize?: number;
   onClick?: (currency: TopUpInputInterface) => void;
 }
 
-export const Currency: FC<CurrencyProps> = ({ currency, activeCurrency, attractSelf, iconSize = 24, onClick }) => {
-  const active = currency.code === activeCurrency.code;
-
-  const elemRef = useScrollIntoViewOnMount<HTMLDivElement>(active && attractSelf);
-
+export const Currency: FC<CurrencyProps> = ({ currency, iconSize = 24, onClick }) => {
   const handleClick = useCallback(() => onClick?.(currency), [currency, onClick]);
 
   return (
     <div
-      ref={elemRef}
-      className="cursor-pointer mb-3 flex justify-between items-center py-2 px-3 rounded-lg shadow-bottom border-0.5 border-transparent group"
+      className="w-full flex justify-start items-center cursor-pointer p-2 rounded-8 hover:bg-secondary-low"
       onClick={handleClick}
     >
-      <div className="flex items-center gap-x-2">
-        <AssetIcon useFlagIcon size={iconSize} src={currency.icon} code={currency.code} />
+      <div className="flex items-center gap-x-2 min-h-10">
+        <div className="w-9 h-9 flex justify-center items-center rounded-full bg-grey-4">
+          <AssetIcon useFlagIcon size={iconSize} src={currency.icon} code={currency.code} />
+        </div>
 
         <div className="flex flex-col">
-          <span className="text-font-medium-bold">{currency.code}</span>
+          <span className="text-font-medium">{currency.code}</span>
 
-          <span className="text-grey-1 text-font-small">{currency.name}</span>
+          <span className="text-font-description text-grey-1 w-40 truncate">{currency.name}</span>
         </div>
       </div>
-
-      <RadioButton active={active} className={active ? undefined : 'opacity-0 group-hover:opacity-100'} />
     </div>
   );
 };
