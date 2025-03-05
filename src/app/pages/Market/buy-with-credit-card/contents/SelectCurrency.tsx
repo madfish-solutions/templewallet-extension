1319db2063a32da1558b65550762047464cdba21
lendihop <mishin.danil2017@gmail.com>
2025-03-05 20:37:26 +0100

    refactoring

--- src/app/pages/Market/buy-with-credit-card/contents/SelectCurrency.tsx
+++ src/app/pages/Market/buy-with-credit-card/contents/SelectCurrency.tsx
@@ -1,22 +1,17 @@
-import React, { FC, useCallback, useLayoutEffect, useMemo, useState } from 'react';
+import React, { FC, useCallback, useLayoutEffect } from 'react';
 
 import { useFormContext } from 'react-hook-form-v7';
-import { useDebounce } from 'use-debounce';
 
-import { FadeTransition } from 'app/a11y/FadeTransition';
-import { EmptyState } from 'app/atoms/EmptyState';
-import { PageLoader } from 'app/atoms/Loader';
 import { BackButton } from 'app/atoms/PageModal';
 import { useCurrenciesLoadingSelector } from 'app/store/buy-with-credit-card/selectors';
-import { SearchBarField } from 'app/templates/SearchField';
 import { TopUpInputInterface } from 'lib/buy-with-credit-card/topup.interface';
 import { t } from 'lib/i18n';
-import { isSearchStringApplicable, searchAndFilterItems } from 'lib/utils/search-items';
 
 import { ModalHeaderConfig } from '../../types';
-import { AssetIcon } from '../components/AssetIcon';
 import { BuyWithCreditCardFormData } from '../form-data.interface';
 import { useAllFiatCurrencies } from '../hooks/use-all-fiat-currencies';
+
+import { SelectAssetBase } from './SelectAssetBase';
 
 interface Props {
   setModalHeaderConfig: SyncFn<ModalHeaderConfig>;
@@ -25,14 +20,15 @@ interface Props {
 }
 
 export const SelectCurrency: FC<Props> = ({ setModalHeaderConfig, onCurrencySelect, onGoBack }) => {
-  const [searchValue, setSearchValue] = useState('');
-  const [searchValueDebounced] = useDebounce(searchValue, 300);
-  const inSearch = isSearchStringApplicable(searchValueDebounced);
-
   const { watch, setValue } = useFormContext<BuyWithCreditCardFormData>();
 
   const inputCurrency = watch('inputCurrency');
   const outputToken = watch('outputToken');
+
+  useLayoutEffect(
+    () => void setModalHeaderConfig({ title: t('selectCurrency'), titleLeft: <BackButton onClick={onGoBack} /> }),
+    [onGoBack, setModalHeaderConfig]
+  );
 
   const { fiatCurrenciesWithPairLimits: allFiatCurrencies } = useAllFiatCurrencies(
     inputCurrency.code,
@@ -40,16 +36,6 @@ export const SelectCurrency: FC<Props> = ({ setModalHeaderConfig, onCurrencySele
   );
 
   const currenciesLoading = useCurrenciesLoadingSelector();
-
-  useLayoutEffect(
-    () => void setModalHeaderConfig({ title: t('selectCurrency'), titleLeft: <BackButton onClick={onGoBack} /> }),
-    [onGoBack, setModalHeaderConfig]
-  );
-
-  const searchedCurrencies = useMemo(
-    () => (inSearch ? searchAndFilterCurrencies(allFiatCurrencies, searchValueDebounced) : allFiatCurrencies),
-    [allFiatCurrencies, inSearch, searchValueDebounced]
-  );
 
   const handleCurrencySelect = useCallback(
     (currency: TopUpInputInterface) => {
@@ -61,56 +47,10 @@ export const SelectCurrency: FC<Props> = ({ setModalHeaderConfig, onCurrencySele
   );
 
   return (
-    <FadeTransition>
-      <div className="p-4">
-        <SearchBarField value={searchValue} defaultRightMargin={false} onValueChange={setSearchValue} className="p-4" />
-      </div>
-
-      <div className="px-4 pb-1 flex-grow flex flex-col overflow-y-auto">
-        {currenciesLoading ? (
-          <PageLoader stretch />
-        ) : searchedCurrencies.length === 0 ? (
-          <EmptyState stretch />
-        ) : (
-          <>
-            {searchedCurrencies.map(currency => (
-              <Currency key={currency.code} currency={currency} onClick={handleCurrencySelect} />
-            ))}
-          </>
-        )}
-      </div>
-    </FadeTransition>
+    <SelectAssetBase<TopUpInputInterface>
+      assets={allFiatCurrencies}
+      loading={currenciesLoading}
+      onCurrencySelect={handleCurrencySelect}
+    />
   );
 };
-
-interface CurrencyProps {
-  currency: TopUpInputInterface;
-  onClick?: (currency: TopUpInputInterface) => void;
-}
-
-const Currency: FC<CurrencyProps> = ({ currency, onClick }) => {
-  const handleClick = useCallback(() => onClick?.(currency), [currency, onClick]);
-
-  return (
-    <div
-      className="w-full flex justify-start items-center cursor-pointer p-2 rounded-8 hover:bg-secondary-low"
-      onClick={handleClick}
-    >
-      <div className="flex items-center gap-x-2 min-h-10">
-        <AssetIcon useFlagIcon src={currency.icon} code={currency.code} />
-
-        <div className="flex flex-col">
-          <span className="text-font-medium">{currency.code}</span>
-
-          <span className="text-font-description text-grey-1 w-40 truncate">{currency.name}</span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const searchAndFilterCurrencies = (currencies: TopUpInputInterface[], searchValue: string) =>
-  searchAndFilterItems(currencies, searchValue.trim(), [
-    { name: 'name', weight: 1 },
-    { name: 'code', weight: 1 }
-  ]);
--- src/app/pages/Market/buy-with-credit-card/contents/SelectToken.tsx
+++ src/app/pages/Market/buy-with-credit-card/contents/SelectToken.tsx
@@ -1,32 +1,20 @@
-import React, { FC, useCallback, useLayoutEffect, useMemo, useState } from 'react';
+import React, { FC, useCallback, useLayoutEffect, useMemo } from 'react';
 
 import { useFormContext } from 'react-hook-form-v7';
-import { useDebounce } from 'use-debounce';
 
-import { FadeTransition } from 'app/a11y/FadeTransition';
-import { EmptyState } from 'app/atoms/EmptyState';
-import { PageLoader } from 'app/atoms/Loader';
 import { BackButton } from 'app/atoms/PageModal';
 import { useCurrenciesLoadingSelector } from 'app/store/buy-with-credit-card/selectors';
-import { SearchBarField } from 'app/templates/SearchField';
-import { getAssetSymbolToDisplay } from 'lib/buy-with-credit-card/get-asset-symbol-to-display';
 import { fromTopUpTokenSlug } from 'lib/buy-with-credit-card/top-up-token-slug.utils';
 import { TopUpOutputInterface } from 'lib/buy-with-credit-card/topup.interface';
 import { t } from 'lib/i18n';
-import { isSearchStringApplicable, searchAndFilterItems } from 'lib/utils/search-items';
-import {
-  useAccountAddressForEvm,
-  useAccountAddressForTezos,
-  useEnabledEvmChains,
-  useTezosMainnetChain
-} from 'temple/front';
-import { useEvmChainByChainId } from 'temple/front/chains';
+import { useAccountAddressForEvm, useAccountAddressForTezos, useEnabledEvmChains } from 'temple/front';
 import { TempleChainKind } from 'temple/types';
 
 import { ModalHeaderConfig } from '../../types';
-import { AssetIcon } from '../components/AssetIcon';
 import { BuyWithCreditCardFormData } from '../form-data.interface';
 import { useAllCryptoCurrencies } from '../hooks/use-all-crypto-currencies';
+
+import { SelectAssetBase } from './SelectAssetBase';
 
 interface Props {
   setModalHeaderConfig: SyncFn<ModalHeaderConfig>;
@@ -35,10 +23,6 @@ interface Props {
 }
 
 export const SelectToken: FC<Props> = ({ setModalHeaderConfig, onTokenSelect, onGoBack }) => {
-  const [searchValue, setSearchValue] = useState('');
-  const [searchValueDebounced] = useDebounce(searchValue, 300);
-  const inSearch = isSearchStringApplicable(searchValueDebounced);
-
   const { setValue } = useFormContext<BuyWithCreditCardFormData>();
 
   const evmChains = useEnabledEvmChains();
@@ -67,11 +51,6 @@ export const SelectToken: FC<Props> = ({ setModalHeaderConfig, onTokenSelect, on
     [allTokens, evmAddress, evmChains, tezosAddress]
   );
 
-  const searchedTokens = useMemo(
-    () => (inSearch ? searchAndFilterTokens(enabledTokens, searchValueDebounced) : enabledTokens),
-    [enabledTokens, inSearch, searchValueDebounced]
-  );
-
   const handleTokenSelect = useCallback(
     (token: TopUpOutputInterface) => {
       setValue('outputToken', token);
@@ -82,67 +61,10 @@ export const SelectToken: FC<Props> = ({ setModalHeaderConfig, onTokenSelect, on
   );
 
   return (
-    <FadeTransition>
-      <div className="p-4">
-        <SearchBarField value={searchValue} defaultRightMargin={false} onValueChange={setSearchValue} className="p-4" />
-      </div>
-
-      <div className="px-4 pb-1 flex-grow flex flex-col overflow-y-auto">
-        {currenciesLoading ? (
-          <PageLoader stretch />
-        ) : searchedTokens.length === 0 ? (
-          <EmptyState stretch />
-        ) : (
-          <>
-            {searchedTokens.map(token => (
-              <Token key={token.code} token={token} onClick={handleTokenSelect} />
-            ))}
-          </>
-        )}
-      </div>
-    </FadeTransition>
+    <SelectAssetBase<TopUpOutputInterface>
+      assets={enabledTokens}
+      loading={currenciesLoading}
+      onCurrencySelect={handleTokenSelect}
+    />
   );
 };
-
-interface TokenProps {
-  token: TopUpOutputInterface;
-  onClick?: (currency: TopUpOutputInterface) => void;
-}
-
-const Token: FC<TokenProps> = ({ token, onClick }) => {
-  const [_, chainKind, chainId] = useMemo(() => fromTopUpTokenSlug(token.slug), [token.slug]);
-
-  const isTez = chainKind === TempleChainKind.Tezos;
-
-  const tezosMainnet = useTezosMainnetChain();
-  const evmChain = useEvmChainByChainId(Number(chainId));
-
-  const networkName = isTez ? tezosMainnet.name : evmChain?.name ?? 'Unknown';
-
-  const handleClick = useCallback(() => onClick?.(token), [token, onClick]);
-
-  return (
-    <div
-      className="w-full flex justify-start items-center cursor-pointer p-2 rounded-8 hover:bg-secondary-low"
-      onClick={handleClick}
-    >
-      <div className="flex items-center gap-x-2 min-h-10">
-        <AssetIcon src={token.icon} code={token.code} />
-
-        <div className="flex flex-col">
-          <span className="text-font-medium">{getAssetSymbolToDisplay(token)}</span>
-
-          <span className="text-font-description text-grey-1 w-40 truncate">{token.name}</span>
-        </div>
-      </div>
-
-      <p className="text-end text-font-num-12 text-grey-1 w-40 truncate">{networkName}</p>
-    </div>
-  );
-};
-
-const searchAndFilterTokens = (currencies: TopUpOutputInterface[], searchValue: string) =>
-  searchAndFilterItems(currencies, searchValue.trim(), [
-    { name: 'name', weight: 1 },
-    { name: 'code', weight: 1 }
-  ]);
