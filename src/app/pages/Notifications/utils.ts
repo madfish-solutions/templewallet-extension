5662caa92acc74ab9341dca8d71d0ea2ef8a8436
lendihop <mishin.danil2017@gmail.com>
2025-03-06 11:41:43 +0100

    use formatDate

--- src/app/pages/Notifications/utils.ts
+++ src/app/pages/Notifications/utils.ts
@@ -1,5 +1,5 @@
 import { formatDate } from 'lib/i18n';
 
-export const formatGeneralDate = (date: string) => formatDate(date, 'dd/MM/yyyy');
+export const formatGeneralDate = (date: string) => formatDate(date, 'P');
 
 export const formatWeekdayHourDate = (date: string) => formatDate(date, 'EEEE HH:mm');
