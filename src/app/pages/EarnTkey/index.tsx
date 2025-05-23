7710d14f0cb98f2cc8a8dad3839ef0dd7c956a68
lendihop <mishin.danil2017@gmail.com>
2025-05-23 17:14:48 +0200

    TW-1696: earn tkey page layout

--- src/app/pages/EarnTkey/index.tsx
+++ src/app/pages/EarnTkey/index.tsx
@@ -1,5 +1,79 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
+
+import { IconBase } from 'app/atoms';
+import { ActionsButtonsBox } from 'app/atoms/PageModal';
+import { StyledButton } from 'app/atoms/StyledButton';
+import { ReactComponent as AdjustmentIcon } from 'app/icons/base/adjustment.svg';
+import { ReactComponent as AdsIcon } from 'app/icons/base/ads_fill.svg';
+import { ReactComponent as ChartIcon } from 'app/icons/base/chart_fill.svg';
+import { ReactComponent as ScheduleIcon } from 'app/icons/base/schedule.svg';
+import PageLayout from 'app/layouts/PageLayout';
+import { dispatch } from 'app/store';
+import { togglePartnersPromotionAction } from 'app/store/partners-promotion/actions';
+import { useShouldShowPartnersPromoSelector } from 'app/store/partners-promotion/selectors';
+import { toastSuccess } from 'app/toaster';
+import { T, t, TID } from 'lib/i18n';
+
+import { EarnTkeySelectors } from './selectors';
+
+interface Advantage {
+  Icon: ImportedSVGComponent;
+  textI18nKey: TID;
+}
+
+const advantages: Advantage[] = [
+  { Icon: AdsIcon, textI18nKey: 'nonIntrusiveAds' },
+  { Icon: ChartIcon, textI18nKey: 'earnOnBackground' },
+  { Icon: ScheduleIcon, textI18nKey: 'autoPayouts' },
+  { Icon: AdjustmentIcon, textI18nKey: 'turnOnWhenNeeded' }
+];
 
 export const EarnTkeyPage = memo(() => {
-  return <div>Earn Tkey</div>;
+  const isEnabled = useShouldShowPartnersPromoSelector();
+
+  const handleStartEarningClick = useCallback(() => {
+    dispatch(togglePartnersPromotionAction(true));
+    toastSuccess(t('rewardsEarningEnabled'));
+  }, []);
+
+  return (
+    <PageLayout pageTitle={t('earn')} contentPadding={false}>
+      <div className="flex-1 px-4 flex flex-col overflow-y-auto">
+        <h3 className="text-font-h3 text-center pb-4">
+          <T id="earnTkeyHeadline" />
+        </h3>
+
+        <div className="grid grid-cols-2 gap-3 pb-5">
+          {advantages.map(a => (
+            <AdvantageComponent key={a.textI18nKey} {...a} />
+          ))}
+        </div>
+
+        <p className="text-font-small text-grey-1 text-center pb-8">
+          <T id="earnTkeyDisclaimer" />
+        </p>
+      </div>
+
+      <ActionsButtonsBox bgSet={false}>
+        <StyledButton
+          size="L"
+          color="primary"
+          disabled={isEnabled}
+          onClick={handleStartEarningClick}
+          testID={EarnTkeySelectors.startEarningButton}
+        >
+          <T id={isEnabled ? 'enabled' : 'startEarning'} />
+        </StyledButton>
+      </ActionsButtonsBox>
+    </PageLayout>
+  );
 });
+
+const AdvantageComponent = memo<Advantage>(({ Icon, textI18nKey }) => (
+  <div className="flex flex-col p-3 bg-grey-4 rounded-8">
+    <IconBase Icon={Icon} className="text-primary" />
+    <p className="text-font-description p-1">
+      <T id={textI18nKey} />
+    </p>
+  </div>
+));
