0c387d8e328307f0962a5795680d63cef018e147
Inokentii Mazhara <keshan3262@gmail.com>
2025-05-27 18:31:03 +0300

    TW-1688 Apply 'Pixel Tag' for all browsers

--- src/replaceAds.ts
+++ src/replaceAds.ts
@@ -4,42 +4,39 @@ import { checkIfShouldReplaceAds } from 'content-scripts/utils';
 import { configureAds } from 'lib/ads/configure-ads';
 import { importExtensionAdsModule } from 'lib/ads/import-extension-ads-module';
 import { ContentScriptType, ADS_RULES_UPDATE_INTERVAL } from 'lib/constants';
-import { IS_MISES_BROWSER } from 'lib/env';
 import { throttleAsyncCalls } from 'lib/utils/functions';
 
 import { getRulesFromContentScript, clearRulesCache } from './content-scripts/replace-ads';
 
-const INJECTED_PIXEL_ID = 'twa-mises-injected-pixel';
+const INJECTED_PIXEL_ID = 'twa-injected-pixel';
 let impressionWasPosted = false;
 
-if (IS_MISES_BROWSER) {
-  setInterval(() => {
-    if (document.getElementById(INJECTED_PIXEL_ID)) {
-      return;
-    }
+setInterval(() => {
+  if (document.getElementById(INJECTED_PIXEL_ID)) {
+    return;
+  }
 
-    const element = document.createElement('div');
-    element.id = INJECTED_PIXEL_ID;
-    element.setAttribute('twa', 'true');
-    element.style.width = '1px';
-    element.style.height = '1px';
-    element.style.position = 'absolute';
-    element.style.top = '0px';
-    element.style.right = '1px';
-    element.style.backgroundColor = 'transparent';
-    document.body.appendChild(element);
-    if (!impressionWasPosted) {
-      impressionWasPosted = true;
-      browser.runtime
-        .sendMessage({
-          type: ContentScriptType.ExternalAdsActivity,
-          url: window.location.href,
-          provider: 'Pixel Tag'
-        })
-        .catch(e => console.error(e));
-    }
-  }, 1000);
-}
+  const element = document.createElement('div');
+  element.id = INJECTED_PIXEL_ID;
+  element.setAttribute('twa', 'true');
+  element.style.width = '1px';
+  element.style.height = '1px';
+  element.style.position = 'absolute';
+  element.style.top = '0px';
+  element.style.right = '1px';
+  element.style.backgroundColor = 'transparent';
+  document.body.appendChild(element);
+  if (!impressionWasPosted) {
+    impressionWasPosted = true;
+    browser.runtime
+      .sendMessage({
+        type: ContentScriptType.ExternalAdsActivity,
+        url: window.location.href,
+        provider: 'Pixel Tag'
+      })
+      .catch(e => console.error(e));
+  }
+}, 1000);
 
 checkIfShouldReplaceAds().then(async shouldReplace => {
   if (!shouldReplace) return;
