0704123cf6fe20ddd6b0f45a0a7d25d017711f20
lendihop <mishin.danil2017@gmail.com>
2025-07-24 13:09:01 +0200

    TW-1816: dapp item hover

--- src/app/pages/Dapps/components/DappItem.tsx
+++ src/app/pages/Dapps/components/DappItem.tsx
@@ -2,7 +2,8 @@ import React, { memo } from 'react';
 
 import clsx from 'clsx';
 
-import { Anchor } from 'app/atoms';
+import { Anchor, IconBase } from 'app/atoms';
+import { ReactComponent as OutLinkIcon } from 'app/icons/base/outLink.svg';
 import { CustomDAppInfo } from 'lib/apis/temple/endpoints/get-dapps-list';
 
 import { DappsPageSelectors } from '../selectors';
@@ -11,56 +12,60 @@ import { DAppIcon } from './DappIcon';
 
 type DAppItemProps = CustomDAppInfo;
 
-export const FeaturedDappItem = memo<DAppItemProps>(({ dappUrl, name, logo }) => {
-  return (
-    <Anchor
-      className={clsx(
-        'flex flex-col w-28 h-23 relative',
-        'justify-center items-center',
-        'rounded-8 shadow-bottom',
-        'transition ease-in-out duration-200',
-        'bg-white hover:bg-gray-100'
-      )}
-      href={dappUrl}
-      rel="noreferrer"
-      testID={DappsPageSelectors.DAppOpened}
-      testIDProperties={{ dappUrl, name, featured: true }}
-      treatAsButton={true}
-    >
-      <DAppIcon name={name} logo={logo} />
-      <p className="text-font-medium mt-2 max-w-20 truncate">{name}</p>
-    </Anchor>
-  );
-});
+export const FeaturedDappItem = memo<DAppItemProps>(({ dappUrl, name, logo }) => (
+  <Anchor
+    className={clsx(
+      'flex flex-col w-28 h-23 relative group',
+      'justify-center items-center rounded-8 shadow-bottom',
+      'bg-white border-0.5 border-transparent hover:border-lines',
+      'transition ease-in-out duration-200'
+    )}
+    href={dappUrl}
+    rel="noreferrer"
+    testID={DappsPageSelectors.DAppOpened}
+    testIDProperties={{ dappUrl, name, featured: true }}
+    treatAsButton={true}
+  >
+    <DAppIcon name={name} logo={logo} />
+    <p className="text-font-medium mt-2 max-w-20 truncate">{name}</p>
 
-export const DappItem = memo<DAppItemProps>(({ slug, dappUrl, name, logo, categories }) => {
-  return (
-    <Anchor
-      className={clsx(
-        'flex p-3 gap-x-2 relative',
-        'justify-start items-center',
-        'rounded-8 shadow-bottom',
-        'transition ease-in-out duration-200',
-        'bg-white hover:bg-gray-100'
-      )}
-      href={dappUrl}
-      rel="noreferrer"
-      testID={DappsPageSelectors.DAppOpened}
-      testIDProperties={{ dappUrl, name, featured: false }}
-      treatAsButton={true}
-    >
+    <IconBase
+      Icon={OutLinkIcon}
+      size={12}
+      className="absolute top-1 right-1 text-secondary opacity-0 group-hover:opacity-100"
+    />
+  </Anchor>
+));
+
+export const DappItem = memo<DAppItemProps>(({ slug, dappUrl, name, logo, categories }) => (
+  <Anchor
+    className={clsx(
+      'flex justify-between items-center p-3 group',
+      'rounded-8 shadow-bottom border-0.5 border-transparent',
+      'transition ease-in-out duration-200',
+      'bg-white hover:border-lines'
+    )}
+    href={dappUrl}
+    rel="noreferrer"
+    testID={DappsPageSelectors.DAppOpened}
+    testIDProperties={{ dappUrl, name, featured: false }}
+    treatAsButton={true}
+  >
+    <div className="flex gap-x-2">
       <DAppIcon name={name} logo={logo} />
       <div className="flex flex-col gap-y-1">
-        <p className="text-font-medium max-w-52 truncate">{name}</p>
+        <p className="text-font-medium max-w-56 truncate">{name}</p>
         <div className="flex flex-wrap gap-2">
           {categories.map(category => (
             <ItemTag key={`${slug}_${category}`} name={category} />
           ))}
         </div>
       </div>
-    </Anchor>
-  );
-});
+    </div>
+
+    <IconBase Icon={OutLinkIcon} className="text-secondary opacity-0 group-hover:opacity-100" />
+  </Anchor>
+));
 
 const ItemTag = memo<{ name: string }>(({ name }) => (
   <div className="px-1 py-0.5 inline-flex items-center rounded bg-grey-4">
