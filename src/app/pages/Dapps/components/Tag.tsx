a4a3042dd83125ba4cdab0a3eac952d94aadd48e
lendihop <mishin.danil2017@gmail.com>
2025-07-24 16:34:40 +0200

    TW-1816: bouncing animation

--- src/app/pages/Dapps/components/Tag.tsx
+++ src/app/pages/Dapps/components/Tag.tsx
@@ -7,6 +7,8 @@ import { ReactComponent as CleanIcon } from 'app/icons/base/x_circle_fill.svg';
 import { DappEnum } from 'lib/apis/temple/endpoints/get-dapps-list';
 import { t, TID } from 'lib/i18n';
 
+const BOUNCING_ANIMATION_STYLES = { transition: 'all 300ms cubic-bezier(0.45, 1.45, 0.8, 1)' };
+
 interface TagProps {
   name: DappEnum;
   onClick: SyncFn<DappEnum>;
@@ -21,17 +23,14 @@ export const Tag = memo<TagProps>(({ name, onClick, selected }) => {
       className={clsx(
         'inline-flex items-center rounded-6 px-2 py-1 bg-grey-4',
         'border-0.5 border-lines text-font-description hover:text-secondary',
-        'transition-all duration-300 ease-in-out',
         selected && 'bg-secondary text-white hover:text-white'
       )}
       onClick={handleClick}
     >
       <span className="whitespace-nowrap">{t(name.toLowerCase() as TID) || name}</span>
       <div
-        className={clsx(
-          'overflow-hidden transition-all duration-300 ease-in-out',
-          selected ? 'w-4 ml-1 opacity-100' : 'w-0 opacity-0'
-        )}
+        style={BOUNCING_ANIMATION_STYLES}
+        className={clsx('overflow-hidden', selected ? 'w-4 opacity-100' : 'w-0 opacity-0')}
       >
         {selected && <IconBase Icon={CleanIcon} size={12} className="text-white" />}
       </div>
