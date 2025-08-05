319e918f8826fd3cd2a73b4bc537eff29d65dcf2
Illia Pospielov <illia.pospielov@gmail.com>
2025-08-05 18:20:33 +0300

    TW-1792 pass browser identifier for ads-api

--- src/lib/browser/index.ts
+++ src/lib/browser/index.ts
@@ -1,8 +1,22 @@
 import type { Browser, Storage } from 'webextension-polyfill';
 import browserDefault from 'webextension-polyfill';
 
+import { IS_FIREFOX, IS_GOOGLE_CHROME_BROWSER, IS_MISES_BROWSER } from 'lib/env';
+
 export { isBrowserVersionSafe } from './info';
 
 export const browser = browserDefault as Browser & {
   storage: { session?: Storage.LocalStorageArea };
 };
+
+export type BrowserIdentifier = 'chrome' | 'firefox' | 'mises' | 'unknown';
+
+function getBrowserIdentifier(): BrowserIdentifier {
+  if (IS_MISES_BROWSER) return 'mises';
+  if (IS_FIREFOX) return 'firefox';
+  if (IS_GOOGLE_CHROME_BROWSER) return 'chrome';
+
+  return 'unknown';
+}
+
+export const BROWSER_IDENTIFIER_HEADER = getBrowserIdentifier();
