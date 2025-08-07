11c1f72746e92cb277873be8acd7e39fced7efb1
Illia Pospielov <illia.pospielov@gmail.com>
2025-08-07 17:22:06 +0300

    TW-1792 add 'chrome' header for all chromium based browsers

--- src/lib/browser/index.ts
+++ src/lib/browser/index.ts
@@ -1,7 +1,7 @@
 import type { Browser, Storage } from 'webextension-polyfill';
 import browserDefault from 'webextension-polyfill';
 
-import { IS_FIREFOX, IS_GOOGLE_CHROME_BROWSER, IS_MISES_BROWSER } from 'lib/env';
+import { IS_CHROMIUM_BROWSER, IS_FIREFOX, IS_MISES_BROWSER } from 'lib/env';
 
 export { isBrowserVersionSafe } from './info';
 
@@ -14,7 +14,7 @@ type BrowserIdentifier = 'chrome' | 'firefox' | 'mises' | 'unknown';
 function getBrowserIdentifier(): BrowserIdentifier {
   if (IS_MISES_BROWSER) return 'mises';
   if (IS_FIREFOX) return 'firefox';
-  if (IS_GOOGLE_CHROME_BROWSER) return 'chrome';
+  if (IS_CHROMIUM_BROWSER) return 'chrome';
 
   return 'unknown';
 }
