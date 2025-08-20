267381e6c7c478734bb99be624ae9464540efa68
lendihop <mishin.danil2017@gmail.com>
2025-08-20 12:49:11 +0200

    TW-1863: update tzkt ghostnet base url

--- src/lib/apis/tzkt/misc.ts
+++ src/lib/apis/tzkt/misc.ts
@@ -3,8 +3,8 @@ import { TempleTezosChainId } from 'lib/temple/types';
 export const TZKT_API_BASE_URLS = {
   //TODO: revert after test
   [TempleTezosChainId.Mainnet]: 'https://staging.api.tzkt.io/v1',
+  [TempleTezosChainId.Ghostnet]: 'https://new.api.ghostnet.tzkt.io/v1',
   [TempleTezosChainId.Paris]: 'https://api.parisnet.tzkt.io/v1',
-  [TempleTezosChainId.Ghostnet]: 'https://api.ghostnet.tzkt.io/v1',
   [TempleTezosChainId.Dcp]: 'https://explorer-api.tlnt.net/v1',
   [TempleTezosChainId.DcpTest]: 'https://explorer-api.test.tlnt.net/v1'
 };
