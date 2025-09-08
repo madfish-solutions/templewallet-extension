1fd969fa1263b6ab0aa521331d926171bcb2fb83
Illia Pospielov <illia.pospielov@gmail.com>
2025-09-08 15:32:56 +0300

    TW-1682 fix find-deadcode

--- src/app/pages/Rewards/index.tsx
+++ src/app/pages/Rewards/index.tsx
@@ -1,84 +1,28 @@
-import React, { memo, useCallback, useEffect, useRef } from 'react';
+import React, { memo, useEffect } from 'react';
 
-import { capitalize, range } from 'lodash';
+import { capitalize } from 'lodash';
 
-import { Alert, PageTitle } from 'app/atoms';
+import { PageTitle } from 'app/atoms';
 import { DeadEndBoundaryError } from 'app/ErrorBoundary';
 import { acknowledgeRewardsBadge } from 'app/hooks/use-rewards-badge';
 import PageLayout from 'app/layouts/PageLayout';
 import { ReferralsCard } from 'app/pages/Rewards/referrals-card';
 import { YourRewardsCards } from 'app/pages/Rewards/your-rewards-cards';
-import { dispatch } from 'app/store';
-import { loadManyMonthsRewardsActions, loadTodayRewardsActions } from 'app/store/rewards/actions';
-import { useRpForMonthsErrorSelector, useRpForTodayErrorSelector } from 'app/store/rewards/selectors';
-import { toMonthYearIndex } from 'lib/apis/ads-api';
-import { T, t } from 'lib/i18n';
-import { useInterval } from 'lib/ui/hooks';
-import { ONE_DAY_MS, ONE_HOUR_MS } from 'lib/utils/numbers';
+import { t } from 'lib/i18n';
 import { useAccountAddressForTezos } from 'temple/front';
 
 export const RewardsPage = memo(() => {
   const accountPkh = useAccountAddressForTezos();
   if (!accountPkh) throw new DeadEndBoundaryError();
 
-  const dailyIntervalRef = useRef<NodeJS.Timer | null>(null);
-  const midnightTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const rpForTodayError = useRpForTodayErrorSelector(accountPkh);
-  const rpForMonthsError = useRpForMonthsErrorSelector(accountPkh);
-
-  const updateStatsDate = useCallback(() => {
-    return new Date();
-  }, []);
-
-  const updateRecentEarnings = useCallback(() => {
-    const newStatsDate = updateStatsDate();
-    dispatch(loadTodayRewardsActions.submit({ account: accountPkh }));
-    dispatch(
-      loadManyMonthsRewardsActions.submit({
-        account: accountPkh,
-        monthYearIndexes: [toMonthYearIndex(newStatsDate)]
-      })
-    );
-  }, [accountPkh, updateStatsDate]);
-
-  useInterval(updateRecentEarnings, [updateRecentEarnings], ONE_HOUR_MS, false);
-
   useEffect(() => {
     void acknowledgeRewardsBadge();
-
-    const newStatsDate = updateStatsDate();
-    const currentMonthYearIndex = toMonthYearIndex(newStatsDate);
-    dispatch(loadTodayRewardsActions.submit({ account: accountPkh }));
-    dispatch(
-      loadManyMonthsRewardsActions.submit({
-        account: accountPkh,
-        monthYearIndexes: range(0, 4).map(i => currentMonthYearIndex - i)
-      })
-    );
-
-    const tomorrowDate = newStatsDate;
-    tomorrowDate.setDate(tomorrowDate.getDate() + 1);
-    tomorrowDate.setHours(0, 0, 0, 0);
-    midnightTimeoutRef.current = setTimeout(() => {
-      updateRecentEarnings();
-      dailyIntervalRef.current = setInterval(() => updateRecentEarnings(), ONE_DAY_MS);
-    }, tomorrowDate.getTime() - Date.now());
-
-    return () => {
-      const dailyInterval = dailyIntervalRef.current;
-      const midnightTimeout = midnightTimeoutRef.current;
-      dailyInterval !== null && clearInterval(dailyInterval);
-      midnightTimeout !== null && clearTimeout(midnightTimeout);
-    };
-  }, [accountPkh, updateRecentEarnings, updateStatsDate]);
+  }, []);
 
   return (
     <PageLayout pageTitle={<PageTitle title={capitalize(t('rewards'))} />}>
       <div className="pt-2 pb-6">
         <div className="w-full max-w-sm mx-auto flex flex-col gap-8">
-          {(rpForTodayError || rpForMonthsError) && (
-            <Alert type="error" description={<T id="somethingWentWrong" />} autoFocus />
-          )}
           <ReferralsCard />
           <YourRewardsCards />
         </div>
--- src/app/pages/Rewards/tooltip.tsx
+++ src/app/pages/Rewards/tooltip.tsx
@@ -1,37 +1,4 @@
-import React, { memo, useMemo } from 'react';
-
-import { ReactComponent as AlertIcon } from 'app/icons/alert.svg';
-import { setTestID } from 'lib/analytics';
 import { t } from 'lib/i18n';
-import useTippy, { UseTippyOptions } from 'lib/ui/useTippy';
-
-interface RewardsTooltipProps {
-  placement: UseTippyOptions['placement'];
-  content: string;
-  testID?: string;
-  Icon?: ImportedSVGComponent;
-}
-
-export const RewardsTooltip = memo<RewardsTooltipProps>(({ placement, content, testID, Icon = AlertIcon }) => {
-  const tippyProps = useMemo(
-    () => ({
-      trigger: 'mouseenter',
-      hideOnClick: false,
-      content,
-      animation: 'shift-away-subtle',
-      placement
-    }),
-    [content, placement]
-  );
-
-  const ref = useTippy<HTMLDivElement>(tippyProps);
-
-  return (
-    <div className="w-6 h-6 flex items-center justify-center" ref={ref} {...setTestID(testID)}>
-      <Icon className="w-4 h-4 text-gray-600 stroke-current" />
-    </div>
-  );
-});
 
 export const inviteAccountInfoTippyProps = {
   trigger: 'mouseenter',
