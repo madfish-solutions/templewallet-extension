fd81f6311511ebbae8e3a8743ead3a14aa2aa9d8
lendihop <mishin.danil2017@gmail.com>
2025-09-12 10:51:22 +0200

    TW-1888: change token tag text

--- src/app/pages/Home/OtherComponents/Tokens/components/TokenTag/EvmIncentiveTag.tsx
+++ src/app/pages/Home/OtherComponents/Tokens/components/TokenTag/EvmIncentiveTag.tsx
@@ -3,7 +3,7 @@ import React, { FC, useMemo } from 'react';
 import { TagButton } from 'app/atoms/TagButton';
 import { EVM_TOKEN_SLUG } from 'lib/assets/defaults';
 import { COMMON_MAINNET_CHAIN_IDS, ETHEREUM_MAINNET_CHAIN_ID } from 'lib/temple/types';
-import { openLink } from 'lib/utils';
+import { isTruthy, openLink } from 'lib/utils';
 import { navigate } from 'lib/woozie';
 import { useEvmChainByChainId } from 'temple/front/chains';
 
@@ -15,19 +15,18 @@ interface Props {
   symbol?: string;
 }
 
-type IncentiveInfo = { label: string; link: string; external: boolean };
+type IncentiveInfo = { label: string; link: string; external?: boolean };
 
 const INCENTIVE_TOKENS: Record<number, Record<string, IncentiveInfo>> = {
   [ETHEREUM_MAINNET_CHAIN_ID]: {
     [EVM_TOKEN_SLUG]: {
-      label: 'Stake',
-      link: '/earn-eth',
-      external: false
+      label: 'APR: 3.4-10%',
+      link: '/earn-eth'
     }
   },
   [COMMON_MAINNET_CHAIN_IDS.etherlink]: {
+    // USDC
     '0x796Ea11Fa2dD751eD01b53C372fFDB4AAa8f00F9_0': {
-      // USDC
       label: 'APY 28%',
       link: 'https://app.applefarm.xyz/referral?code=APPLE-FARM-880788',
       external: true
@@ -47,7 +46,7 @@ export const EvmIncentiveTag: FC<Props> = ({ chainId, assetSlug, symbol }) => {
       e.preventDefault();
       e.stopPropagation();
 
-      incentive.external
+      isTruthy(incentive.external)
         ? openLink(incentive.link)
         : navigate({
             pathname: incentive.link
@@ -60,7 +59,6 @@ export const EvmIncentiveTag: FC<Props> = ({ chainId, assetSlug, symbol }) => {
   return (
     <TagButton
       onClick={onClick}
-      className="normal-case"
       testID={AssetsSelectors.assetItemApyButton}
       testIDProperties={{
         network: network?.name,
