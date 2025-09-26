b76e0254311f042802f3c2c194d11b5b8cdc3ae1
lendihop <mishin.danil2017@gmail.com>
2025-09-26 10:30:09 +0200

    Revert "TW-1888: Stake Ethereum page (#1395)"
    This reverts commit b044eedb2f6ad7be099ddba1d74c73df54a9c016.

--- src/app/hooks/use-should-show-v2-intro-modal.ts
+++ src/app/hooks/use-should-show-v2-intro-modal.ts
@@ -1,27 +1,17 @@
 import { useEffect } from 'react';
 
 import { isDefined } from '@rnw-community/shared';
-import { compare } from 'compare-versions';
 
-import { SHOULD_SHOW_EARN_ETH_INTRO_MODAL_STORAGE_KEY, SHOULD_SHOW_V2_INTRO_MODAL_STORAGE_KEY } from 'lib/constants';
+import { SHOULD_SHOW_V2_INTRO_MODAL_STORAGE_KEY } from 'lib/constants';
 import { APP_VERSION } from 'lib/env';
 import { useStorage } from 'lib/temple/front';
 
-export const useShouldShowIntroModals = (value: boolean) => {
+export const useShouldShowV2IntroModal = (value: boolean) => {
   const [shouldShowV2IntroModal, setShouldShowV2IntroModal] = useStorage(SHOULD_SHOW_V2_INTRO_MODAL_STORAGE_KEY);
-  const [shouldShowEarnEthIntroModal, setShouldShowEarnEthIntroModal] = useStorage(
-    SHOULD_SHOW_EARN_ETH_INTRO_MODAL_STORAGE_KEY
-  );
 
   useEffect(() => {
     if (!isDefined(shouldShowV2IntroModal) && Number(APP_VERSION.slice(0, 1)) >= 2) {
       setShouldShowV2IntroModal(value);
     }
   }, [value, shouldShowV2IntroModal, setShouldShowV2IntroModal]);
-
-  useEffect(() => {
-    if (!isDefined(shouldShowEarnEthIntroModal) && compare(APP_VERSION, '2.0.7', '>')) {
-      setShouldShowEarnEthIntroModal(value);
-    }
-  }, [value, shouldShowEarnEthIntroModal, setShouldShowEarnEthIntroModal]);
 };
