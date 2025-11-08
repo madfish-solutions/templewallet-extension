18514f39fd547b0f27409501e58e8f3523d4bb37
Inokentii Mazhara <keshan3262@gmail.com>
2025-11-08 10:09:31 +0200

    TW-1987 Move to deprecated Hypelab script version

--- webpack.config.ts
+++ webpack.config.ts
@@ -140,7 +140,11 @@ const mainConfig = (() => {
 
       /** TODO: Type def */
       new (SaveRemoteFilePlugin as any)([
-        { url: 'https://api.hypelab.com/v1/scripts/hp-sdk.js?v=0', filepath: 'scripts/hypelab.embed.js', hash: false }
+        {
+          url: 'https://r2.ixncdn.com/up/script_asset/7e7f552c9f.js',
+          filepath: 'scripts/hypelab.embed.js',
+          hash: false
+        }
       ]),
 
       // TODO: Enable, when Swap route hops SVGs are used again
