b0d14b27bd1468bac5ad0772fa84c4f391a145c7
Inokentii Mazhara <keshan3262@gmail.com>
2025-11-10 14:30:56 +0200

    TW-1998 Use iframe to display internal banner ads

--- src/app/templates/partners-promotion/components/hypelab-promotion/hypelab-image-promotion.tsx
+++ src/app/templates/partners-promotion/components/hypelab-promotion/hypelab-image-promotion.tsx
@@ -1,17 +1,27 @@
-import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { FC, useEffect, useMemo, useRef, useState } from 'react';
 
-import { Banner } from '@hypelab/sdk-react';
+import { AES } from 'crypto-js';
+import { nanoid } from 'nanoid';
 
 import { useAdTimeout } from 'app/hooks/ads/use-ad-timeout';
 import { AdsProviderTitle } from 'lib/ads';
 import { HYPELAB_STUB_CAMPAIGN_SLUG } from 'lib/constants';
 import { EnvVars } from 'lib/env';
+import { useAccountAddressForEvm } from 'temple/front';
 
 import { HypelabBannerAd, SingleProviderPromotionProps } from '../../types';
 import { ImagePromotionView } from '../image-promotion-view';
 
-import { getHypelabBannerAd } from './get-hypelab-ad';
-import { useChildAdElementRef } from './use-child-ad-element-ref';
+interface AdParams {
+  origin: string;
+  width: number;
+  height: number;
+  id: string;
+  evmAccountAddress?: string;
+  chainName?: string;
+}
+
+const adsIframeOrigin = new URL(EnvVars.HYPELAB_ADS_WINDOW_URL).origin;
 
 export const HypelabImagePromotion: FC<Omit<SingleProviderPromotionProps, 'variant'>> = ({
   accountPkh,
@@ -21,10 +31,11 @@ export const HypelabImagePromotion: FC<Omit<SingleProviderPromotionProps, 'varia
   onError,
   onReady
 }) => {
-  const hypelabBannerParentRef = useRef<HTMLDivElement>(null);
-  const hypelabBannerElementRef = useChildAdElementRef(hypelabBannerParentRef, 'hype-banner');
+  const evmAccountAddress = useAccountAddressForEvm();
+  const hypelabIframeRef = useRef<HTMLIFrameElement>(null);
   const [adIsReady, setAdIsReady] = useState(false);
   const [currentAd, setCurrentAd] = useState<HypelabBannerAd | null>(null);
+  const [adSize, setAdSize] = useState<{ width: number; height: number }>({ width: 320, height: 100 });
   const prevAdUrlRef = useRef('');
   const { backgroundAssetType, backgroundAssetUrl } = useMemo(() => {
     const creativeSet = currentAd?.creative_set;
@@ -42,39 +53,60 @@ export const HypelabImagePromotion: FC<Omit<SingleProviderPromotionProps, 'varia
 
   useAdTimeout(adIsReady, onError);
 
-  const handleReady = useCallback(() => {
-    try {
-      if (!hypelabBannerElementRef.current) {
-        throw new Error('Failed to find banner element');
-      }
-
-      const ad = getHypelabBannerAd(hypelabBannerElementRef.current);
-
-      if (ad && prevAdUrlRef.current !== ad.cta_url && ad.campaign_slug === HYPELAB_STUB_CAMPAIGN_SLUG) {
-        throw new Error('Stub ad detected');
-      } else if (ad && prevAdUrlRef.current !== ad.cta_url) {
-        setCurrentAd(ad);
-        prevAdUrlRef.current = ad.cta_url;
-        setAdIsReady(true);
-        onReady();
-      }
-    } catch (e) {
-      console.error(e);
-      onError();
-    }
-  }, [hypelabBannerElementRef, onError, onReady]);
-
+  const adId = useMemo(() => nanoid(), []);
   useEffect(() => {
-    // Banner refreshing isn't stopped by `@hypelab/sdk-react` itself
-    const banner = hypelabBannerElementRef.current;
+    if (!hypelabIframeRef.current) {
+      return;
+    }
 
-    return () => {
-      if (banner) {
-        // @ts-expect-error
-        banner.disconnectedCallback();
+    const messagesListener = (event: MessageEvent) => {
+      if (event.origin !== adsIframeOrigin) {
+        return;
+      }
+
+      try {
+        const data = typeof event.data === 'string' ? JSON.parse(event.data) : event.data;
+
+        if (data.id !== adId) return;
+
+        switch (data.type) {
+          case 'ready':
+            const ad: HypelabBannerAd | undefined = data?.ad;
+
+            if (!ad) {
+              return;
+            }
+
+            if (ad && prevAdUrlRef.current !== ad.cta_url && ad.campaign_slug === HYPELAB_STUB_CAMPAIGN_SLUG) {
+              onError();
+            } else if (ad && prevAdUrlRef.current !== ad.cta_url) {
+              setCurrentAd(ad);
+              prevAdUrlRef.current = ad.cta_url;
+              setAdIsReady(true);
+              onReady();
+            }
+            break;
+          case 'error':
+            onError();
+            break;
+          case 'resize':
+            if (data.width !== 0 && data.height !== 0) {
+              setAdSize({ width: data.width, height: data.height });
+            }
+        }
+      } catch (e) {
+        console.error(e);
       }
     };
-  }, [hypelabBannerElementRef]);
+    window.addEventListener('message', messagesListener);
+
+    return () => window.removeEventListener('message', messagesListener);
+  }, [adId, onError, onReady]);
+
+  const iframeSrc = useMemo(
+    () => getAdsTwUrl({ origin: globalThis.location.origin, width: 320, height: 100, id: adId, evmAccountAddress }),
+    [adId, evmAccountAddress]
+  );
 
   return (
     <ImagePromotionView
@@ -87,15 +119,49 @@ export const HypelabImagePromotion: FC<Omit<SingleProviderPromotionProps, 'varia
       backgroundAssetUrl={backgroundAssetUrl}
       backgroundAssetType={backgroundAssetType}
     >
-      <div ref={hypelabBannerParentRef}>
-        <Banner
-          placement={EnvVars.HYPELAB_INTERNAL_MEDIUM_PLACEMENT_SLUG}
-          // @ts-expect-error
-          class="rounded overflow-hidden"
-          onReady={handleReady}
-          onError={onError}
+      <div>
+        <iframe
+          title="Ad"
+          sandbox="allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+          className="block border-none overflow-hidden"
+          style={adSize}
+          src={iframeSrc}
+          ref={hypelabIframeRef}
         />
       </div>
     </ImagePromotionView>
   );
+};
+
+const plainSearchParamsKeys: Record<string, string> = {
+  width: 'w',
+  height: 'h',
+  slug: 'p',
+  id: 'id',
+  evmAccountAddress: 'ea',
+  chainName: 'cn'
+};
+
+const encryptWithAES = (text: string) => AES.encrypt(text, EnvVars.TEMPLE_ADS_ORIGIN_PASSPHRASE).toString();
+
+const getAdsTwUrl = ({ origin, ...restParams }: AdParams) => {
+  const url = new URL(EnvVars.HYPELAB_ADS_WINDOW_URL);
+
+  const setUrlSearchParam = (key: string, value: string | number) => {
+    url.searchParams.set(key, String(value));
+  };
+
+  setUrlSearchParam('ps', EnvVars.HYPELAB_PROPERTY_SLUG);
+
+  setUrlSearchParam('ap', 'hypelab');
+
+  Object.entries({ slug: EnvVars.HYPELAB_INTERNAL_MEDIUM_PLACEMENT_SLUG, ...restParams }).forEach(([key, value]) => {
+    if (value || value === 0) {
+      setUrlSearchParam(plainSearchParamsKeys[key], value);
+    }
+  });
+
+  setUrlSearchParam('o', encryptWithAES(origin));
+
+  return url.toString();
 };
