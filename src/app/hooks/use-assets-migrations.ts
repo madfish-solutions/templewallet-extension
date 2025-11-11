44b170df2f8a70957f400760c832a2c4ca99119a
Illia Pospielov <illia.pospielov@gmail.com>
2025-11-11 13:59:33 +0200

    TW-1967 add migration to enable etherlink by default and disable not active networks

--- src/app/hooks/use-assets-migrations.ts
+++ src/app/hooks/use-assets-migrations.ts
@@ -1,6 +1,9 @@
 import { useAllTokensMetadataSelector } from 'app/store/tezos/tokens-metadata/selectors';
 import { migrateFromIndexedDB } from 'lib/assets/migrations';
+import { SHOULD_DISABLE_NOT_ACTIVE_NETWORKS_STORAGE_KEY, EVM_CHAINS_SPECS_STORAGE_KEY } from 'lib/constants';
 import { migrate } from 'lib/local-storage/migrator';
+import { fetchFromStorage, putToStorage } from 'lib/storage';
+import { COMMON_MAINNET_CHAIN_IDS } from 'lib/temple/types';
 import { useDidMount } from 'lib/ui/hooks';
 
 export const useAssetsMigrations = () => {
@@ -12,6 +15,25 @@ export const useAssetsMigrations = () => {
         {
           name: 'assets-migrations@1.18.2',
           up: () => migrateFromIndexedDB(allMetadatas)
+        },
+        {
+          name: 'networks-auto-disable@2.0.15',
+          up: () => putToStorage(SHOULD_DISABLE_NOT_ACTIVE_NETWORKS_STORAGE_KEY, true)
+        },
+        {
+          name: 'etherlink-always-enabled@2.0.15',
+          up: async () => {
+            const stored = (await fetchFromStorage<Record<string, any>>(EVM_CHAINS_SPECS_STORAGE_KEY)) ?? {};
+            await putToStorage(EVM_CHAINS_SPECS_STORAGE_KEY, {
+              ...stored,
+              [COMMON_MAINNET_CHAIN_IDS.etherlink]: {
+                ...(stored?.[COMMON_MAINNET_CHAIN_IDS.etherlink] ?? {}),
+                disabled: false,
+                disabledAutomatically: false,
+                testnet: false
+              }
+            });
+          }
         }
       ])
   );
