cecf6597423bc27ad2f177495fae479aefc65da3
lendihop <mishin.danil2017@gmail.com>
2025-12-03 03:27:30 +0100

    TW-1999: adjust buttons behavior in controller

--- src/app/hooks/use-assets-view-state.tsx
+++ src/app/hooks/use-assets-view-state.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import constate from 'constate';
 import { useDebounce } from 'use-debounce';
@@ -6,28 +6,25 @@ import { useDebounce } from 'use-debounce';
 import { useBooleanState } from 'lib/ui/hooks';
 
 export const [AssetsViewStateProvider, useAssetsViewState] = constate(() => {
-  const [manageActive, _, setManageInactive, toggleManageActive] = useBooleanState(false);
-
+  const [manageActive, setManageActive, setManageInactive] = useBooleanState(false);
+  const [searchMode, setSearchModeActive, setSearchModeInactive] = useBooleanState(false);
   const [filtersOpened, _2, setFiltersClosed, toggleFiltersOpened] = useBooleanState(false);
-
-  const [searchMode, setSearchModeActive, setSearchModeInactive, toggleSearchMode] = useBooleanState(false);
 
   const [searchValue, setSearchValue] = useState('');
   const [searchValueDebounced] = useDebounce(searchValue, 300);
 
-  const resetSearchValue = () => setSearchValue('');
+  const resetSearchValue = useCallback(() => setSearchValue(''), []);
 
   return {
     manageActive,
+    setManageActive,
     setManageInactive,
-    toggleManageActive,
     filtersOpened,
     setFiltersClosed,
     toggleFiltersOpened,
     searchMode,
     setSearchModeActive,
     setSearchModeInactive,
-    toggleSearchMode,
     searchValue,
     searchValueDebounced,
     setSearchValue,
