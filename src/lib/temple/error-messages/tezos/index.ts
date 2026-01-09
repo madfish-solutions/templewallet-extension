e8efffc68e4c0f18dfd22160740b655f88e68cba
lendihop <mishin.danil2017@gmail.com>
2026-01-09 17:22:26 +0100

    TW-1984: update typescript part 1

--- src/lib/temple/error-messages/tezos/index.ts
+++ src/lib/temple/error-messages/tezos/index.ts
@@ -149,14 +149,14 @@ function hasErrorPattern(error: SerializedTezosOperationError, patterns: readonl
   return error.errors.some(err => patterns.some(pattern => err.id.includes(pattern)));
 }
 
+function isTezosGenericOperationErrorArray(body: unknown): body is TezosGenericOperationError[] {
+  return Array.isArray(body) && body.length > 0 && body.every(item => isObject(item) && 'kind' in item && 'id' in item);
+}
+
 function tryMakeTezosOperationError(error: SerializedHttpResponseError) {
   try {
     const body = JSON.parse(error.body);
-    if (
-      Array.isArray(body) &&
-      body.length > 0 &&
-      body.every(item => isObject(item) && 'kind' in item && 'id' in item)
-    ) {
+    if (isTezosGenericOperationErrorArray(body)) {
       return new TezosOperationError(body, '', []);
     }
   } catch {
