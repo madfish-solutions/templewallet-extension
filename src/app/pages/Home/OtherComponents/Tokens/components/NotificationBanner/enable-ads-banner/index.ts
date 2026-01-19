7b71066cd97ab11ffb0283175592302aa0c13e41
lendihop <mishin.danil2017@gmail.com>
2026-01-19 17:17:40 +0100

    TW-1984: revert deleted comments

--- src/app/pages/Home/OtherComponents/Tokens/components/NotificationBanner/enable-ads-banner/index.ts
+++ src/app/pages/Home/OtherComponents/Tokens/components/NotificationBanner/enable-ads-banner/index.ts
@@ -2,6 +2,10 @@ import { use } from 'react';
 
 const importEnableAdsBannerModule = async () => {
   try {
+    // An error appears below if and only the imported file is removed
+    // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+    // @ts-ignore
+    // eslint-disable-next-line import/no-unresolved
     return await import(
       'app/pages/Home/OtherComponents/Tokens/components/NotificationBanner/enable-ads-banner/component'
     );
