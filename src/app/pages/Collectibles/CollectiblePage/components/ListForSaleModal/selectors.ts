d49626377af57d74fe098002f7b0ac11f5510363
Illia Pospielov <illia.pospielov@gmail.com>
2026-01-29 11:37:11 +0200

    TW-2075 add list for sale button for tezos nfts (#1488)
    * TW-2075 add list for sale button for tezos nfts
    
    * TW-2075 fix opening new tab

---
+++ src/app/pages/Collectibles/CollectiblePage/components/ListForSaleModal/selectors.ts
@@ -0,0 +1,5 @@
+export enum ListForSaleModalSelectors {
+  closeButton = 'List For Sale Modal/Close Button',
+  goToMarketplaceButton = 'List For Sale Modal/Go To Marketplace Button',
+  redirectWithListInWalletButton = 'List For Sale Modal/Redirect With List In Wallet Button'
+}
