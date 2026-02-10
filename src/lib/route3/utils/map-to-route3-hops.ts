9feef9fe1e3e654c7e38167d4ad568f45326cb29
lendihop <mishin.danil2017@gmail.com>
2026-02-10 13:16:39 +0100

    TW-2131: fix hops mapping

--- src/lib/route3/utils/map-to-route3-hops.ts
+++ src/lib/route3/utils/map-to-route3-hops.ts
@@ -1,20 +1,19 @@
-import { MichelsonMap } from '@taquito/michelson-encoder';
 import { BigNumber } from 'bignumber.js';
 
 import { Hop, Route3Hop } from 'lib/route3/interfaces';
 
-export const mapToRoute3ExecuteHops = (hops: Route3Hop[]): MichelsonMap<string, Hop> => {
-  const result = new MichelsonMap<string, Hop>();
+export const mapToRoute3ExecuteHops = (hops: Route3Hop[]): Record<string, Hop> => {
+  const result: Record<string, Hop> = {};
 
-  hops.forEach(({ dexId, tokenInAmount, tradingBalanceAmount, code, params }, index) =>
-    result.set(index.toString(), {
+  hops.forEach(({ dexId, tokenInAmount, tradingBalanceAmount, code, params }, index) => {
+    result[index.toString()] = {
       dex_id: dexId,
       code,
       amount_from_token_in_reserves: new BigNumber(tokenInAmount),
       amount_from_trading_balance: new BigNumber(tradingBalanceAmount),
       params: params ?? ''
-    })
-  );
+    };
+  });
 
   return result;
 };
