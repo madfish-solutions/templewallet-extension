6422da8eff20edff950b355967448c5f0509e197
lendihop <mishin.danil2017@gmail.com>
2026-02-10 15:17:55 +0100

    fix eslint rule disable

--- src/app/pages/Home/OtherComponents/Tokens/components/NotificationBanner/enable-ads-banner/index.ts
+++ src/app/pages/Home/OtherComponents/Tokens/components/NotificationBanner/enable-ads-banner/index.ts
@@ -2,11 +2,11 @@ import { use } from 'react';
 
 const importEnableAdsBannerModule = async () => {
   try {
-    // An error appears below if and only the imported file is removed
-    // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
-    // @ts-ignore
-    // eslint-disable-next-line import/no-unresolved
     return await import(
+      // An error appears below if and only the imported file is removed
+      // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+      // @ts-ignore
+      // eslint-disable-next-line import/no-unresolved
       'app/pages/Home/OtherComponents/Tokens/components/NotificationBanner/enable-ads-banner/component'
     );
   } catch {
