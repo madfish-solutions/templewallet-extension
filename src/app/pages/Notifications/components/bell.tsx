daf4cc5eafb6c6615317059d578e69483085edff
Danyl Mishyn <35381314+lendihop@users.noreply.github.com>
2026-02-16 10:10:08 +0100

    TW-2137: add dot (#1499)

--- src/app/pages/Notifications/components/bell.tsx
+++ src/app/pages/Notifications/components/bell.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 
 import { IconBase } from 'app/atoms';
+import { AnimatedDot } from 'app/atoms/AnimatedDot';
 import { ReactComponent as BellIcon } from 'app/icons/base/bell.svg';
 import { useNewNotificationsAmountSelector } from 'app/store/notifications/selectors';
 
@@ -10,18 +11,9 @@ export const NotificationsBell = memo(() => {
 
   return (
     <div className="relative">
-      {isNewNotificationsAvailable && <AnimatedDot />}
+      {isNewNotificationsAvailable && <AnimatedDot className="top-1 left-0.5" />}
 
       <IconBase Icon={BellIcon} size={16} className="text-secondary" />
     </div>
   );
 });
-
-const AnimatedDot = memo(() => (
-  <div className="absolute top-1 left-0.5">
-    <span className="relative flex w-1 h-1">
-      <span className="animate-ping absolute inline-flex h-full w-full rounded-circle bg-primary-hover opacity-75" />
-      <span className="relative inline-flex rounded-circle w-1 h-1 bg-primary" />
-    </span>
-  </div>
-));
--- src/app/pages/Notifications/components/rewards.tsx
+++ src/app/pages/Notifications/components/rewards.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 
 import { IconBase } from 'app/atoms';
+import { AnimatedDot } from 'app/atoms/AnimatedDot';
 import { useRewardsBadgeVisible } from 'app/hooks/use-rewards-badge';
 import { ReactComponent as GiftIcon } from 'app/icons/base/gift.svg';
 
@@ -9,18 +10,9 @@ export const RewardsIconWithBadge = memo(({ className }: { className?: string })
 
   return (
     <div className="relative">
-      {rewardsBadgeVisible && <AnimatedDot />}
+      {rewardsBadgeVisible && <AnimatedDot className="top-1 left-0.5" />}
 
       <IconBase Icon={GiftIcon} size={16} className={className ?? 'text-secondary'} />
     </div>
   );
 });
-
-const AnimatedDot = memo(() => (
-  <div className="absolute top-1 left-0.5">
-    <span className="relative flex w-1 h-1">
-      <span className="animate-ping absolute inline-flex h-full w-full rounded-circle bg-primary-hover opacity-75" />
-      <span className="relative inline-flex rounded-circle w-1 h-1 bg-primary" />
-    </span>
-  </div>
-));
