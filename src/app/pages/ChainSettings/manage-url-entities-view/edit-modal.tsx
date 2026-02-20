2fdb17ea6e82891faf465c2e5fe29fd4d417e090
lendihop <mishin.danil2017@gmail.com>
2026-02-20 14:51:43 +0100

    TW-2145: rm unrelated changes

--- src/app/pages/ChainSettings/manage-url-entities-view/edit-modal.tsx
+++ src/app/pages/ChainSettings/manage-url-entities-view/edit-modal.tsx
@@ -158,15 +158,9 @@ export const EditUrlEntityModal = <T extends UrlEntityBase>({
               <SettingsCellSingle cellName={<T id={activeI18nKey} />} Component="div">
                 <Controller
                   control={control}
+                  disabled={isSubmitting}
                   name="isActive"
-                  render={({ field }) => (
-                    <ToggleSwitch
-                      {...field}
-                      checked={field.value}
-                      disabled={isSubmitting}
-                      testID={activeSwitchTestID}
-                    />
-                  )}
+                  render={({ field }) => <ToggleSwitch {...field} checked={field.value} testID={activeSwitchTestID} />}
                 />
               </SettingsCellSingle>
             </SettingsCellGroup>
