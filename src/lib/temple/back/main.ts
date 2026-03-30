1dcdbaec383d2658df0efef6ffafcf5d829b59e6
Illia Pospielov <illia.pospielov@gmail.com>
2026-03-30 12:25:54 +0300

    TW-2186 adjust names

--- src/lib/temple/back/main.ts
+++ src/lib/temple/back/main.ts
@@ -4,7 +4,6 @@ import browser, { Runtime } from 'webextension-polyfill';
 import { ValidationError } from 'yup';
 
 import { getStoredAppInstallIdentity } from 'app/storage/app-install-id';
-import { EXTERNAL_ADS_PAGE_NAME } from 'lib/ads-constants';
 import { importExtensionAdsReferralsModule } from 'lib/ads/import-extension-ads-module';
 import { importUpdateRulesStorageModule } from 'lib/ads/import-update-rules-storage';
 import { importAdsApiModule } from 'lib/apis/ads-api';
@@ -428,19 +427,13 @@ browser.runtime.onMessage.addListener(async (msg, sender) => {
           const rewardsAddresses = await getRewardsAccountCredentials();
 
           if (rewardsAddresses.evmAddress) {
-            await postAdImpression(rewardsAddresses, msg.provider, {
-              urlDomain,
-              pageName: EXTERNAL_ADS_PAGE_NAME
-            });
+            await postAdImpression(rewardsAddresses, msg.provider, { urlDomain });
           }
           else {
             const identity = await getStoredAppInstallIdentity();
             if (!identity) throw new Error('App identity not found');
             const installId = identity.publicKeyHash;
-            await postAnonymousAdImpression(installId, msg.provider, {
-              urlDomain,
-              pageName: EXTERNAL_ADS_PAGE_NAME
-            });
+            await postAnonymousAdImpression(installId, msg.provider, { urlDomain });
           }
         });
         break;
