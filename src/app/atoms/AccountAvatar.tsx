e397b4c7aee3d2af6ed664acb2d1832441ecf73a
lendihop <mishin.danil2017@gmail.com>
2026-04-21 20:39:06 +0200

    TW-2088: part 3

--- src/app/atoms/AccountAvatar.tsx
+++ src/app/atoms/AccountAvatar.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import { FC } from 'react';
 
 import { Avatar, AvatarProps } from './Avatar';
 import { Identicon } from './Identicon';
@@ -7,8 +7,8 @@ type AccountAvatarProps = AvatarProps & {
   seed: string;
 };
 
-export const AccountAvatar = memo<AccountAvatarProps>(({ seed, size, ...restProps }) => (
+export const AccountAvatar: FC<AccountAvatarProps> = ({ seed, size, ...restProps }) => (
   <Avatar size={size} {...restProps}>
     <Identicon type="botttsneutral" hash={seed} size={size - 4} />
   </Avatar>
-));
+);
