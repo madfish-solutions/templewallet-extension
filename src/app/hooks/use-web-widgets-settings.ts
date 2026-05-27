da0ce918d9ba7a0b2f7f66d459c3ad42aa94d1d7
Illia Pospielov <illia.pospielov@gmail.com>
2026-05-27 13:53:55 +0300

    TW-2232: use store dispatch

--- src/app/hooks/use-web-widgets-settings.ts
+++ src/app/hooks/use-web-widgets-settings.ts
@@ -1,15 +1,13 @@
 import { ChangeEvent } from 'react';
 
-import { useDispatch } from 'react-redux';
 import browser from 'webextension-polyfill';
 
+import { dispatch } from 'app/store';
 import { setWebWidgetsTokenInsightEnabledAction } from 'app/store/settings/actions';
 import { useWebWidgetsTokenInsightEnabledSelector } from 'app/store/settings/selectors';
 import { WEB_WIDGETS_TOKEN_INSIGHT_ENABLED } from 'lib/constants';
 
 export const useWebWidgetsSettings = () => {
-  const dispatch = useDispatch();
-
   const isEnabled = useWebWidgetsTokenInsightEnabledSelector();
 
   const setEnabled = (toChecked: boolean, event?: ChangeEvent<HTMLInputElement>) => {
