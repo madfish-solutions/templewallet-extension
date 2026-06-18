e73d326fdeb99d23ae662c062f273947df67900b
lendihop <mishin.danil2017@gmail.com>
2026-06-18 18:12:03 +0200

    TW-2183: shadow-top styles

--- src/app/atoms/PageModal/actions-buttons-box.tsx
+++ src/app/atoms/PageModal/actions-buttons-box.tsx
@@ -28,7 +28,7 @@ export const ActionsButtonsBox: FC<ActionsButtonsBoxProps> = ({
         'p-4 pb-6 flex gap-2.5',
         `flex-${flexDirection}`,
         bgSet && 'bg-white',
-        shouldCastShadow && 'shadow-bottom border-t-0.5 border-lines overflow-y-visible',
+        shouldCastShadow && 'shadow-top overflow-y-visible',
         className
       )}
       {...restProps}
